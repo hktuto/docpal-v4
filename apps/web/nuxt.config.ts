85950eefa5626f443e76ea52ff31c94382d9d760
Sean Tsang <9394wing@gmail.com>
2024-09-13 00:20:25 +0800

    setup

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr:false,
-  modules: ['@nuxt/test-utils/module', 'nuxt-authorization'],
+  modules: ['@nuxt/test-utils/module'],
+  extends:[
+    '../../packages/base',
+    '../../packages/authApp',
+  ]
   
 })
---
+++ packages/authApp/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <AuthState>
+        <template #default="{ loggedIn }">
+            <AppWrapper>
+                <template #sidebar>
+                    sidebar
+                </template>
+                <template #header>
+                    header
+                </template>
+                <template #default>
+                    <slot />
+                </template>
+            </AppWrapper>
+        </template>
+        <template #placeholder>
+            loading
+        </template>
+    </AuthState>
+
+</template>
