14ac1d67b51ea37810a8f67a07d6c57f60698db7
Sean Tsang <9394wing@gmail.com>
2024-09-17 16:47:11 +0800

    remove

--- packages/base/components/app/wrapper.vue
+++ packages/base/components/app/wrapper.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts" >
-<<<<<<<< HEAD:packages/base/components/app/wraper.vue
 import { useMouse } from '@vueuse/core'
 const {x,y} = useMouse()
+
 </script>
 
 <template>
     <div class="appFullPage" :style="`--mouse-x:${x}px;--mouse-y:${y}px;`">
-        <div class="appSideBar">
-            <slot name="sidebar" />
-========
-
-</script>
-
-<template>
-    <div class="appFullPage">
         <div class="appSidebar">
             <slot name="sidebar"></slot>
->>>>>>>> setup:packages/base/components/app/wrapper.vue
         </div>
         <div class="appMainContainer">
             <div class="appContent">
@@ -36,21 +27,6 @@ const {x,y} = useMouse()
     display: grid;
     grid-template-columns: min-content 1fr;
     grid-template-rows: 1fr;
-<<<<<<<< HEAD:packages/base/components/app/wraper.vue
-    background-color: var(--app-bg-level-1);
-    padding: var(--page-padding);
-}
-.appSideBar{
-    
-    overflow: visible;
-    z-index: 1;
-    
-}
-.appMainContainer{
-    border-radius: var(--app-border-radius-s);
-    background-color: var(--app-bg-level-3);
-    box-shadow: var(--el-box-shadow-lighter);
-========
     background-color: var(--app-grey-900);
     padding: var(--app-space-xs);
 }
@@ -67,7 +43,6 @@ const {x,y} = useMouse()
     height: 100%;
     width:100%;
     position: relative;
->>>>>>>> setup:packages/base/components/app/wrapper.vue
     overflow: hidden;
     padding: var(--app-space-s);
     z-index: 2;
