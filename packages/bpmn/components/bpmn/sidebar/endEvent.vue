c6893d150c92278927de7bd342b268526a82efca
seantsang <9394wing@gmail.com>
2024-09-27 21:05:13 +0800

    bpmn update

---
+++ packages/bpmn/components/bpmn/sidebar/endEvent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { Node } from '@antv/x6'
+const { node } = defineProps<{
+    node:Node
+}>()
+
+</script>
+
+<template>
+<div class="fromContainer">
+    {{ node }}
+</div>
+</template>
