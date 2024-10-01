1381ef6f7f4d8c97bb5ed0710cec194b51b368b0
Sean Tsang <9394wing@gmail.com>
2024-10-01 21:43:55 +0800

    electron set up and test drop to other tag

--- packages/publicApp/app.vue
+++ packages/publicApp/app.vue
@@ -1,36 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
-    <App>
-        <template #header>
-            <div class="logoContainer">
-                <AppLogo />
-            </div>
-        </template>
-        <template #footer>
-            <Icon name="lucide:log-out" @click="logout"/>
-            <LangSwitch />
-        </template>
-    </App>
-</template>
-
-<style scoped lang="scss">
-.sideMenu{
-    --menu-gap: var(--app-space-xs);
-    --icon-font-size: var(--app-font-size-l);
-    --menu-item-padding: var(--app-space-xs);
-    --menu-item-radius: var(--app-border-radius-s);
-    --menu-item-normal-bg: rgba(0,0,0,0);
-    --menu-item-hover-bg: var(--app-grey-950);
-    --menu-item-active-bg: var(--app-grey-1000);
-    --menu-item-normal-color: var(--app-grey-100);
-    --menu-item-hover-color: var(--app-accent-color);
-    --menu-item-active-color: var(--app-main-color);
-}
-.logoContainer{
-    // padding: var(--menu-item-padding);
-    --icon-size: calc(var(--icon-font-size) + (var(--menu-item-padding) * 2));
-}
-</style>
+    <NuxtPage />
+  </template>
