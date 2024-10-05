29e3cfffdf540484be970950c80ee424e145bfe3
seantsang <9394wing@gmail.com>
2024-10-05 17:03:01 +0800

    collection test

--- pages/browse/components/global/browse/Menu/collection.vue
+++ pages/browse/components/global/browse/Menu/collection.vue
@@ -1,5 +1,31 @@
-<template>
+<script lang="ts" setup>
+import { TabManagerKey } from '#imports'
+import { clientApi } from 'api'
+const tabProvider = inject(TabManagerKey)
+if(!tabProvider) {
+    throw createError('tab manger not found')
+}
+const query = ref({
+    currentPageIndex: 0,
+    pageSize: 10,
+})
+const { data, refresh, pending } = useAsyncData('collection-menu', () => clientApi.collectionNuxeo.getCollection({
+    currentPageIndex: 0,
+    pageSize: 20,
+}))
 
-    Collections
+const entryList = computed(() => {
+    return data.value?.data?.entryList || []
+})
+
+
+</script>
+
+<template>
+<div class="inlineListCOntainer">
+    <div v-for="item in entryList" :key="item.id" class="item">
+        {{ item.name}}
+    </div>
+</div>
 </template>
 
