170d43a755ac186a24c9f203350d0d624cd3505e
Sean Tsang <9394wing@gmail.com>
2024-11-05 17:44:03 +0800

    update build step

--- libraries/deployment/index.js
+++ libraries/deployment/index.js
@@ -3,13 +3,14 @@ const argv = require('minimist')(process.argv.slice(2));
 
 function normalizeSetting(){
     let path = argv.path || '../api/src'
-    const { API_URL } = argv
-    if(!API_URL ) throw new Error('Missing arguments')
+    const { CLIENT_URL, ADMIN_URL } = argv
+    if(!CLIENT_URL || !ADMIN_URL ) throw new Error('Missing arguments')
     
     return {
         path,
         setting:{
-            API_URL
+            CLIENT_URL,
+            ADMIN_URL
         }
     }
 }
