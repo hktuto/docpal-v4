38d70928770fb744c9f4b938266407e35448fee2
Sean Tsang <9394wing@gmail.com>
2024-11-26 16:08:55 +0800

    table and tab name update

--- packages/authApp/middleware/needAuth.ts
+++ packages/authApp/middleware/needAuth.ts
@@ -1,11 +1,11 @@
 import {defineNuxtRouteMiddleware, usePublicPageState, abortNavigation, useAuth} from '#imports'
 export default defineNuxtRouteMiddleware(async(to, from) => {
     const publicPage = usePublicPageState()
-    const { loggedIn, fetch } = useAuth()
+    const { loggedIn, login } = useAuth()
     if(!publicPage.value.includes(to.path)) {
 
       if(!loggedIn.value) {
-        await fetch()
+        await login()
         abortNavigation()
       }
     }
--- pages/admin-workflow-editor/components/global/workflowEditor/List.vue
+++ pages/admin-workflow-editor/components/global/workflowEditor/List.vue
@@ -3,167 +3,205 @@ import {TabManagerKey, useI18n, workflowEditorListTableSetting} from '#imports'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import { adminApi } from 'api';
 import { createTableConfig } from '../../../../../packages/base/utils/tableHelper';
-
+import type {VxeGridListeners} from 'vxe-table'
 
 
 const tabManager = inject(TabManagerKey)
-if(!tabManager) {
+const routerProvider = inject(MenuRouterKey)
+if(!tabManager || !routerProvider) {
     throw createError('menu manger not found')
 }
 const { t } = useI18n()
-const {page, pageSize, time, tab} = defineProps<{
-    page:number, pageSize:number, time?:number, tab:any
-}>()
 
-
-const pageParams = reactive({
-    pageNum: 0,
-    pageSize: 20,
-    totalSize:0,
-    
-})
-const tableSetting = workflowEditorListTableSetting
-    const state = reactive({
-        loading: false,
-        tableData: [],
-        options: {
-            showPagination: true,
-            paginationConfig: {
-                total: 0,
-                currentPage: 1,
-                pageSize: pageParams.pageSize
-            },
-            rowKey: 'id'
-        },
-        extraParams: {}
-    })
-
-    async function getList (param?:any) {
-        state.loading = true
-        try {
-
-            const {data}:any = await adminApi.workflowProcessDefinitionController.postPage(pageParams)
-            // await GetWorkflowDraftPageApi({ ...param, ...state.extraParams })
-            state.tableData = data.entryList
-            pageParams.totalSize = data.totalSize
-            pageParams.pageSize = param.pageSize
-            state.options.paginationConfig.total = data.totalSize
-            state.options.paginationConfig.pageSize = param.pageSize
-            state.options.paginationConfig.currentPage = param.pageNum + 1
-        } catch (error) {
-            throw error
-        }
-        state.loading = false
-    }
-
-    function handlePaginationChange (page: number, pageSize?: number) {
-        if(!page) page = pageParams.pageNum + 1
-        if(!pageSize) pageSize = pageParams.pageSize
-        const time = new Date().valueOf().toString()
-        tabManager?.navigateTo({
-            menuKey: tabManager.menuSymbol,
-            id:'workflow-editor-list',
-            label: "Workflow Editor List",
-            icon: "dp-icon:flow-outline",
-            component: "LazyWorkflowEditorList",
-            props: {
-                page, pageSize, time
-            }
-        })
-        // scroll top
-
-    }
-
-    function handleAction (command:string, row: any, rowIndex: number) {
-    switch (command) {
-        case 'edit':
-            handleDblclick(row)
-            break
-    }
-}
-
-
-async function handleDeactive(row:any) {
-    const action = await ElMessageBox.confirm(`${t('msg.confirmWhetherToDeactivate')}`)
-    if(action !== 'confirm') return
-    state.loading = true
-    await adminApi.workflowProcessDefinitionController.deleteSuspend(row.id)
-    // await DeactiveWorkflowApi(row.id)
-    state.loading = false
-    handlePaginationChange(1)
-}
-async function handleActive(row:any) {
-    state.loading = true
-    try {
-        await adminApi.workflowProcessDefinitionController.postActive(row.id)
-
-        // await ActiveWorkflowApi(row.id)
-        handlePaginationChange(1)
-    } catch (error) {
-    }
-    state.loading = false
-}
-function handleDblclick(row:any) {
-    tabManager?.navigateTo({
-        menuKey: tabManager.menuSymbol,
-        id: 'workflow-editor-detail',
-        icon: 'dp-icon:flow-outline',
-        label: 'workflowEditorDetail',
-        component: 'LazyWorkflowEditorDetail',
-        props: {
-            id: row.id
-        }
-    })
-}
-const WorkflowEditorDialogRef = ref()
-function handleAdd () {
-    WorkflowEditorDialogRef.value.handleOpen()
-}
-
-const config = createTableConfig(
-    "workflowEditorListTableSetting", 
-    adminApi.workflowProcessDefinitionController.postPage,
-    [
+const config = createTableConfig({
+    id: 'workflowEditorListTableSetting',
+    api: adminApi.workflowProcessDefinitionController.postPage,
+    columns:  [
                 {
                     field: 'name',
                     title: 'Name',
-                    fixed:'left',
-                    'min-width': 100,
+                    fixed: 'left',
+                    minWidth: 100,
                 },
                 {
                     field: 'productionVersion',
                     title: 'productionVersion',
-                    'min-width': 100,
+                    minWidth: 60,
                 },
                 {
                     field: 'latestVersion',
                     title: 'latestVersion',
-                    'min-width': 100,
+                    minWidth: 60,
                 },
                 {
                     field:'modifiedBy',
                     title: 'modifiedBy',
-                    'min-width': 100,
+                    minWidth: 200,
                 },
                 {
                     title: 'Action',
                     fixed: 'right',
-                    width: 60,
+                    minWidth: 40,
+                    slots:{
+                        default:'action'
+                    }
                 }
+            ],
+    menuConfig:{
+        body: {
+            options: [
+                [
+                    { code: 'edit_latest_version', name: 'Edit Latest Version', visible: true, disabled: false },
+                    { code: 'edit_latest_version_new_tab', name: 'Edit Latest Version in New Tab', visible: true, disabled: false },
+                ]
             ]
+        }
+    }
+
+        
+}
 )
 
-onMounted(() => {
-    // TODO: move ResponsiveFilterRef from old repo to here
+const tableEvents: VxeGridListeners = {
+    cellDblclick: ({ row, column, rowIndex }) => {
+        handleDblclick(row)
+    },
+    menuClick: ({ menu, row, column }) => {
+        switch (menu.code) {
+            case 'edit_latest_version':
+                openLastestVersion(row)
+                break;
+            case 'edit_latest_version_new_tab':
+                openLastestVersion(row, true)
+                break;
+        }
+    }
+}
 
-})
+function handleDblclick(data:any){
+    console.log('item clicked', data)
+    // TODO: open detail page
+    const newItem: any = {
+        menuKey: routerProvider?.menuSymbol,
+        id: "workflow-editor-detail-" + new Date().getTime(),
+        name: "workflow-editor-detail-" + data.id,
+        icon: 'dp-icon:flow-outline',
+        label: data.name,
+        component: 'LazyWorkflowEditorDetail',
+        props: {
+            id: data.id,
+            currentVersion: data.latestVersion,
+            productionVersion: data.productionVersion,
+            name: data.name,
+            item: data,
+        }
+    }
+    routerProvider?.navigateTo(newItem)
+    // tabManager?.openTab(newItem)
+}
+
+function openLastestVersion(data:any, openInNewTab = false){
+    console.log('item clicked', data)
+    // TODO: open detail page
+    const newItem: any = {
+        menuKey: routerProvider?.menuSymbol,
+        id: "workflow-editor-detail-" + new Date().getTime(),
+        name: "workflow-editor-detail-" + data.id,
+        icon: 'dp-icon:flow-outline',
+        label: data.name,
+        component: 'LazyWorkflowEditorDetail',
+        props: {
+            id: data.id,
+            currentVersion: data.latestVersion,
+            productionVersion: data.productionVersion,
+            name: data.name,
+            item: data,
+        }
+    }
+    if(!openInNewTab){
+        routerProvider?.navigateTo(newItem)
+    }else{
+        tabManager?.openTab(newItem)
+    }
+}
+
+function openProductionVersion(data:any){
+    console.log('item clicked', data)
+    // TODO: open detail page
+    const newItem: any = {
+        menuKey: routerProvider?.menuSymbol,
+        id: "workflow-editor-detail-" + new Date().getTime(),
+        name: "workflow-editor-detail-" + data.id,
+        icon: 'dp-icon:flow-outline',
+        label: data.name,
+        component: 'LazyWorkflowEditorDetail',
+        props: {
+            id: data.id,
+            currentVersion: data.productionVersion,
+            productionVersion: data.productionVersion,
+            name: data.name,
+            item: data,
+        }
+    }        
+    routerProvider?.navigateTo(newItem)
+    // tabManager?.openTab(newItem)
+}
+
+function saveAsNewWorkflow(data:any){
+
+}
+
+function openVersions(data:any){
+    const newItem:any = {
+        menuKey: routerProvider?.menuSymbol,
+        id: "workflow-editor-versions-" + new Date().getTime(),
+        name: "workflow-editor-versions-" + data.id,
+        icon: 'dp-icon:flow-outline',
+        label: data.name,
+        component: 'LazyWorkflowEditorVersion',
+        props: {
+            id: data.id,
+            currentVersion: data.productionVersion,
+            productionVersion: data.productionVersion,
+            name: data.name,
+            item: data,
+        }
+    }
+    routerProvider?.navigateTo(newItem)
+}
+
+function createNewWorkflow(){
+
+}
+
 </script>
 
 <template>
     <div class="pageContainer">
         <vxe-grid
             v-bind="config"
-        > </vxe-grid>
+            v-on="tableEvents"
+        >
+        <template #toolbar_buttons>
+            <ElButton type="primary" @click="createNewWorkflow">Add New Workflow</ElButton>
+        </template>
+            <template #action="{row, rowIndex}"> 
+                <ElDropdown >
+                    <ElButton type="primary" link>
+                        <ElIcon><SvgIcon src="/icons/dots.svg"/></ElIcon>
+                    </ElButton>
+                    <template #dropdown>
+                        <ElDropdownMenu>
+                            <ElDropdownItem @click="openLastestVersion(row)">Edit latest version</ElDropdownItem>
+                            <ElDropdownItem v-if="row.productionVersion" @click="openProductionVersion(row)">View Production</ElDropdownItem>
+                            <ElDropdownItem @click="handleDblclick(row)">Save As New Workflow</ElDropdownItem>
+                            <ElDropdownItem @click="handleDblclick(row)">View  Versions</ElDropdownItem>
+                            
+                        </ElDropdownMenu>
+                    </template>
+                </ElDropdown>       
+            </template>
+        </vxe-grid>
 
     </div>
 </template>
