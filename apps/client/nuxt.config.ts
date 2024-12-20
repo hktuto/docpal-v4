3c8a2d73a642641a830387dad552b7d897b4a916
Sean Tsang <9394wing@gmail.com>
2024-12-20 17:47:33 +0800

    add browse page

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/test-utils/module'],
   extends:[
     '../../packages/authApp',
-    "../../pages/browse",
+    "../../pages/client-browse",
   ],
   features:{
     inlineStyles: true,
