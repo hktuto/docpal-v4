e81af26cebd6e5adb8bd19845901661cec3c9f03
oy <OY.Ou@weltronics.com>
2025-01-06 17:29:20 +0800

    base config

--- packages/authApp/nuxt.config.ts
+++ packages/authApp/nuxt.config.ts
@@ -12,12 +12,13 @@ export default defineNuxtConfig({
   runtimeConfig:{
     public: {
       needAuth: true,
-      endpoint: {
+      endPoint: {
         admin : process.env.ADMIN_END_POINT,
         docpal : process.env.DOCPAL_END_POINT,
         office  : process.env.OFFICE_END_POINT,
         upload : process.env.UPLOAD_END_POINT,
         dashboard : process.env.DASHBOARD_PROXY,
+        upload: process.env.UPLOAD_PROXY,
       }
     }
   }
