9695340ec6d43b576d12f5c61f201b378f3cf1c2
oy <OY.Ou@weltronics.com>
2025-01-07 14:23:30 +0800

    easy form designer

--- pages/admin-easy-form/util/routerHelper.ts
+++ pages/admin-easy-form/util/routerHelper.ts
@@ -1,10 +1,22 @@
 export const routeEasyFormDetail  = function(params: any){
   return {
-    id: "master-table-detail-versions-" + new Date().getTime(),
-    name: "master-table-detail-versions-" + params.id,
+    id: "easy-form-detail-versions-" + new Date().getTime(),
+    name: "easy-form-detail-versions-" + params.id,
     icon: 'material-symbols-light:table-edit-rounded',
     label: params.name,
     component: 'LazyEasyFormDetail',
+    props: {
+      id: params.id,
+    }
+  } as TabItem
+}
+export const routeEasyFormDesigner  = function(params: any){
+  return {
+    id: "easy-form-detail-versions-" + new Date().getTime(),
+    name: "easy-form-detail-versions-" + params.id,
+    icon: 'material-symbols-light:table-edit-rounded',
+    label: params.name,
+    component: 'LazyEasyFormDesigner',
     props: {
       id: params.id,
     }
