31ee372307f15dc8e36137158d4f5af8f2d90db5
Sean Tsang <9394wing@gmail.com>
2025-01-08 16:43:07 +0800

    add public api

--- apps/electron/nuxt.config.ts
+++ apps/electron/nuxt.config.ts
@@ -36,6 +36,11 @@ export default defineNuxtConfig({
         changeOrigin: true,
         prependPath: true
       },
+      '/public-api':{
+        target: process.env.CLIENT_PROXY,
+        changeOrigin: true,
+        prependPath: true
+      },
     }
   },
   
