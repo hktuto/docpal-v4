90ed592f05f9c0985597524ae8fbfd75ec188e83
Sean Tsang <9394wing@gmail.com>
2025-01-08 17:13:27 +0800

    add document template

---
+++ pages/admin-docker-log/components/global/docker/setting.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { publicApi } from 'api'
+const routerProvider = inject(MenuRouterKey)
+if(!routerProvider) {
+    throw createError('MenuRouterKey is not provided')
+}
+
+
+onMounted(async() => {
+    const data = await publicApi.api.postUserDashboardPage({})
+})
+
+</script>
+
+
+<template>
+
+    <div class="pageContainer">
+        docker log
+    </div>
+</template>
--- pages/admin-document-template/utils/documentTemplateHelper.ts
+++ pages/admin-document-template/utils/documentTemplateHelper.ts
@@ -1,5 +1,30 @@
 interface DocumentTemplateProvider {
     getListApi: (params:any) => Promise<any>,
+    dblClickHandle: (row:any) => void,
+    handleActive: (row:any, enable:boolean) => void,
+    handleReplace: (row:any, openInNewTab?:boolean) => void,
+    handleEdit: (row:any, openInNewTab?:boolean) => void,
+    handleEditInfo: (row:any, openInNewTab?:boolean) => void,
+    handleDelete: (row:any) => void,
+    handleDownload: (row:any) => void,
+    actionPermission: (args:PermissionMethodParams) => {visible:boolean, disabled:boolean}
 }
 
 export const DocumentTemplateProviderKey: InjectionKey<DocumentTemplateProvider> = Symbol('DocumentTemplateProviderKey')
+
+
+export const createNewDocumentTemplateDetail = (data:any) => {
+    const newItem: TabItem = {
+        id: "document-template-detail-" + new Date().getTime(),
+        name: "document-template-detail-" + data.id,
+        icon: 'lucide:file-sliders',
+        label: data.name,
+        component: 'LazyDocumentTemplateDetail',
+        props: {
+            id: data.id,
+            name: data.name,
+            item: data
+        }
+    }
+    return newItem
+}
