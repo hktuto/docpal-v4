1f4686f0e6726730b8f972dd04086bd6491b9f96
Sean Tsang <9394wing@gmail.com>
2025-01-08 18:46:06 +0800

    finish docker log

---
+++ pages/admin-docker-log/components/global/docker/log.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+const routerProvider = inject(MenuRouterKey)
+if(!routerProvider) {
+    throw createError('MenuRouterKey is not provided')
+}
+const iframe = ref();
+
+const hostname = computed(() => location.host.includes('localhost') ? 'http://132.148.160.88:8989' : location.protocol + '//' + location.host)
+
+
+</script>
+
+
+<template>
+
+    <div class="pageContainer">
+        <iframe ref="iframe" :src="`${hostname}/container_logs/`" crossorigin allowfullscreen   />
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.pageContainer{
+    position: relative;
+    height: 100%;
+    width:100%;
+    iframe{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        border: none;
+        border-radius: var(--app-space-xs);
+        // opacity:0;
+        transition: opacity .3s ease-in-out;
+        &.show{
+            opacity:1;
+        }
+    }
+}
+</style>
