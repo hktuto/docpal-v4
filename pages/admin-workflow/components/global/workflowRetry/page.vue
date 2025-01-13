3b4b782d43251ee44f741efed79f6701760b2358
oy <OY.Ou@weltronics.com>
2025-01-13 14:43:43 +0800

    [workflow]manage & retry

---
+++ pages/admin-workflow/components/global/workflowRetry/page.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="pageContainer--padding">
+    <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
+      <template #toolbar_buttons>
+        <ResponsiveFilter ref="ResponsiveFilterRef" @form-change="handleFilterFormChange"
+                        inputKey="businessKey" @clear-filter="handleClearFilter" />
+      </template>
+      <template #status="{ row }">
+        <el-tag v-if="retryStatues.includes(row.state.toLowerCase())" type="danger">{{ row.state }}</el-tag>
+        <el-tag v-else-if="row.state.toLowerCase() === 'completed'" type="success">{{ row.state }}</el-tag>
+        <el-tag v-else type="info">{{ row.state }}</el-tag>
+      </template>
+    </VxeGrid>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ElMessageBox } from "element-plus";
+import { adminApi } from "api";
+import dayjs from "dayjs";
+
+const { t } = useI18n();
+let extraParams: any = {};
+const retryStatues = ['error', 'fail', 'start error'];
+const state = reactive<any>({
+});
+const {
+  tableConfig,
+  tableEvent,
+  tableRef,
+  query,
+  reload,
+} = useVxeTable({
+  id: "userTableSetting",
+  api: async (pageParams: any) => {
+    return await adminApi.api.postWorkflowQueryWorkflowRetryPage({ ...pageParams, ...extraParams });
+  },
+  columns: [
+    { field: "businessKey", title: "tableHeader_name", fixed: "left", type: "checkbox" },
+    {
+      field: "startTime",
+      title: "dpTable_createdDate",
+      formatter({ cellValue }: any) {
+        const format = userDisplayTimeSetting();
+        return dayjs(cellValue).format(format);
+      },
+    },
+    { field: "creator", title: "role.creator" },
+    {
+      field: "status",
+      title: "common_status",
+      slots: {
+        default: "status",
+      },
+    },
+  ],
+  bodyActions: [
+    [
+      {
+        code: "try",
+        name: "moreAction.re-try",
+        visible: true,
+        disabled: false,
+        action: ({ row }: any) => {
+          handleRetry(row.id);
+        },
+      },
+      
+    ],
+  ],
+  permissionMethod: (args:PermissionMethodParams) => {
+    return {
+      visible: retryStatues.includes(args.row.state.toLowerCase()),
+      disabled: false
+    }
+  }
+});
+
+async function handleRetry(id:string){
+  try{
+    await adminApi.api.postWorkflowRetryFailWorkflow({ id })
+    query()
+  }catch(error:any) {
+  }
+}
+function handleFilterFormChange(formModel: any) {
+  if (!formModel.isDesc) formModel.isDesc = true;
+  if (!!formModel.isDesc) formModel.isDesc = formModel.isDesc === "false" ? false : true;
+  let filterParams: any = {
+    businessKey: formModel.businessKey === "" ? undefined : formModel.businessKey,
+    orderBy:
+      formModel.orderBy === undefined || formModel.orderBy === ""
+        ? "startTime"
+        : formModel.orderBy,
+  };
+  filterParams.isDesc = formModel.isDesc;
+  extraParams = filterParams;
+  reload();
+}
+const ResponsiveFilterRef = ref();
+function getFilter() {
+  const data = [
+    {
+      key: "orderBy",
+      label: "tableHeader.sortBy",
+      type: "string",
+      isMultiple: false,
+      options: [
+        { label: "tableHeader_name", value: "businessKey" },
+        { label: "common_status", value: "workflowState" },
+        { label: "role.creator", value: "creator" },
+        { label: "workflow_createDate", value: "startTime" },
+      ],
+    },
+    {
+      key: "isDesc",
+      label: "tableHeader.sortOrder",
+      type: "string",
+      isMultiple: false,
+      options: [
+        { label: "tableHeader.desc", value: false },
+        { label: "tableHeader.asc", value: true },
+      ],
+    },
+  ];
+  ResponsiveFilterRef.value.init(data);
+}
+onMounted(() => {
+  getFilter();
+});
+</script>
+<style lang="scss" scoped>
+:deep .vxe-buttons--wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+.responsive-container {
+  width: 50%;
+  :deep .el-input {
+    width: 200px;
+  }
+}
+</style>
---
+++ pages/admin-workflow/components/workflow/reallocateDialog.vue
@@ -0,0 +1,91 @@
+<template>
+    <el-dialog v-model="dialogVisible" :title="$t('common_reallocation')">
+        <el-form ref="formRef"
+                :model="form"
+                label-width="120px"
+                label-position="top"
+                @submit.native.prevent
+        >
+            <el-form-item :label="$t('workflow_assignee')" prop="assignee"
+                :rules="[ { required: true, message: $t('form_common_requird'), trigger: 'change'}]">
+                <el-select v-model="form.assignee" filterable clearable
+                    style="width: 100%">
+                    <el-option v-for="item in state.userList" :key="item.id" :label="item.userId" :value="item.userId"></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <el-button @click="handleSubmit">{{$t('submit')}}</el-button>
+        </template>
+    </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import type { FormInstance } from 'element-plus'
+import { adminApi } from 'api'
+
+const emit = defineEmits(['success'])
+const state = reactive({
+    userList: []
+})
+// #region module: dialog
+    const dialogVisible = ref(false)
+    async function handleOpen(row) {
+        dialogVisible.value = true
+        if(row.assignee) {
+            form.oldAssignee = row.assignee
+            form.assignee = row.assignee
+            form.id = row.id
+        } else {
+            form.oldAssignee = ''
+            form.assignee = ''
+            form.id = row.id
+        }
+        state.userList = await adminApi.api.postNuxeoIdentityUsers({}).then(res => res.data)
+        state.userList = state.userList.filter(item => item.userId !== row.assignee && item.userId)
+    }
+// #endregion
+// #region module: form
+    const formRef = ref<FormInstance>()
+    const form = reactive({
+        oldAssignee: '',
+        assignee: '',
+        id: ''
+    })
+    async function handleSubmit() {
+        if (form.oldAssignee && form.oldAssignee === form.assignee) {
+            dialogVisible.value = false
+            return
+        }
+        try {
+            const valid = await formRef.value.validate((valid, fields) => valid)
+            if (!valid) return
+            if (!form.assignee) return
+            if (form.oldAssignee){
+                const res = await adminApi.api.postWorkflowTaskUnclaim({
+                    taskId: form.id
+                })
+                if (!res) return
+            }
+            const res2 = await adminApi.api.postWorkflowTaskClaim({
+                taskId: form.id, 
+                userId: form.assignee
+            })
+            if (!res2) return
+            emit('success')
+            dialogVisible.value = false
+        } catch (error) {
+            
+        }
+        finally {
+
+        }
+        
+    }
+// #endregion
+defineExpose({ handleOpen })
+</script>
+
+<style scoped lang="scss">
+
+</style>
