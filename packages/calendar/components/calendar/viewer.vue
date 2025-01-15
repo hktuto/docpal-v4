331d293d3378df8b3c5e5c4ae1a9ae60ba8bef00
sean tsang <9394wing@gmail.com>
2025-01-15 13:34:30 +0800

    admin calendar

---
+++ packages/calendar/components/calendar/viewer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+    calendatView: "MONTH" | "WEEK" | "DAY",
+    firstOfWeek: "MONDAY" | "SUNDAY",
+    defaultSlot: number,
+}>();
+
+const { setting } = useCalendarStore();
+
+
+</script>
+
+
+<template>
+    <div class="calendarContainer">
+
+    </div>
+</template>
