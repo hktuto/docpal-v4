b57288bd0a912a79ba181d7e58e3c57283505771
sean tsang <9394wing@gmail.com>
2025-01-15 17:52:26 +0800

    add basic calendar viewer

--- packages/calendar/components/calendar/viewer.vue
+++ packages/calendar/components/calendar/viewer.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-const props = defineProps<{
-    calendatView: "MONTH" | "WEEK" | "DAY",
-    firstOfWeek: "MONDAY" | "SUNDAY",
-    defaultSlot: number,
-}>();
+import dayjs from 'dayjs'
+import { ScheduleXCalendar } from '@schedule-x/vue'
+import {
+  createCalendar,
+  createViewDay,
+  createViewMonthAgenda,
+  createViewMonthGrid,
+  createViewWeek,
+} from '@schedule-x/calendar'
+import '@schedule-x/theme-default/dist/index.css'
+
 
 const { setting } = useCalendarStore();
 
+const calendarApp = createCalendar({
+    selectedDate: dayjs().format('YYYY-MM-DD'),
+    views:[
+        createViewDay(),
+        createViewWeek(),
+        createViewMonthGrid(),
+        createViewMonthAgenda(),
+    ],
+    events: [
+        {
+            id: 1,
+            title: 'Event 1',
+            start: '2023-12-19',
+            end: '2023-12-19',
+        },
+    ],
+
+})
 
 </script>
 
 
 <template>
-    <div class="calendarContainer">
-
+    <div class="calendarViewerContainer">
+        <ScheduleXCalendar :calendar-app="calendarApp" />
     </div>
 </template>
+
+<style lang="scss" scoped>
+.calendarViewerContainer{
+    width: 100%;
+    height: 100%;
+    min-height: 500px;
+    position: relative;
+}
+</style>
