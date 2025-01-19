2c3cbaa71578e1820e87e633901a669835055eeb
sean tsang <9394wing@gmail.com>
2025-01-19 17:00:59 +0800

    fix wrapper css

--- packages/base/components/tab/header/tab.vue
+++ packages/base/components/tab/header/tab.vue
@@ -95,7 +95,7 @@ function closeTab(){
 </script>
 
 <template>
-    <div class="wrapper" @click="tabFocus">
+    <div class="tabWrapper" @click="tabFocus">
 
         <div ref="elRef" :data-tab-id="tab.id" :id="`tab-header-${tab.parent}-${tab.id}`"
         :class="{tabItem:true, showing:selected, [dragState.type]:true, [(dragState as any).closestEdge] :true}" >
