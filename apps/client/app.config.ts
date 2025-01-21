59ccf3afcc4f27064b03055ed58aac521ba40029
oy <OY.Ou@weltronics.com>
2025-01-21 16:20:39 +0800

    menu

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -1,16 +1,96 @@
 export default defineAppConfig({
-    appMenu:[
+    // appMenu:[
+    //     {
+    //         name:'client-browse'
+    //     },
+    //     {
+    //         name:'client-search'
+    //     },
+    //     {
+    //         name:'client-master-table'
+    //     },
+    //     {
+    //         name:'client-folder-cabinet'
+    //     }
+    // ],
+    appMenu: [
         {
-            name:'client-browse'
+            name: "client-browse"
         },
         {
-            name:'client-search'
+            name: "client-search"
         },
         {
-            name:'client-master-table'
+            label:"file_uploads",
+            icon: "dp-icon:flow-outline",
+            children: [
+                {
+                    name: "clientAIUpload"
+                },
+                {
+                    name: "client-fileRequest"
+                }
+            ]
         },
         {
-            name:'client-folder-cabinet'
+            label:"file_share_module",
+            icon: "dp-icon:flow-outline",
+            children: [
+                {
+                    name: "client-share"
+                },
+                {
+                    name: "client-share-me"
+                },
+                {
+                    name: "client-share-other"
+                }
+            ]
+        },
+        {
+            name: "client-collections"
+        },
+        {
+            name: "client-smartFolder"
+        },
+        {
+            name: "client-folder-cabinet"
+        },
+        {
+            name: "client-workflow"
+        },
+        {
+            label:"retention_policies",
+            icon: "dp-icon:flow-outline",
+            children: [
+                {
+                    name: "client-retention"
+                },
+                {
+                    name: "client-holdPolicies"
+                }
+            ]
+        },
+        {
+            label:"DASHBOARD",
+            icon: "dp-icon:flow-outline",
+            children: [
+                {
+                    name: "client-work-panel"
+                },
+                {
+                    name: "client-dashboard"
+                }
+            ]
+        },
+        {
+            name: "client-master-table"
+        },
+        {
+            name: "client-trash"
+        },
+        {
+            name: "client-case"
         }
-    ],
+    ]
 })
