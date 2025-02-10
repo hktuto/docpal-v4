78ea58b51dc57a474de808cebb2c61e54f09aa49
oy <OY.Ou@weltronics.com>
2025-02-10 08:53:19 +0800

    v-form i18n

--- pages/admin-easy-form/util/routerHelper.ts
+++ pages/admin-easy-form/util/routerHelper.ts
@@ -12,8 +12,8 @@ export const routeEasyFormDetail  = function(params: any){
 }
 export const routeEasyFormDesigner  = function(params: any){
   return {
-    id: "easy-form-detail-versions-" + new Date().getTime(),
-    name: "easy-form-detail-versions-" + params.id,
+    id: "easy-form-designer-versions-" + new Date().getTime(),
+    name: "easy-form-designer-versions-" + params.id,
     icon: 'material-symbols-light:table-edit-rounded',
     label: params.name,
     component: 'LazyEasyFormDesigner',
