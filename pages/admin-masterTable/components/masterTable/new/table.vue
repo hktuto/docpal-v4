d0d63b42356a274a0b08fd7652af4f475ea35a11
oy <OY.Ou@weltronics.com>
2025-02-12 13:49:17 +0800

    [masterTable]add fix dbl click

--- pages/admin-masterTable/components/masterTable/new/table.vue
+++ pages/admin-masterTable/components/masterTable/new/table.vue
@@ -1,153 +1,231 @@
 <template>
-    <VxeGrid ref="tableRef" v-bind="tableConfig">
-      <template #toolbar_buttons>
-        <div class="flex-x-start ">
-          <span v-if="!state.name" class="color--danger">{{ $t('masterTable.emptyName') }}</span>
-          {{ state.name }}
-          <Icon name="material-symbols:edit-square" class="normal cursor-pointer" @click="tableDialogRef.handleOpen({ name: state.name})"></Icon>
+  <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
+    <template #toolbar_buttons>
+      <div class="flex-x-start">
+        <span v-if="!state.name" class="color--danger">{{
+          $t("masterTable.emptyName")
+        }}</span>
+        {{ state.name }}
+        <Icon
+          name="material-symbols:edit-square"
+          class="normal cursor-pointer"
+          @click="tableDialogRef.handleOpen({ name: state.name })"
+        ></Icon>
+      </div>
+      <div class="flex-x-center">
+        <el-dropdown>
+          <el-button class="el-icon--left"> {{ $t("masterTable.newSchema") }} </el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item
+                v-for="item in state.dataTypeList"
+                :key="item.value"
+                @click="handleSingleSchemaAdd(item)"
+              >
+                {{ $t(`marsterTable.type.${item.value}`) }}
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <el-button type="primary" @click="handleSubmit">{{
+          $t("common_submit")
+        }}</el-button>
+      </div>
+    </template>
+    <template #columnName="{ row }">
+      <div class="masterTable-columnName">
+        <span>{{ row.fieldName }}</span>
+        <div
+          v-if="row.unique && !isDefault(row)"
+          class="column-dynamic"
+          style="--column-color: #0099ff"
+        >
+          <div class="column-dynamic-point"></div>
+          {{ $t("marsterTable.unique") }}
         </div>
-        <div class="flex-x-center">
-          <el-dropdown>
-              <el-button class="el-icon--left">  {{ $t('masterTable.newSchema') }} </el-button>
-              <template #dropdown>
-                  <el-dropdown-menu>
-                      <el-dropdown-item v-for="item in state.dataTypeList" :key="item.value" 
-                          @click="handleSingleSchemaAdd(item)">
-                          {{ $t(`marsterTable.type.${item.value}`) }}
-                      </el-dropdown-item>
-                  </el-dropdown-menu>
-              </template>
-          </el-dropdown>
-          <el-button type="primary" @click="handleSubmit">{{ $t('common_submit') }}</el-button>
+        <div
+          v-else-if="row.required && !isDefault(row)"
+          class="column-dynamic"
+          style="--column-color: #7b61ff"
+        >
+          <div class="column-dynamic-point"></div>
+          {{ $t("marsterTable.required") }}
         </div>
-      </template>
-      <template #columnName="{ row }">
-        <div class="masterTable-columnName">
-            <span>{{ row.fieldName }}</span>
-            <div v-if="row.unique && !isDefault(row)" class="column-dynamic" style="--column-color: #0099FF">
-                <div class="column-dynamic-point"></div>
-                {{ $t('marsterTable.unique') }}
-            </div>
-            <div v-else-if="row.required && !isDefault(row)" class="column-dynamic" style="--column-color: #7B61FF">
-                <div class="column-dynamic-point"></div>
-                {{ $t('marsterTable.required') }}
-            </div>
-        </div>
-      </template>
-      <template #more="{ row }">
-        <Icon v-if="!isDefault(row)" class="closeIcon" name="ic:round-close" @click.stop="handleDelete(row)"></Icon>
-      </template>
-     </VxeGrid>
-  <MasterTableDialog ref="tableDialogRef" @confirm="({name}) => state.name = name"></MasterTableDialog>
-  <MasterTableNewSchemaDialog ref="schemaDialogRef" disabledUniqueList="" @add="handleAddSchama"
-      @update="handleUpdateSchama"/>
+      </div>
+    </template>
+    <template #more="{ row }">
+      <Icon
+        v-if="!isDefault(row)"
+        class="closeIcon"
+        name="ic:round-close"
+        @click.stop="handleDelete(row)"
+      ></Icon>
+    </template>
+  </VxeGrid>
+  <MasterTableDialog
+    ref="tableDialogRef"
+    @confirm="({ name }) => (state.name = name)"
+  ></MasterTableDialog>
+  <MasterTableNewSchemaDialog
+    ref="schemaDialogRef"
+    disabledUniqueList=""
+    @add="handleAddSchama"
+    @update="handleUpdateSchama"
+  />
 </template>
 <script lang="ts" setup>
-import {MenuRouterKey} from '#imports'
-import { getIgnoreSchemas } from '~/utils/masterTableProvider';
-const ignoreList = getIgnoreSchemas()
-const routerProvider = inject(MenuRouterKey)
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { adminApi } from 'api'
+import { MenuRouterKey } from "#imports";
+import { getIgnoreSchemas } from "~/utils/masterTableProvider";
+const ignoreList = getIgnoreSchemas();
+const routerProvider = inject(MenuRouterKey);
+import { ElMessage, ElMessageBox } from "element-plus";
+import { adminApi } from "api";
 const state = reactive<any>({
-  name: '',
-  dataTypeList: []
-})
+  name: "",
+  dataTypeList: [],
+});
 
-const { tableConfig, tableEvent , tableRef, reload, query } = useVxeTable({
-  id: 'masterTableTableSetting',
-  columns:  [
-      { field: 'fieldName', title: 'masterTable.columnName', fixed: 'left',
-        slots:{
-          default:'columnName',
-        }},
-      { field: 'dataType', title: 'masterTable.dataType',},
-      { title: 'dpTable_actions', 
-        slots:{
-          default:'more',
-        }
-      }
-  ],  
-  height: 'auto',
+const { tableConfig, tableEvent, tableRef, reload, query } = useVxeTable({
+  id: "masterTableTableSetting",
+  columns: [
+    {
+      field: "fieldName",
+      title: "masterTable.columnName",
+      fixed: "left",
+      slots: {
+        default: "columnName",
+      },
+    },
+    { field: "dataType", title: "masterTable.dataType" },
+    {
+      title: "dpTable_actions",
+      slots: {
+        default: "more",
+      },
+    },
+  ],
+  height: "auto",
   optionalConfig: {
     data: [
       { dataType: "timestamp", fieldName: "created_date", required: true, unique: false },
-      { dataType: "varchar:255", fieldName: "id", primaryKey: true, required: true, unique: true },
-      { dataType: "timestamp", fieldName: "modified_date", required: true, unique: false },
-      { dataType: "varchar:255", fieldName: "modified_by", required: true, unique: false },
+      {
+        dataType: "varchar:255",
+        fieldName: "id",
+        primaryKey: true,
+        required: true,
+        unique: true,
+      },
+      {
+        dataType: "timestamp",
+        fieldName: "modified_date",
+        required: true,
+        unique: false,
+      },
+      {
+        dataType: "varchar:255",
+        fieldName: "modified_by",
+        required: true,
+        unique: false,
+      },
       { dataType: "varchar:255", fieldName: "created_by", required: true, unique: false },
-      { dataType: "boolean", fieldName: "status", required: true, unique: false }
+      { dataType: "boolean", fieldName: "status", required: true, unique: false },
     ],
+  },
+  dblClickAction: ({ row, column, event }: any) => {
+    console.log(row);
+
+    handleDblclick(row);
+  },
+});
+
+function handleDblclick(row: any) {
+  if (!isDefault(row)) {
+    state.curRow = row;
+    checkDisabledUniqueAndOpenSchemaDialog(row, true);
   }
-})
-function isDefault(row: any) {
-  return ignoreList.includes(row.fieldName)
 }
-const tableDialogRef = ref()
-const schemaDialogRef = ref()
+function isDefault(row: any) {
+  return ignoreList.includes(row.fieldName);
+}
+const tableDialogRef = ref();
+const schemaDialogRef = ref();
 function handleSingleSchemaAdd(row: any) {
-  schemaDialogRef.value.handleOpen({ unique: row.unique, dataType: row.value }, { disabledUnique: !row.unique })
+  schemaDialogRef.value.handleOpen(
+    { unique: row.unique, dataType: row.value },
+    { disabledUnique: !row.unique }
+  );
 }
 async function handleSubmit() {
-  if(!state.name) {
-    tableDialogRef.value.handleOpen({ name: state.name})
-    return
+  if (!state.name) {
+    tableDialogRef.value.handleOpen({ name: state.name });
+    return;
   }
-  if(tableConfig.data.length === 6) {
-    ElMessage.error($i18n.t('tip.masterTable.noValidData'))
-    handleSingleSchemaAdd(state.dataTypeList[0])
-    return
+  if (tableConfig.data.length === 6) {
+    ElMessage.error($i18n.t("tip.masterTable.noValidData"));
+    handleSingleSchemaAdd(state.dataTypeList[0]);
+    return;
   }
   try {
-    const {data} = await adminApi.api.postMasterTables({
+    const { data } = await adminApi.api.postMasterTables({
       name: state.name,
-      fields: tableConfig.data
-    })
-    routerProvider.navigateTo(routeMasterTableDetail(data)) 
+      fields: tableConfig.data,
+    });
+    routerProvider.navigateTo(routeMasterTableDetail(data));
   } catch (error) {
-      console.log(error)
+    console.log(error);
   }
 }
 function checkDisabledUniqueAndOpenSchemaDialog(row: any, edit: boolean = false) {
-  const dataType = state.dataTypeList.find((item: any) => item.value === row.dataType)
-  schemaDialogRef.value.handleOpen(row, { edit, disabledUnique: !dataType.unique })
+  const dataType = state.dataTypeList.find((item: any) => item.value === row.dataType);
+  schemaDialogRef.value.handleOpen(row, { edit, disabledUnique: !dataType.unique });
 }
 async function handleDelete(row: any) {
-    const action = await ElMessageBox.confirm(`${$i18n.t('msg_confirmWhetherToDelete')}`)
-    if(action !== 'confirm') return
-    const index = tableConfig.data.findIndex(item => item.fieldName === row.fieldName)
-    tableConfig.data.splice(index, 1)
+  const action = await ElMessageBox.confirm(`${$i18n.t("msg_confirmWhetherToDelete")}`);
+  if (action !== "confirm") return;
+  const index = tableConfig.data.findIndex((item) => item.fieldName === row.fieldName);
+  tableConfig.data.splice(index, 1);
 }
 function handleAddSchama(schema: any) {
-    const index = tableConfig.data.findIndex(item => item.fieldName === schema.fieldName)
-    if(index !== -1) {
-      ElMessage.error($i18n.t('tip.duplicate'))
-      setTimeout(() => {
-        checkDisabledUniqueAndOpenSchemaDialog(schema)
-      }, 1000)
-    } else {
-      tableConfig.data.push({ ...schema, label: schema.fieldName })
-    }
+  console.log("????", schema);
+  const index = tableConfig.data.findIndex((item) => item.fieldName === schema.fieldName);
+  if (index !== -1) {
+    ElMessage.error($i18n.t("tip.duplicate"));
+    setTimeout(() => {
+      checkDisabledUniqueAndOpenSchemaDialog(schema);
+    }, 1000);
+  } else {
+    tableConfig.data.push({ ...schema, label: schema.fieldName });
+  }
 }
 function handleUpdateSchama(schema: any) {
-    const curIndex =  tableConfig.data.findIndex((item: any) => item.fieldName === state.curRow.fieldName)
-    const duplicateIndex =  tableConfig.data.findIndex((item: any, index: number) => item.fieldName === schema.fieldName && index !== curIndex)
-    if(duplicateIndex !== -1) {
-        ElMessage.error($i18n.t('tip.duplicate'))
-        setTimeout(() => {
-            checkDisabledUniqueAndOpenSchemaDialog(schema, true)
-        }, 1000)
-    } else {
-      tableConfig.data.splice(curIndex, 1, schema)
-    }
-}
-onMounted(async() => {
-  const res = await adminApi.api.getMasterTablesDatatypeMapping()
-  state.dataTypeList = res.data
-  state.dataTypeList.push({
-    value: 'relation', label:  $i18n.t(`marsterTable.type.relation`), unique: false
-  })
-})
+  console.log("????", schema, state.curRow);
+  // state.curRow = schema;
 
+  const curIndex = tableConfig.data.findIndex(
+    (item: any) => item.fieldName === state.curRow.fieldName
+  );
+  const duplicateIndex = tableConfig.data.findIndex(
+    (item: any, index: number) =>
+      item.fieldName === schema.fieldName && index !== curIndex
+  );
+  if (duplicateIndex !== -1) {
+    ElMessage.error($i18n.t("tip.duplicate"));
+    setTimeout(() => {
+      checkDisabledUniqueAndOpenSchemaDialog(schema, true);
+    }, 1000);
+  } else {
+    tableConfig.data.splice(curIndex, 1, schema);
+    tableRef.value?.loadData(tableConfig.data);
+  }
+}
+onMounted(async () => {
+  const res = await adminApi.api.getMasterTablesDatatypeMapping();
+  state.dataTypeList = res.data;
+  state.dataTypeList.push({
+    value: "relation",
+    label: $i18n.t(`marsterTable.type.relation`),
+    unique: false,
+  });
+});
 </script>
 <style lang="scss" scoped></style>
