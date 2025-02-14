c3ff1b1e7c196e469d9c6c8421d76a78787a85d1
sean tsang <9394wing@gmail.com>
2025-02-14 17:29:41 +0800

    move page

--- pages/client-browse/components/global/browse/page.vue
+++ pages/client-browse/components/global/browse/page.vue
@@ -11,40 +11,6 @@ const props = defineProps<{
     idOrPath: string ,
     filter: any,
 }>();
-const { idOrPath } = toRefs(props)
-const tabProvider = inject(TabManagerKey)
-const routerProvider = inject(MenuRouterKey)
-const selectedItem = ref<any[]>([])
-if(!tabProvider || !routerProvider) {
-    throw createError('provider not found')
-}
-const tableRef = ref<InstanceType<typeof BrowseListTable>>();
-function addToSelection(items: any[]){
-    selectedItem.value.push(...items)
-}
-function removeFromSelection(items: any[]){
-    selectedItem.value = selectedItem.value.filter(item => !items.includes(item))
-}
-
-
-function changeRoute(path:string) {
-    // change route, update tab
-    // clean filter
-    routerProvider?.updateProps({
-        idOrPath: path,
-        filter: {}
-    })
-}
-provide(BrowseListProviderKey,{
-    getchildApi:(pageParams:any) => {
-        return clientApi.api.postNuxeoDocumentChildrenThumbnailV2(pageParams)
-    },
-    idOrPath,
-    changeRoute,
-    addToSelection,
-    removeFromSelection,
-    
-})
 
 
 
@@ -53,13 +19,8 @@ provide(BrowseListProviderKey,{
 <template>
         
     <div class="pageContainer" >
-        <BrowseListTable ref="tableRef" >
-            <template #toolbar_buttons>
-                <div class="toolsBarContainer">
-                    <BrowseBreadcrumb :idOrPath="idOrPath" />
-                </div>
-            </template>
-        </BrowseListTable>
+        <BrowsePageComponent v-bind="props" >
+        </BrowsePageComponent>
     </div>
 </template>
 
