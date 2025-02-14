64e4d9acf4681f0b40ee7fc2985a40f686bc3c3a
oy <OY.Ou@weltronics.com>
2025-02-14 17:51:20 +0800

    BrowsePageComponent

--- pages/client-share/components/global/internalShare/mePageFolder.vue
+++ pages/client-share/components/global/internalShare/mePageFolder.vue
@@ -2,10 +2,21 @@
 const props = defineProps<{ 
     idOrPath: string ,
     filter: any,
+    title: string,
 }>();
 </script>
 <template>
-<BrowseListTable :idOrPath="idOrPath"></BrowseListTable>
+  <div class="pageContainer--padding">
+    <BrowsePageComponent :idOrPath="idOrPath">
+      <!-- <template #toolbar_buttons> 
+        <el-breadcrumb :separator-icon="ArrowRight">
+                <el-breadcrumb-item :to="{ path: backPath }">{{$t(title)}}</el-breadcrumb-item>
+                <el-breadcrumb-item v-for="(item,index) in state.breadcrumbs" :key="item.path"
+                    @click="goRoute(item.path, index === state.breadcrumbs.length - 1)">{{item.name}}</el-breadcrumb-item>
+            </el-breadcrumb>
+      </template> -->
+    </BrowsePageComponent>
+  </div>
 </template>
 <style lang="scss" scoped>
 </style>
