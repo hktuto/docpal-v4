3d2ae5abdd70474d668b768eb02e3a8a7cbfd914
sean tsang <9394wing@gmail.com>
2025-02-15 11:20:11 +0800

    add table to calendar setting

---
+++ pages/admin-calendar-setting/components/calendarSetting/categories/index.vue
@@ -0,0 +1,90 @@
+
+
+<script lang="ts" setup>
+import { adminApi } from 'api';
+const { setting, categoriesColumn } = useCalendarStore();
+
+const calendarProvider = inject(CalendarSettingKey);
+const addDialogRef = ref();
+
+const { tableConfig, tableEvent, tableRef, reload } = useVxeTable({
+    id: 'calendarSetting_categories',
+    api: (params:any) => adminApi.api.postMasterTablesRecordPage({
+                ...params,
+                id:setting.value.category.master_table
+            }),
+    pageSize: 5,
+    columns: categoriesColumn.value.map((item: any) => {
+        if(item.columnName === 'color'){
+            return {
+                field: item.columnName,
+                title: item.columnName,
+                type: 'html',
+                formatter: ({cellValue}) => {
+                    return `
+                        <div style="display:inline-block;border:1px solid black; margin-right:var(--app-space-xs);width:var(--app-space-m);height:var(--app-space-m);background-color: ${cellValue}"></div><span>${cellValue}</span>
+                    `
+                }
+            }
+        }
+        return {
+            field: item.columnName,
+            title: item.columnName,
+        }}
+    ),
+    bodyActions:[
+        [{
+            code: 'edit',
+            name: 'common_edit',
+            action: ({ row }: any) => {
+                addDialogRef.value.open(row)
+            },
+        }]
+    ]
+})
+console.log("tableConfig", tableConfig)
+
+onMounted(() => {
+    console.log(categoriesColumn.value)
+})
+
+function addRecord(){
+    addDialogRef.value.open()
+}
+
+
+
+
+
+</script>
+
+
+<template>
+<div class="section category">
+    <div class="title">{{  $t('calendarSetting.categories') }}</div>
+    <ElForm label-position="top" @submit.stop="">
+        <ElFormItem :label="$t('calendarSetting.location_masterTable')">
+            <ElSelect v-model="setting.category.master_table"  disabled></ElSelect>
+        </ElFormItem>
+    </ElForm>
+    <div class="categoriesContainer">
+        <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
+            <template #toolbar_buttons>
+                <ElButton type="primary" @click="addRecord">{{ $t("Add") }}</ElButton>
+            </template>
+        </VxeGrid>
+    </div>
+    <CalendarSettingCategoriesAddDialog ref="addDialogRef" @submit="reload" />
+</div>
+</template>
+
+<style lang="scss" scoped>
+.category{
+    width: 100%;
+}
+.categoriesContainer{
+    width: 100%;
+    height: 300px;
+}
+</style>
+
