ecb6c468c163327d866854e0e63372b20092b0ae
oy <OY.Ou@weltronics.com>
2025-02-17 11:01:44 +0800

    me folder

--- pages/client-share/utils/routerHelper.ts
+++ pages/client-share/utils/routerHelper.ts
@@ -1,3 +1,14 @@
+export const routeShareMePage = function(params: any){
+  return {
+    id: "internal-share-me-page-versions-" + new Date().getTime(),
+    name: "internal-share-me-page-",
+    icon: 'icon-park-outline:document-folder',
+    label: 'file_share_me',
+    component: 'LazyInternalShareMePage',
+    props: {
+    }
+  } as TabItem
+}
 export const routeShareMePageFolder = function(params: any){
   return {
     id: "internal-share-me-page-folder-versions-" + new Date().getTime(),
@@ -7,7 +18,20 @@ export const routeShareMePageFolder = function(params: any){
     component: 'LazyInternalShareMePageFolder',
     props: {
       id: params.id,
-      idOrPath: params.documentIds
+      idOrPath: params.documentIds,
+      homeId: params.documentIds
+    }
+  } as TabItem
+}
+
+export const routeShareOtherPage = function(params: any){
+  return {
+    id: "internal-share-other-page-versions-" + new Date().getTime(),
+    name: "internal-share-other-page-",
+    icon: 'icon-park-outline:document-folder',
+    label: 'file_share_other',
+    component: 'LazyInternalShareOtherPage',
+    props: {
     }
   } as TabItem
 }
