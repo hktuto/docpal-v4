54a2ff97cf28644c419126185c7d6fd8e58ec539
sean tsang <9394wing@gmail.com>
2025-02-17 21:21:29 +0800

    setup update data workflow

---
+++ packages/bpmn/components/global/bpmn/context/updateData/condition/index.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+const condition = defineModel<any>('condition', {required: true})
+
+const emits = defineEmits(['remove'])
+
+const conditionTypeOption = [
+    'Update_Number',
+    'Look_Up_User_Group',
+    'Look_Up_User',
+    'Look_Up_Master_Table',
+    'Look_Up_Case_info'
+]
+
+function remove() {
+    emits('remove')
+}
+
+</script>
+
+<template>
+    <div class="cardContainer">
+        <div class="delete">
+            <Icon name="lucide:trash" class="el-icon--left" @click="remove" />
+        </div>
+        <ElForm :model="condition" label-position="top" width="100%">
+            <ElFormItem label="Condition Type">
+                <ElSelect v-model="condition.attr_type" placeholder="请选择">
+                    <ElOption v-for="item in conditionTypeOption" :key="item" :label="item" :value="item" />
+                </ElSelect>
+            </ElFormItem>
+        </ElForm>
+        <template v-if="condition.attr_type === 'Update_Number'">
+            {{condition}}
+        </template>
+        <template v-else-if="condition.attr_type === 'Look_Up_User_Group'">
+        </template>
+        <template v-else-if="condition.attr_type === 'Look_Up_User'">
+        </template>
+        <template v-else-if="condition.attr_type === 'Look_Up_Master_Table'">
+        </template>
+        <template v-else-if="condition.attr_type === 'Look_Up_Case_info'">  
+        </template>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+.cardContainer{
+    width: 100%;
+    padding: var(--app-space-s);
+    border-radius: var(--app-border-radius-m);
+    border: 1px solid var(--app-grey-825);
+}
+</style>
