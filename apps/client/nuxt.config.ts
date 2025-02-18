8ac0e7c8e2e20fced7190909e51053b49d6273c9
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-02-18 14:19:20 +0800

    [UPDATE] 更新Trash頁面的UI與邏輯

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,42 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  ssr:false,
-  modules: ['@nuxt/test-utils/module'],
-  extends:[
-    "../../packages/icon",
-    '../../packages/authApp',
-    "../../pages/client-browse",
-    "../../pages/client-search",
-    "../../pages/client-master-table",
-    "../../pages/client-folder-cabinet",
-    "../../pages/client-workflow",
-    "../../packages/dp-dashboard",
-    "../../pages/client-share"
-  ],
-  features:{
-    inlineStyles: true,
-  },
-  runtimeConfig:{
-    public:{
-      plarform:'client'
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    ssr: false,
+    modules: ['@nuxt/test-utils/module'],
+    extends: [
+        "../../packages/icon",
+        '../../packages/authApp',
+        "../../pages/client-browse",
+        "../../pages/client-search",
+        "../../pages/client-master-table",
+        "../../pages/client-folder-cabinet",
+        "../../pages/client-workflow",
+        "../../packages/dp-dashboard",
+        "../../pages/client-share",
+        "../../pages/client-trash"
+    ],
+    features: {
+        inlineStyles: true,
+    },
+    runtimeConfig: {
+        public: {
+            plarform: 'client'
+        }
+    },
+    nitro: {
+        devProxy: {
+            '/public-api/report/v1/api': {
+                target: process.env.DASHBOARD_PROXY,
+                changeOrigin: true,
+                prependPath: true
+            },
+            '/api': {
+                target: process.env.CLIENT_PROXY,
+                changeOrigin: true,
+                prependPath: true
+            },
+
+        }
     }
-  },
-  nitro:{
-    devProxy:{
-      '/public-api/report/v1/api':{
-        target: process.env.DASHBOARD_PROXY,
-        changeOrigin: true,
-        prependPath: true
-      },
-      '/api':{
-        target: process.env.CLIENT_PROXY,
-        changeOrigin: true,
-        prependPath: true
-      },
-      
-    }
-  }
-  
+
 })
