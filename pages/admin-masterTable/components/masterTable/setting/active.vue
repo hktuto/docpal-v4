57272d220ba575fe055cc35f3c7e2e2eb572974d
oy <OY.Ou@weltronics.com>
2025-02-18 15:37:18 +0800

    [admin-master-table]data keep-alive tableId

--- pages/admin-masterTable/components/masterTable/setting/active.vue
+++ pages/admin-masterTable/components/masterTable/setting/active.vue
@@ -1,42 +1,45 @@
 <template>
-<el-card>
-    <h3 class="title">{{ $t('master.setting.active') }}</h3>
-    <div class="description">{{ $t('master.setting.activeDescription') }}</div>
+  <el-card>
+    <h3 class="title">{{ $t("master.setting.active") }}</h3>
+    <div class="description">{{ $t("master.setting.activeDescription") }}</div>
     <el-select v-model="state.isActive" placeholder="Select">
-      <el-option :label="$t('actions.active')" value="A"/>
-      <el-option :label="$t('actions.inactive')" value="D"/>
+      <el-option :label="$t('actions.active')" value="A" />
+      <el-option :label="$t('actions.inactive')" value="D" />
     </el-select>
-    <el-button type="primary" :loading="state.loading" @click="handleSave">{{ $t('common_save') }}</el-button>
-</el-card>
+    <el-button type="primary" :loading="state.loading" @click="handleSave">{{
+      $t("common_save")
+    }}</el-button>
+  </el-card>
 </template>
 <script setup lang="ts">
-import { adminApi } from 'api';
-const props = defineProps(['table'])
+import { adminApi } from "api";
+const props = defineProps(["table", "tableId"]);
 const state = reactive<any>({
-    isActive: 'D',
-    loading: false
-})
+  isActive: "D",
+  loading: false,
+});
 async function handleSave() {
-    state.loading = true
-    try {
-        await adminApi.api.putMasterTables({
-            id: props.table.id,
-            status: state.isActive,
-
-        })
-    } catch (error) {
-        state.isActive = state.isActive === 'A' ? 'D' : 'A'
-    }
-    setTimeout(() => state.loading = false, 500)
+  state.loading = true;
+  try {
+    await adminApi.api.putMasterTables({
+      id: props.tableId,
+      status: state.isActive,
+    });
+  } catch (error) {
+    state.isActive = state.isActive === "A" ? "D" : "A";
+  }
+  setTimeout(() => (state.loading = false), 500);
 }
-watch(() => props.table,(newVal) => {
-    state.isActive = props.table.status
-})
+watch(
+  () => props.table,
+  (newVal) => {
+    state.isActive = props.table.status;
+  }
+);
 </script>
 <style lang="scss" scoped>
 .el-select {
-    width: 100%;
-    padding: var(--app-space-xs) 0;
+  width: 100%;
+  padding: var(--app-space-xs) 0;
 }
 </style>
-    
