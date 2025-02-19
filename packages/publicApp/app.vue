2f55469367bb54c8c3191b7053a5e90d639722d0
oy <OY.Ou@weltronics.com>
2025-02-19 10:01:23 +0800

    style

--- packages/publicApp/app.vue
+++ packages/publicApp/app.vue
@@ -1,32 +1,29 @@
 <script lang="ts" setup>
-import { getLocale } from '~/utils/loading'
-const loadState:string = useLoadState()
-initPublicLayout()
-
+import { getLocale } from "~/utils/loading";
+const loadState: string = useLoadState();
+initPublicLayout();
 </script>
 
 <template>
-<div class="pageContainer--padding">
+  <div class="pageContainer--padding">
     <template v-if="loadState === 'Ready'" class="main-container">
-        <NuxtPage />
+      <NuxtPage />
     </template>
-    <div v-else style="height: 100%;width: 100%;">
+    <div v-else style="height: 100%; width: 100%">
       <LoadingBg></LoadingBg>
       <div class="loadState-container">
         <div>
           <img src="/logoWithName.png" />
-          <h1>{{loadState}}</h1>
+          <h1>{{ loadState }}</h1>
         </div>
       </div>
+    </div>
   </div>
-</div>
-
 </template>
-
 
 <style scoped lang="scss">
 .pageContainer--padding {
-    height: 100vh
+  height: 100vh;
 }
 
 .loadState-container {
@@ -35,7 +32,7 @@ initPublicLayout()
   overflow: hidden;
   display: flex;
   align-items: center;
-  justify-content: center;   
+  justify-content: center;
   div:first-child {
     font-size: 28px;
     width: 40%;
@@ -60,12 +57,6 @@ initPublicLayout()
       z-index: -1;
       background-color: rgba(255, 255, 255, 0.6);
     }
-  } 
-}
-.logoContainer{
-    // padding: var(--menu-item-padding);
-    --icon-size: calc(var(--icon-font-size) + (var(--menu-item-padding) * 2));
+  }
 }
 </style>
-
-
