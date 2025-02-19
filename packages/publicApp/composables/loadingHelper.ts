0f9702a6d98b1b1597ed19c4b5ae56c5d01ac2e4
oy <OY.Ou@weltronics.com>
2025-02-19 09:10:25 +0800

    [public-share]

--- packages/publicApp/composables/loadingHelper.ts
+++ packages/publicApp/composables/loadingHelper.ts
@@ -3,42 +3,51 @@ import zhCN from 'vxe-table/lib/locale/lang/zh-CN'
 import enUS from 'vxe-table/lib/locale/lang/en-US'
 import zhHK from 'vxe-table/lib/locale/lang/zh-HK'
 export const useLoadState = () => useState<string>(() => 'Init');
-export const initPublicLayout = async() => {
-    const loadState = useLoadState()
-    loadState.value = await getLocale()
-    setTimeout(() => {
-        loadState.value = 'Ready'
-    }, 3000)
+export const initPublicLayout = async () => {
+  const loadState = useLoadState()
+
+  // load language
+  const navigatorLanguage = navigator.language
+  let resultLanguage = 'en-US'
+  if (navigatorLanguage === 'zh-HK') resultLanguage = 'zh-HK'
+  else if (navigatorLanguage.includes('zh')) resultLanguage = 'zh-CN'
+  loadState.value = await getLocale(resultLanguage)
+
+  setTimeout(() => {
+    loadState.value = 'Ready'
+  }, 3000)
 }
-export async function getLocale(){
-  const { locale, availableLocales, setLocaleMessage } = useI18n()
-  await Promise.all( availableLocales.map( async(code) => {
-          const vxeLang = code === 'zh-CN' ? zhCN : code === 'en-US' ? enUS : zhHK
-          const { data:clientData } = await clientApi.api.getRelationQuerylanguage({
-                  locale:code, 
-                  languageKey: 'client'
-              }) as any
-          const clientJson = JSON.parse(clientData[0].languageContent)
+export async function getLocale(curLocale: string = 'en-US') {
+  // @ts-ignore
+  const { locale, availableLocales, setLocaleMessage, setLocale } = useI18n()
+  await Promise.all(availableLocales.map(async (code) => {
+    const vxeLang = code === 'zh-CN' ? zhCN : code === 'en-US' ? enUS : zhHK
+    const { data: clientData } = await clientApi.api.getRelationQuerylanguage({
+      locale: code,
+      languageKey: 'client'
+    }) as any
+    const clientJson = JSON.parse(clientData[0].languageContent)
 
-          const { data:adminData } = await clientApi.api.getRelationQuerylanguage({
-                  locale:code, 
-                  languageKey: 'admin'
-              }) as any
-          const adminJson = JSON.parse(adminData[0].languageContent)
+    const { data: adminData } = await clientApi.api.getRelationQuerylanguage({
+      locale: code,
+      languageKey: 'admin'
+    }) as any
+    const adminJson = JSON.parse(adminData[0].languageContent)
 
-          const { data:metaData } = await clientApi.api.getRelationQuerylanguage({
-                  locale:code, 
-                  languageKey: 'meta'
-              }) as any
-          const metaJson = JSON.parse(metaData[0].languageContent)
-          setLocaleMessage(code, {
-              ...clientJson,
-              ...adminJson,
-              ...metaJson,
-              ...vxeLang
-          })
-          
-      })
+    const { data: metaData } = await clientApi.api.getRelationQuerylanguage({
+      locale: code,
+      languageKey: 'meta'
+    }) as any
+    const metaJson = JSON.parse(metaData[0].languageContent)
+    setLocaleMessage(code, {
+      ...clientJson,
+      ...adminJson,
+      ...metaJson,
+      ...vxeLang
+    })
+
+  })
   )
+  setLocale(curLocale)
   return 'Language ready'
 }
--- pages/public-share/pages/share.vue
+++ pages/public-share/pages/share.vue
@@ -1,7 +1,105 @@
-<script lang="ts" setup>
-</script>
 <template>
-<div>div</div>
+  <div class="pageContent" v-if="shareState">
+    <template v-if="shareState === 'list'">
+      <main v-loading="state.loading">
+        <ShareTable ref="tableRef" :tableData="state.shareList">
+          <template #toolbar_buttons>
+            <h3 class="title">{{ $t("share_shareFiles") }}</h3>
+            <el-button @click="getData()"> {{ $t("refresh") }} </el-button>
+          </template>
+        </ShareTable>
+      </main>
+    </template>
+    <template v-if="shareState === 'expired'">
+      <main class="expired">{{ $t("share_shareExpired") }}</main>
+    </template>
+  </div>
+  <SharePassword v-else @submit="handleGetPublicDocument"></SharePassword>
 </template>
+
+<script lang="ts" setup>
+import { clientApi } from "api";
+import { ElMessage, ElMessageBox } from "element-plus";
+const route = useRoute();
+const state = reactive({
+  shareState: "",
+  shareList: [],
+  loading: false,
+});
+const { shareState, shareList } = toRefs(state);
+function getData() {
+  const password = sessionStorage.getItem("sharePWD");
+  if (password) handleGetPublicDocument({ password });
+}
+const tableRef = ref();
+async function handleGetPublicDocument(formData) {
+  try {
+    state.loading = true;
+    formData.token = route.query.token;
+    if (!formData.token) throw new Error(`${$i18n.t("responseMsg_errorCode_2")}`);
+    const res = await clientApi.api
+      .getNuxeoPublicDocument(formData)
+      .then((res) => res.data);
+    if (res.errorCode) {
+      switch (res.errorCode) {
+        case 10:
+          shareState.value = "expired";
+          return;
+        default:
+          throw new Error(res.message);
+      }
+      sessionStorage.removeItem("sharePWD");
+    } else if (res instanceof Array) {
+      state.shareList = [];
+      sessionStorage.setItem("sharePWD", formData.password);
+      state.shareList.push(...res);
+      state.shareState = "list";
+      setTimeout(() => {
+        tableRef.value?.loadData(state.shareList);
+      }, 10);
+    } else {
+      sessionStorage.removeItem("sharePWD");
+    }
+  } catch (error) {
+    state.shareState = "";
+    // ElMessage.error(error?.response?.data?.message || error.message)
+  }
+  state.loading = false;
+}
+onMounted(() => {
+  getData();
+});
+</script>
+
 <style lang="scss" scoped>
+.pageContent {
+  height: 100%;
+  overflow: hidden;
+}
+main {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  padding: var(--app-padding) calc(var(--app-padding) * 3) 1rem;
+}
+.expired {
+  padding-top: 10%;
+  text-align: center;
+  font-size: 3rem;
+}
+
+.title {
+  font-size: 1.4rem;
+  line-height: 1.2;
+  font-weight: 700;
+  white-space: nowrap;
+  color: var(--primary-color);
+  padding: 1rem calc(var(--app-padding) * 3);
+  background: var(--header-bg);
+  margin: unset;
+}
+:deep(.vxe-buttons--wrapper) {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
