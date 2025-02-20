4cc3e07c83b60a2a701b7bba88910923ed9f9960
oy <OY.Ou@weltronics.com>
2025-02-20 13:32:40 +0800

    [dashboard-work-panel]

--- packages/dp-dashboard/components/global/personal/search/history.vue
+++ packages/dp-dashboard/components/global/personal/search/history.vue
@@ -1,37 +1,47 @@
 <template>
   <el-card ref="cardRef" class="dashboard-item dashboard-item-card">
     <template #header="{ close, titleId, titleClass }">
-      <h4>{{$t('search.recentSearchs')}}</h4>
-      <Icon v-show="!hideSetting" name="material-symbols:delete-rounded" class="normal cursor-pointer"  @click="handleDelete"></Icon>
+      <h4>{{ $t("search.recentSearchs") }}</h4>
+      <Icon
+        v-show="!hideSetting"
+        name="material-symbols:delete-rounded"
+        class="normal cursor-pointer"
+        @click="handleDelete"
+      ></Icon>
     </template>
     <el-card v-if="!hideSetting" v-for="item in 2" :key="item" class="detail">
       <el-skeleton :rows="5" />
     </el-card>
-    <!-- <SearchGroupBarRecentSearchList  v-else ref="listRef" @search="handleSearch"></SearchGroupBarRecentSearchList> -->
-
+    <SearchGroupBarRecentSearchList
+      v-else
+      ref="listRef"
+      @search="handleSearch"
+    ></SearchGroupBarRecentSearchList>
   </el-card>
 </template>
 <script lang="ts" setup>
-import { ElMessageBox } from 'element-plus'
+import { ElMessageBox } from "element-plus";
 const emits = defineEmits(["setSearchParams", "delete"]);
-const { t } = useI18n()
+const { t } = useI18n();
 
-const props = withDefaults( defineProps<{
+const props = withDefaults(
+  defineProps<{
     dates?: any;
     setting?: any;
-    hideSetting?: boolean,
-}>() , {
+    hideSetting?: boolean;
+  }>(),
+  {
     setting: {},
-    hideSetting: false
-})
+    hideSetting: false,
+  }
+);
 const {
   public: { endPoint },
 } = useRuntimeConfig();
 
-function resize() {
-}
+function resize() {}
 function handleSearch(data: any) {
-  sessionStorage.setItem('searchParams', JSON.stringify(data))
+  sessionStorage.setItem("searchParams", JSON.stringify(data));
   // router.push('/searchGroup')
 }
 async function handleDelete() {
@@ -40,7 +50,7 @@ async function handleDelete() {
   emits("delete");
 }
 defineExpose({
-  resize
+  resize,
 });
 </script>
 <style lang="scss" scoped>
@@ -57,12 +67,11 @@ defineExpose({
     background-color: var(--app-primary-color);
     color: #fff;
   }
-  
 }
 .center {
-    text-align: center;
-    padding: var(--app-space-xs);
-  }
+  text-align: center;
+  padding: var(--app-space-xs);
+}
 .el-tag {
   max-width: 100%;
   overflow: hidden;
--- packages/dp-search/components/searchGroup/bar/recentSearch/list.vue
+++ packages/dp-search/components/searchGroup/bar/recentSearch/list.vue
@@ -1,75 +1,96 @@
 <template>
-<div class="list">
-  <div v-infinite-scroll="getList" 
-    :infinite-scroll-disabled="state.scrollNoMore || state.loading" 
-    infinite-scroll-distance="3"
-    :infinite-scroll-immediate="false"
-    class="list-scroll">
-    <el-card v-for="(item,index) in state.list" :key="index" @dblclick="handleSearch(item)">
-      <template v-for="(q,qIndex) in item.searchRequest.query" :key="'q' + qIndex">
-        <div class="search-child">
-          <template v-for="(c,cIndex) in q.matchs" :key="'qc' + qIndex">
-            <div class="search-child-child">
-              <el-tag class="el-icon--left el-tag--ellipsis" type="info" size="small" effect="dark">
-                {{c.queryType}}：
-                <b>{{c.value}}</b>
-              </el-tag>
-            </div>
-            <span v-if="cIndex !== q.matchs.length - 1">{{ $t(`logic.${q.condition}`)  }}</span>
-          </template>
-        </div>
-        <span v-if="qIndex !== item.searchRequest.query.length - 1">{{ $t(`logic.${item.searchRequest.condition}`)  }}</span>
-      </template>
-      <div>{{$t('search.result')}}: {{item.totalSize}}</div>
-    </el-card>
-    <div v-if="state.loading" class="flex-x-center">{{ $t('dpTip.loading') }}</div>
-    <div v-if="state.scrollNoMore" class="flex-x-center">{{ $t('dpTip.noMore') }}</div>
+  <div class="list">
+    <div
+      v-infinite-scroll="getList"
+      :infinite-scroll-disabled="state.scrollNoMore || state.loading"
+      infinite-scroll-distance="3"
+      :infinite-scroll-immediate="false"
+      class="list-scroll"
+    >
+      <el-card
+        v-for="(item, index) in state.list"
+        :key="index"
+        @dblclick="handleSearch(item)"
+      >
+        <template v-for="(q, qIndex) in item.searchRequest.query" :key="'q' + qIndex">
+          <div class="search-child">
+            <template v-for="(c, cIndex) in q.matchs" :key="'qc' + qIndex">
+              <div class="search-child-child">
+                <el-tag
+                  class="el-icon--left el-tag--ellipsis"
+                  type="info"
+                  size="small"
+                  effect="dark"
+                >
+                  {{ c.queryType }}：
+                  <b>{{ c.value }}</b>
+                </el-tag>
+              </div>
+              <span v-if="cIndex !== q.matchs.length - 1">{{
+                $t(`logic.${q.condition}`)
+              }}</span>
+            </template>
+          </div>
+          <span v-if="qIndex !== item.searchRequest.query.length - 1">{{
+            $t(`logic.${item.searchRequest.condition}`)
+          }}</span>
+        </template>
+        <div>{{ $t("search.result") }}: {{ item.totalSize }}</div>
+      </el-card>
+      <div v-if="state.loading" class="flex-x-center">{{ $t("dpTip.loading") }}</div>
+      <div v-if="state.scrollNoMore" class="flex-x-center">{{ $t("dpTip.noMore") }}</div>
+    </div>
   </div>
-</div>
 </template>
 <script lang="ts" setup>
-import { clientApi } from 'api'
-import { conditionDecorators } from '~/utils/searchFormHelper'
+import { clientApi } from "api";
+import { conditionDecorators } from "~/utils/searchFormHelper";
 const state = reactive<any>({
   list: [],
   scrollNoMore: false,
-  loading: false
-})
-const emits = defineEmits(['search'])
+  loading: false,
+});
+const emits = defineEmits(["search"]);
 const pageParams = reactive({
-    pageNum: -1,
-    pageSize: 10
-})
+  pageNum: -1,
+  pageSize: 10,
+});
 function handleSearch(item: any) {
-  const data = { ...item.searchRequest }
-  conditionDecorators(data)
-  emits('search', data)
+  const data = { ...item.searchRequest };
+  conditionDecorators(data);
+  emits("search", data);
 }
 async function getList() {
+  if (state.loading) return;
   try {
-    pageParams.pageNum ++
-    state.loading = true
-    const {data:res} = await clientApi.api.postLogsRecentSearchPageV2(pageParams) as any
-    console.log(res)
-    state.list.push(...res.entryList)
-    state.scrollNoMore = state.list.length >= res.totalSize
+    pageParams.pageNum++;
+    state.loading = true;
+    const { data: res } = (await clientApi.api.postLogsRecentSearchPageV2(
+      pageParams
+    )) as any;
+    console.log(res);
+    state.list.push(...res.entryList);
+    state.scrollNoMore = state.list.length >= res.totalSize;
   } catch (error) {
-    console.log("get recent error", error)
+    console.log("get recent error", error);
   } finally {
-    state.loading = false
+    state.loading = false;
   }
 }
-function initList(){
-  state.list = []
-  pageParams.pageNum = -1
-  getList()
+function initList() {
+  state.list = [];
+  pageParams.pageNum = -1;
+  getList();
 }
 onActivated(() => {
-  initList()
-})
+  initList();
+});
+onMounted(() => {
+  initList();
+});
 defineExpose({
-  initList
-})
+  initList,
+});
 </script>
 <style lang="scss" scoped>
 .list {
