5ad4a6f2806da911927002f075eca8ef42ac2794
sean tsang <9394wing@gmail.com>
2025-02-20 13:26:53 +0800

    handle 404 page

--- packages/authApp/pages/[...all].vue
+++ packages/authApp/pages/[...all].vue
@@ -1,3 +1,11 @@
 <template>
-    catch all , return to index
+    <div></div>
 </template>
+
+
+<script lang="ts" setup>
+const router =useRouter()
+    onMounted(() => {
+        router.push('/')
+    })
+</script>
