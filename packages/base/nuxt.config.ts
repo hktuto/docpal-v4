787f4457492cc4bc5069c0820f29cc77645e47a2
sean tsang <9394wing@gmail.com>
2025-02-20 13:45:23 +0800

    tem show log

--- packages/base/nuxt.config.ts
+++ packages/base/nuxt.config.ts
@@ -115,10 +115,10 @@ const config = {
         },
       },
     },
-    esbuild: {
-      drop: ['debugger'],
-      pure: ['console.log', 'console.error', 'console.warn', 'console.debug', 'console.trace'],
-    },
+    // esbuild: {
+    //   drop: ['debugger'],
+    //   pure: ['console.log', 'console.error', 'console.warn', 'console.debug', 'console.trace'],
+    // },
     vue: {
       features: {
         propsDestructure: true,
