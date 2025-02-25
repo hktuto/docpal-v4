5b7cbe7e0438f17708034d35c6cf0110e258e17d
Sean Tsang <9394wing@gmail.com>
2025-02-25 18:18:32 +0800

    add global

--- apps/client/pages/[...all].vue
+++ apps/client/pages/[...all].vue
@@ -1,12 +1,36 @@
 <template>
-    <div></div>
+    <div>
+        loading
+    </div>
 </template>
 
 
 <script lang="ts" setup>
 // TODO : add custom router control
+const route = useRoute()
 const router =useRouter()
-    onMounted(() => {
-        router.push('/')
-    })
+onMounted(() => {
+    // step1 normalize route path by removing trailing slash
+    const path = route.path.replace(/\/$/, '')
+    switch(path){
+        case '/browse':
+            const path = ((route.query.id || route.query.path) || '/') as string
+            const newTab = createBrowseListPageParams({
+                idOrPath: decodeURI(path)
+            })
+            console.log("newTab", newTab)
+            router.push({
+                path: '/',
+                query: {
+                    navigateTab: btoa(JSON.stringify(newTab))
+                }
+            })
+            break;
+        case '/workflow':
+            // TODO: add workflow page
+            break;
+        default:
+            router.push('/')
+    }
+})
 </script>
