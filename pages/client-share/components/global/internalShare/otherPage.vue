2d4a02a315c878f41e57e66f4dc98f9948935160
oy <OY.Ou@weltronics.com>
2025-02-26 13:34:51 +0800

    [share]rightclick

--- pages/client-share/components/global/internalShare/otherPage.vue
+++ pages/client-share/components/global/internalShare/otherPage.vue
@@ -101,26 +101,272 @@ const {
       },
     },
   ],
+  // bodyActions: [
+  //   [
+  //     {
+  //       code: "preview",
+  //       name: t("common_preview"),
+  //       visible: true,
+  //       disabled: false,
+  //       action: ({ row }: any) => {
+  //         handleDblclick(row);
+  //       },
+  //     },
+  //   ],
+  // ],
   bodyActions: [
     [
       {
-        code: "preview",
-        name: t("common_preview"),
-        visible: true,
-        disabled: false,
+        code: "docOpen",
+        name: "common_open",
         action: ({ row }: any) => {
           handleDblclick(row);
         },
       },
+      {
+        code: "docActionAddFolder",
+        name: "filePopover_newFolder",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionAddFolder", {
+            detail: initToDocument(row),
+          });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionNewFile",
+        name: "filePopover_newFile",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionNewFile", { detail: initToDocument(row) });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionUploadFile",
+        name: "filePopover_uploadFile",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionUploadFile", {
+            detail: initToDocument(row),
+          });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionUploadFolder",
+        name: "filePopover_uploadFolder",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionUploadFolder", {
+            detail: initToDocument(row),
+          });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionRename",
+        name: "filePopover_rename",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionRename", { detail: initToDocument(row) });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionChangeDocType",
+        name: "filePopover_changeDocType",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionChangeDocType", {
+            detail: initToDocument(row),
+          });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docWatermark",
+        name: "filePopover_watermark",
+        action: async ({ row }: any) => {
+          const detail = await clientApi.api
+            .postNuxeoDocument({ idOrPath: row.documentIds })
+            .then((res) => res.data);
+          const ev = new CustomEvent("docWatermark", { detail: detail });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionInternalShare",
+        name: "filePopover_internalShare",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionInternalShare", {
+            detail: initToDocument(row),
+          });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionDelete",
+        name: "filePopover_delete",
+        action: ({ row }: any) => {
+          const ev = new CustomEvent("docActionDelete", { detail: initToDocument(row) });
+          document.dispatchEvent(ev);
+        },
+      },
+      {
+        code: "docActionRefresh",
+        name: "common_refresh",
+        action: ({ row }: any) => {
+          reload();
+        },
+      },
+      {
+        code: "docActionNewTab",
+        name: "rightClick.newTab",
+        action: ({ row }: any) => {
+          if (row.isFolder) {
+            const link = createBrowseListPageParams({
+              idOrPath: row.documentIds,
+            });
+            routerProvider?.navigateTo(link, true);
+            return;
+          }
+          const detailLink = createDetailPageParams({
+            idOrPath: row.documentIds,
+            docName: row.documentNames,
+            showHeaderAction: true,
+          });
+          routerProvider?.navigateTo(detailLink, true);
+        },
+      },
+      {
+        code: "docActionDownload",
+        name: "rightClick.download",
+        action: ({ row }: any) => {
+          downloadHandler(initToDocument(row));
+        },
+      },
     ],
   ],
+  additionalPermission: async ({ row }: any) => {
+    const userId = useUserId();
+    const permission = await getPermission(row.documentIds, userId.value);
+    return permission;
+  },
+  permissionMethod: ({ options, code, column, row, rowIndex, additionalData }: any) => {
+    // if click on empty row, return empty
+    if (!row) {
+      if (code === "docActionRefresh") {
+        return {
+          visible: true,
+          disabled: false,
+        };
+      }
+      if (code === "docActionPaste") {
+        return {
+          visible:
+            AllowTo({ feature: "ReadWrite", permission: additionalData }) &&
+            copyDocumentList.value.length > 0,
+          disabled: false,
+        };
+      }
+      const otherPublicAction = [
+        "docActionAddFolder",
+        "docActionNewFile",
+        "docActionUploadFile",
+        "docActionUploadFolder",
+      ];
+      if (otherPublicAction.includes(code)) {
+        return {
+          visible: AllowTo({ feature: "ReadWrite", permission: additionalData }),
+          disabled: false,
+        };
+      }
+      return {
+        visible: false,
+        disabled: false,
+      };
+    }
+    const publicActionsCode = ["docActionRefresh", "docActionNewTab", "docOpen"];
+    if (publicActionsCode.includes(code)) {
+      return { visible: true, disabled: false };
+    }
+    // hide all action when click on temp file
+    if (row.source === "tempFile") {
+      return { visible: false, disabled: false };
+    }
+    // need other permissiion check list
+    if (code === "docActionPaste") {
+      return {
+        visible:
+          AllowTo({ feature: "ReadWrite", permission: additionalData }) &&
+          copyDocumentList.value.length > 0,
+        disabled: false,
+      };
+    }
+    const actionThatFolderAndFileHave = [
+      "docActionRename",
+      "docActionInternalShare",
+      "docActionChangeDocType",
+      "docActionCopy",
+      "docActionCut",
+      "docActionPaste",
+      "docActionDelete",
+    ];
+    if (actionThatFolderAndFileHave.includes(code)) {
+      const ManageCode = ["docActionInternalShare"];
+      if (ManageCode.includes(code)) {
+        return {
+          visible: AllowTo({ feature: "ManageRecord", permission: additionalData }),
+        };
+      }
+      return {
+        visible: AllowTo({ feature: "ReadWrite", permission: additionalData }),
+      };
+    }
+    // get permission
+    const folderActionsCode = [
+      "docActionAddFolder",
+      "docActionNewFile",
+      "docActionUploadFile",
+      "docActionUploadFolder",
+    ];
+    // handle folder actions
+    if (folderActionsCode.includes(code)) {
+      return {
+        visible:
+          row.isFolder && AllowTo({ feature: "ReadWrite", permission: additionalData }),
+        disabled: false,
+      };
+    } else {
+      return {
+        visible:
+          !row.isFolder && AllowTo({ feature: "ReadWrite", permission: additionalData }),
+        disabled: false,
+      };
+    }
+    return {
+      visible: false,
+      disabled: false,
+    };
+  },
   dblClickAction: ({ row, column, event }: any) => {
     handleDblclick(row);
   },
 });
 function handleDblclick(row: any) {
   if (row.isFolder) {
-    routerProvider?.navigateTo(routeShareOtherPageFolder(row), false);
+    routerProvider?.navigateTo(routeShareMePageFolder(row), false);
+  } else {
+    routerProvider?.navigateTo(
+      createDetailPageParams({
+        docName: row.documentNames,
+        idOrPath: row.documentIds,
+        showHeaderAction: true,
+      }),
+      false
+    );
+  }
+}
+function initToDocument(row: any) {
+  return {
+    id: row.documentIds,
+    name: row.documentNames
   }
 }
 </script>
