6f1605a4720a275b7275378c69aab9375df1763f
oy <OY.Ou@weltronics.com>
2025-02-26 14:48:46 +0800

    [browse download]会报错SyntaxError: Must be called at the top of a `setup` function

--- packages/base/utils/browseHelper.ts
+++ packages/base/utils/browseHelper.ts
@@ -212,10 +212,9 @@ async function DownloadDocApi(idOrPath: string, cb?: Function) {
     }
   })
 }
-
 export async function downloadFileHandler(doc: any) {
 
-  const { t } = useI18n()
+  // const { t } = useI18n() // 会报错SyntaxError: Must be called at the top of a `setup` function
   // exportFolderStructureApi
   const id = new Date().valueOf() + doc.name
   const notification = ElNotification({
@@ -237,12 +236,14 @@ export async function downloadFileHandler(doc: any) {
     await downloadBlob(blob, doc.name)
     // await DownloadDocApi(props.doc.id)
   } catch (error: any) {
-    ElMessage.error(t('download_noFile') as string)
+    // @ts-ignore
+    ElMessage.error($i18n.t('download_noFile') as string)
   }
   setTimeout(() => {
     notification.close()
   }, 3000)
 }
+
 
 export function allowFeature(f: string) {
   const features = useFeature().value
