c37b85cb28f6d61143f0b02be59c02e103893cde
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-03-04 16:28:24 +0800

    [UPDATE] 更新Document - Template i18n文本

--- pages/admin-document-template/components/template/addStep1Dialog.vue
+++ pages/admin-document-template/components/template/addStep1Dialog.vue
@@ -1,22 +1,24 @@
 <template>
-<el-dialog 
-    class="scroll-dialog"
-    v-model="state.visible" :title="state.isEdit ? $t('template.editInfo') : $t('template.create')"
-    :close-on-click-modal="false" append-to-body
+    <el-dialog
+        class="scroll-dialog"
+        v-model="state.visible" :title="state.isEdit ? $t('template.editInfo') : $t('documentTemplate_Create')"
+        :close-on-click-modal="false" append-to-body
     >
-    <FormRenderer ref="FormRendererRef" :form-json="formJson">
-    </FormRenderer>
-    <template #footer>
-        <!-- <el-button @click="createFile('Excel', 'test')"></el-button> -->
-        <el-button type="primary" :loading="state.loading" @click="handleSubmit">{{$t('common_submit')}}</el-button>
-    </template>
-</el-dialog>
-<TemplateAddStep2Dialog ref="TemplateAddStep2DialogRef"/>
+        <FormRenderer ref="FormRendererRef" :form-json="formJson">
+        </FormRenderer>
+        <template #footer>
+            <!-- <el-button @click="createFile('Excel', 'test')"></el-button> -->
+            <el-button type="primary" :loading="state.loading" @click="handleSubmit">{{ $t('common_submit') }}
+            </el-button>
+        </template>
+    </el-dialog>
+    <TemplateAddStep2Dialog ref="TemplateAddStep2DialogRef"/>
 </template>
 <script lang="ts" setup>
-import { adminApi } from 'api'
+import {adminApi} from 'api'
 import {ExtensionMap} from "~/utils/documentTemplateHelper";
 import formJson from './templateAddStep1.vform.json'
+
 const emits = defineEmits([
     'update'
 ])
@@ -26,35 +28,37 @@ const state = reactive({
     setting: {},
     isEdit: false
 })
-
+const {t} = useI18n()
 const routerProvider = inject(MenuRouterKey)
 const form = reactive({
     labelRule: []
 })
 const FormRendererRef = ref()
 const TemplateAddStep2DialogRef = ref()
+
 async function handleSubmit() {
     const formData = await FormRendererRef.value.vFormRenderRef.getFormData()
-    if(!formData) return
+    if (!formData) return
     try {
         state.loading = true
-        if(state.isEdit) {
+        if (state.isEdit) {
             await adminApi.api.putTemplateDocument({
                 name: formData.name,
                 description: formData.description,
                 id: state.setting.id,
                 fileType: state.setting.fileType
             })
+            routerProvider?.message.success(t('documentTemplate_updateSuccessMsg'));
             emits('update')
-        }
-        else {
+        } else {
             const file = await createFile(formData.type, formData.name)
             const params = new FormData()
             params.append('file', file)
             params.append('name', formData.name)
             params.append('fileType', formData.type)
             params.append('description', formData.description)
-            const { data } = await adminApi.api.postTemplateDocument({requestDTO:{}},params as any) as any
+            const {data} = await adminApi.api.postTemplateDocument({requestDTO: {}}, params as any) as any
+            routerProvider?.message.success(t('documentTemplate_createdSuccessMsg'));
             const templateInfo = data
             const link = createNewDocumentTemplateDetail({
                 id: templateInfo.id,
@@ -62,7 +66,7 @@ async function handleSubmit() {
                 item: templateInfo
             })
             routerProvider?.navigateTo(link)
-            // TemplateAddStep2DialogRef.value.handleOpen(templateInfo)
+            TemplateAddStep2DialogRef.value.handleOpen(templateInfo)
         }
         state.visible = false
     } catch (error) {
@@ -73,9 +77,9 @@ async function handleSubmit() {
 
 async function handleOpen(setting?) {
     state.visible = true
-    setTimeout(async() => {
+    setTimeout(async () => {
         await FormRendererRef.value.vFormRenderRef.resetForm()
-        if(setting && setting.isEdit) {
+        if (setting && setting.isEdit) {
             state.isEdit = true
             state.setting = setting
             await FormRendererRef.value.vFormRenderRef.setFormData({...state.setting})
@@ -86,13 +90,15 @@ async function handleOpen(setting?) {
         }
     })
 }
+
 async function createFile(fileType: 'Word' | 'Excel' | 'PPT' | 'PDF', name: string) {
     const path = `/docTemplate/template${ExtensionMap[fileType]}`
     const file = await fetch(path)
     const fileArrayBuffer = await file.arrayBuffer()
-    return new File([fileArrayBuffer], `${name}${ExtensionMap[fileType]}`,{type: ExtensionMimeTypeMap[fileType]})
+    return new File([fileArrayBuffer], `${name}${ExtensionMap[fileType]}`, {type: ExtensionMimeTypeMap[fileType]})
 }
-defineExpose({ handleOpen })
+
+defineExpose({handleOpen})
 </script>
 <style lang="scss" scoped>
 
