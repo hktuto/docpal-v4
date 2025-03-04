4147f48e4a5bb7496d59377ae8d8eb0a5d0876d9
Seantsang <9394wing@gmail.com>
2025-03-04 13:56:14 +0800

    update case

--- packages/calendar/components/global/calendar/workflowViewer.vue
+++ packages/calendar/components/global/calendar/workflowViewer.vue
@@ -1,13 +1,31 @@
+
 <script lang="ts" setup>
 
-const {disabled, formData, options} = defineProps<{
+const {disabled, formData, options, taskDetail} = defineProps<{
     disabled: boolean,
     formData: any
     options?: Object,
+    taskDetail:any
 }>();
 
+const filter = ref()
+
+function filterChange(newFilter: any) {
+    console.log("filterChange", newFilter)
+    filter.value = newFilter
+}
+
 function getFormData(){
-    console.log("getFormData")
+    // console.log("getFormData", formData)
+}
+
+function createEvent(){
+
+}
+const newEventFromRef = ref();
+function popNewEvent(dateTime: string) {
+    console.log("popNewEvent", dateTime)
+    newEventFromRef.value.open(dateTime, filter.value)
 }
 
 defineExpose({ getFormData })
@@ -15,6 +33,8 @@ defineExpose({ getFormData })
 
 <template>
     {{options}}
+    {{ taskDetail }}
     {{formData}}
-    <CalendarViewer  />
+    <CalendarViewer :options="options" @newEvent="popNewEvent" @filter-change="filterChange"/>
+    <CalendarNewEventForm ref="newEventFromRef" @submit="createEvent"/>
 </template>
