efb428ea7138fed5e657751f6fe9b3b712f29b08
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-03-04 17:54:33 +0800

    [UPDATE] 更新Email - Template  i18n文本

--- pages/admin-email-template/components/global/emailTemplate/detail.vue
+++ pages/admin-email-template/components/global/emailTemplate/detail.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-import { ElMessage } from "element-plus";
-import { adminApi } from "api";
+import {ElMessage} from "element-plus";
+import {adminApi} from "api";
+
 const routerProvider = inject(MenuRouterKey)
-const { t } = useI18n()
-const { id } = defineProps<{
-  id: string;
+const {t} = useI18n()
+const {id} = defineProps<{
+    id: string;
 }>();
 const editInfoOpened = ref(false);
 const testEmailOpened = ref(false);
@@ -18,32 +19,33 @@ const infoFormEl = ref();
 const showClose = ref(true);
 
 const layoutHtml = computed(() => {
-  if (!selectedLayout.value || !layouts.value) return null;
-  return layouts.value.find((item) => item.id === selectedLayout.value).layoutContent;
+    if (!selectedLayout.value || !layouts.value) return null;
+    return layouts.value.find((item) => item.id === selectedLayout.value).layoutContent;
 });
 let data: any = ref(null)
-async function handleInit() {
-  // TODO : if id is new , create new dummy data
-  if (!id || id === "new") {
-    ready.value = true;
-    await getTemplateLayout("");
-    editInfoOpened.value = true;
-    showClose.value = true;
-    return {
-      subject: "new template",
-      body: "",
-      emailLayoutId: "",
-      emailTemplateJson: "",
-      emailTemplateVariable: "",
-    };
-  }
-  const res = await adminApi.api.getTemplateEmailTemplateId(id).then((res) => res.data);
-  // loop template body and get all variables
-  // const body = res?.body;
-  await getTemplateLayout(res?.emailLayoutId);
 
-  ready.value = true;
-  return res;
+async function handleInit() {
+    // TODO : if id is new , create new dummy data
+    if (!id || id === "new") {
+        ready.value = true;
+        await getTemplateLayout("");
+        editInfoOpened.value = true;
+        showClose.value = true;
+        return {
+            subject: "new template",
+            body: "",
+            emailLayoutId: "",
+            emailTemplateJson: "",
+            emailTemplateVariable: "",
+        };
+    }
+    const res = await adminApi.api.getTemplateEmailTemplateId(id).then((res) => res.data);
+    // loop template body and get all variables
+    // const body = res?.body;
+    await getTemplateLayout(res?.emailLayoutId);
+
+    ready.value = true;
+    return res;
 }
 
 /**
@@ -55,217 +57,237 @@ async function handleInit() {
  * @param templateId
  */
 async function getTemplateLayout(templateId?: any) {
-  const res = await adminApi.api
-    .postTemplateEmailLayoutPage({ pageNum: 0, pageSize: 1000 })
-    .then((res) => res.data);
-  layouts.value = res?.entryList;
-  selectedLayout.value = templateId || layouts.value[0].id;
-  // selectedLayout.value = templateId || entryList[0].id;
+    const res = await adminApi.api
+        .postTemplateEmailLayoutPage({pageNum: 0, pageSize: 1000})
+        .then((res) => res.data);
+    layouts.value = res?.entryList;
+    selectedLayout.value = templateId || layouts.value[0].id;
+    // selectedLayout.value = templateId || entryList[0].id;
 }
 
 function handleClose() {
-  if(id === 'new') {
-    routerProvider?.navigateTo(routeLayoutTemplatePage())
-  }
+    if (id === 'new') {
+        routerProvider?.navigateTo(routeLayoutTemplatePage())
+    }
 }
 
 /**
  *  儲存
  */
 async function save() {
-  const { html, json, variable } = await editorEl.value.getData();
-  // if id is new , create new
-  // check form valid
+    const {html, json, variable} = await editorEl.value.getData();
+    // if id is new , create new
+    // check form valid
 
-  if (id === "new") {
-    if (infoFormEl.value) {
-      const valid = await infoFormEl.value.validate();
-      if (!valid) return;
+    if (id === "new") {
+        if (infoFormEl.value) {
+            const valid = await infoFormEl.value.validate();
+            if (!valid) return;
+        }
+        const result = await adminApi.api.postTemplateEmailTemplate({
+            ...data.value,
+            // TODO : send html to body
+            // url encode html
+            body: html,
+            emailLayoutId: selectedLayout.value,
+            emailTemplateJson: JSON.stringify(json),
+            emailTemplateVariable: JSON.stringify(variable),
+        }).then(res => res.data)
+        if (result?.id) {
+            routerProvider?.updateProps({
+                label: result.id,
+                id: result.id
+            })
+        }
+        // router.push(`/emailTemplate/${result.id}`);
+        routerProvider?.message.success(t('emailContentTemplate_createdSuccessMsg', {name: "aa"}));
+        editInfoOpened.value = false;
+        showClose.value = true;
+        // TODO : add notification
+        return;
     }
-    const result = await adminApi.api.postTemplateEmailTemplate({
-      ...data.value,
-      // TODO : send html to body
-      // url encode html
-      body: html,
-      emailLayoutId: selectedLayout.value,
-      emailTemplateJson: JSON.stringify(json),
-      emailTemplateVariable: JSON.stringify(variable),
-    }).then(res => res.data)
-    if(result?.id) {
-        routerProvider?.updateProps({
-          label: result.id,
-          id: result.id
-        })
-    }
-    // router.push(`/emailTemplate/${result.id}`);
+    // update new variable
+    // test save json to backend
+    await adminApi.api.putTemplateEmailTemplate({
+        id: id,
+        ...data.value,
+        // TODO : send html to body
+        // url encode html
+        body: html,
+        emailLayoutId: selectedLayout.value,
+        emailTemplateJson: JSON.stringify(json),
+        emailTemplateVariable: JSON.stringify(variable),
+    });
+    ElMessage.success(t('dpMsg_success'))
     editInfoOpened.value = false;
-    showClose.value = true;
     // TODO : add notification
-    return;
-  }
-  // update new variable
-  // test save json to backend
-  await adminApi.api.putTemplateEmailTemplate({
-    id: id,
-    ...data.value,
-    // TODO : send html to body
-    // url encode html
-    body: html,
-    emailLayoutId: selectedLayout.value,
-    emailTemplateJson: JSON.stringify(json),
-    emailTemplateVariable: JSON.stringify(variable),
-  });
-  ElMessage.success(t('dpMsg_success'))
-  editInfoOpened.value = false;
-  // TODO : add notification
 }
+
 function handleEdit() {
-  editInfoOpened.value = true;
-  showClose.value = true;
+    editInfoOpened.value = true;
+    showClose.value = true;
 }
+
 /**
  *  送出測試信
  */
 async function sendTest() {
-  testEmailDialog.value.send();
-  // TODO : add notification
+    testEmailDialog.value.send();
+    // TODO : add notification
 }
+
 onActivated(async () => {
-  data.value = await handleInit()
+    data.value = await handleInit()
 })
 </script>
 <template>
-  <div class="pageContainer--padding">
-    <Editorjs v-if="data" ref="editorEl" :data="data" :layout="layoutHtml">
-      <template #name>
-        <div class="editButton">
-          <SvgIcon :src="'/icons/edit.svg'" @click="handleEdit" />
-        </div>
-      </template>
-      <template #action>
-        <ElSelect type="primary" v-model="selectedLayout">
-          <ElOption
-            v-for="item in layouts"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id"
-          ></ElOption>
-        </ElSelect>
-        <ElButton type="primary" size="small" @click="testEmailOpened = true">{{
-          $t("email_send_test")
-        }}</ElButton>
-        <ElButton type="primary" size="small" @click="save">{{
-          $t("common_save")
-        }}</ElButton>
-      </template>
-    </Editorjs>
+    <div class="pageContainer--padding">
+        <Editorjs v-if="data" ref="editorEl" :data="data" :layout="layoutHtml">
+            <template #name>
+                <div class="editButton">
+                    <SvgIcon :src="'/icons/edit.svg'" @click="handleEdit"/>
+                </div>
+            </template>
+            <template #action>
+                <ElSelect type="primary" v-model="selectedLayout">
+                    <ElOption
+                        v-for="item in layouts"
+                        :key="item.id"
+                        :label="item.name"
+                        :value="item.id"
+                    ></ElOption>
+                </ElSelect>
+                <ElButton type="primary" size="small" @click="testEmailOpened = true">{{
+                        $t("email_send_test")
+                    }}
+                </ElButton>
+                <ElButton type="primary" size="small" @click="save">{{
+                        $t("common_save")
+                    }}
+                </ElButton>
+            </template>
+        </Editorjs>
 
-    <ElDialog
-      ref="editInfoDialog"
-      v-model="editInfoOpened"
-      append-to-body
-      destroy-on-close
-      :close-on-press-escape="showClose"
-      :close-on-click-modal="showClose"
-      :show-close="showClose"
-      @closed="handleClose"
-    >
-      <EditorjsInfoForm v-if="data" ref="infoFormEl" :data="data" />
-      <template #footer>
-        <!-- <ElButton v-if="!showClose" type="primary" @click="$router.back()">{{
-        $t("common_back")
-      }}</ElButton> -->
-        <ElButton type="primary" @click="save">{{ $t("common_save") }}</ElButton>
-      </template>
-    </ElDialog>
-    <ElDialog v-model="testEmailOpened" append-to-body destroy-on-close>
-      <EditorjsTestDialog
-        ref="testEmailDialog"
-        v-if="data"
-        :data="data"
-        :variables="variables"
-      />
-      <template #footer>
-        <ElButton
-          type="primary"
-          @click="
+        <ElDialog
+            ref="editInfoDialog"
+            v-model="editInfoOpened"
+            append-to-body
+            destroy-on-close
+            :close-on-press-escape="showClose"
+            :close-on-click-modal="showClose"
+            :show-close="showClose"
+            @closed="handleClose"
+            :title="t('emailContentTemplate_create')"
+        >
+            <EditorjsInfoForm v-if="data" ref="infoFormEl" :data="data"/>
+            <template #footer>
+                <!-- <ElButton v-if="!showClose" type="primary" @click="$router.back()">{{
+                $t("common_back")
+              }}</ElButton> -->
+                <ElButton type="primary" @click="save">{{ $t("common_submit") }}</ElButton>
+            </template>
+        </ElDialog>
+        <ElDialog v-model="testEmailOpened" append-to-body destroy-on-close>
+            <EditorjsTestDialog
+                ref="testEmailDialog"
+                v-if="data"
+                :data="data"
+                :variables="variables"
+            />
+            <template #footer>
+                <ElButton
+                    type="primary"
+                    @click="
             () => {
               sendTest();
               testEmailOpened = false;
             }
           "
-          >{{ $t("email_send_test") }}</ElButton
-        >
-      </template>
-    </ElDialog>
-  </div>
+                >{{ $t("email_send_test") }}
+                </ElButton
+                >
+            </template>
+        </ElDialog>
+    </div>
 </template>
 <style lang="scss" scoped>
 .actions {
-  display: grid;
-  grid-template-columns: 1fr min-content;
-  gap: var(--app-space-xs);
-  margin-bottom: 20px;
-  .name {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: flex-start;
-    align-items: center;
+    display: grid;
+    grid-template-columns: 1fr min-content;
     gap: var(--app-space-xs);
-    flex: 1 0 auto;
-    word-break: break-all;
-    span {
-      font-size: 1.2rem;
-      font-weight: bold;
+    margin-bottom: 20px;
+
+    .name {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        gap: var(--app-space-xs);
+        flex: 1 0 auto;
+        word-break: break-all;
+
+        span {
+            font-size: 1.2rem;
+            font-weight: bold;
+        }
     }
-  }
-  .action {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: flex-start;
-    align-items: center;
-    gap: var(--app-space-xs);
-    .el-select {
-      width: 200px;
+
+    .action {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: center;
+        gap: var(--app-space-xs);
+
+        .el-select {
+            width: 200px;
+        }
+
+        > * {
+            margin: 0;
+        }
     }
-    > * {
-      margin: 0;
-    }
-  }
 }
+
 .email_content {
-  flex: 1 0 auto;
-  position: relative;
-  &.desktop {
-    width: 100%;
-  }
-  &.mobile {
-    width: 375px;
-    margin: 0 auto;
-  }
-}
-.emailTemplateContainer {
-  height: 100%;
-}
-.responsiveSizeEditorContainer {
-  position: absolute;
-  left: calc(50% - 31px);
-  top: -15px;
-  width: 62px;
-  height: 30px;
-  background-color: #fff;
-  z-index: 2;
-  border: 1px solid #eee;
-  border-radius: 8px;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  --icon-color: var(--app-grey-600);
-  .desktop,
-  .mobile {
-    cursor: pointer;
-    &.active {
-      --icon-color: var(--app-primary-color);
+    flex: 1 0 auto;
+    position: relative;
+
+    &.desktop {
+        width: 100%;
     }
-  }
+
+    &.mobile {
+        width: 375px;
+        margin: 0 auto;
+    }
+}
+
+.emailTemplateContainer {
+    height: 100%;
+}
+
+.responsiveSizeEditorContainer {
+    position: absolute;
+    left: calc(50% - 31px);
+    top: -15px;
+    width: 62px;
+    height: 30px;
+    background-color: #fff;
+    z-index: 2;
+    border: 1px solid #eee;
+    border-radius: 8px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    --icon-color: var(--app-grey-600);
+
+    .desktop,
+    .mobile {
+        cursor: pointer;
+
+        &.active {
+            --icon-color: var(--app-primary-color);
+        }
+    }
 }
 </style>
