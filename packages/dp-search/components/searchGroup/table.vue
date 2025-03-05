b4415a1b74446962d9e8a309f006a07234588c8b
oy <OY.Ou@weltronics.com>
2025-03-05 15:14:31 +0800

    style

--- packages/dp-search/components/searchGroup/table.vue
+++ packages/dp-search/components/searchGroup/table.vue
@@ -25,7 +25,7 @@
                   </el-icon>
               </div>
               <template v-if="row.properties.summarys.length <= 1 || row.expandSummary">
-                  <div v-for="item in row.properties.summarys" class="summaryItem">
+                  <div v-for="item in row.properties.summarys" class="summaryItem" :title="`${item.summaryKey}:${item.summaryValue}`">
                       [{{item.summaryKey}}]: <b>{{item.summaryValue}}</b>
                   </div>
               </template>
@@ -294,6 +294,8 @@ defineExpose({ initBar, initAgg, initSearch })
   margin-bottom: var(--app-space-xs);
   color: var(--app-grey-000);
   border-radius: 4px;
+  overflow: hidden;
+    text-overflow: ellipsis;
   b {
 
   }
