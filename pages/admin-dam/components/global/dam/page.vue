cadbf2e91358d77cc3ae39206b6d7bebd793c734
oy <OY.Ou@weltronics.com>
2025-03-10 13:51:39 +0800

    [DAM]api fix

--- pages/admin-dam/components/global/dam/page.vue
+++ pages/admin-dam/components/global/dam/page.vue
@@ -1,77 +1,78 @@
 <script lang="ts" setup>
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { adminApi } from 'api'
-import {DamListTable} from '#components'
+import { ElMessage, ElMessageBox } from "element-plus";
+import { adminApi } from "api";
+import { DamListTable } from "#components";
 // deepCopy, GetDocDetailApi
 
 // #region  tree
-const tabProvider = inject(TabManagerKey)
-const routerProvider = inject(MenuRouterKey)
-if(!tabProvider || !routerProvider) {
-    throw new Error('MenuRouterKey is not provided')
+const tabProvider = inject(TabManagerKey);
+const routerProvider = inject(MenuRouterKey);
+if (!tabProvider || !routerProvider) {
+  throw new Error("MenuRouterKey is not provided");
 }
 // #endregion
 const tableRef = ref<InstanceType<typeof DamListTable>>();
 const tableData = ref([]);
 const filteredData = ref<any[]>();
-const { t } = useI18n()
-function handleKeywordFilter(data:any) {
-    console.log("handleKeywordFilter", data)
+const { t } = useI18n();
+function handleKeywordFilter(data: any) {
+  console.log("handleKeywordFilter", data);
 }
 
-
-async function handleDelete (tag:any) {
-    ElMessageBox.confirm(`${t('msg_confirmWhetherToDelete')}`)
-    .then(async() => {
-        await adminApi.api.postDamDeletesettings([tag.id])
-        tableRef.value?.reload()
-    })
-    
+async function handleDelete(tag: any) {
+  const action = await ElMessageBox.confirm(`${t("msg_confirmWhetherToDelete")}`, {
+    confirmButtonText: `${t("dpButtom_confirm")}`,
+    cancelButtonText: `${t("dpButtom_cancel")}`,
+  }).catch((action: any) => {
+    return action;
+  });
+  if (action !== "confirm") return;
+  await adminApi.api.postDamDeletesettings([tag.id]);
+  refresh()
 }
 
-function handleDialog(data?:any) {
-    console.log("handleDialog", data)
-    DamDialogRef.value.handleOpen(deepCopy(data))
+function handleDialog(data?: any) {
+  DamDialogRef.value.handleOpen(deepCopy(data));
 }
 
 const DamDialogRef = ref();
-
+function refresh() {
+  tableRef.value?.reload();
+}
 provide(DamProviderKey, {
-    getListApi: async(params:any) =>{
-        console.log("getListApi", params, filteredData.value)
-        if(!filteredData.value ){
-            const { data:{list} } = await adminApi.api.postDamGetallsetting() as any
-            const data = mergeDataByKey(list, 'sourceType') as any
-           
-            tableData.value = data
-            filteredData.value = data
-            return data
-        }
-        return filteredData.value
-    },
-    handleDialog,
-    handleDelete
-})
+  getListApi: async (params: any) => {
+    console.log("getListApi", params, filteredData.value);
+    const {
+      data: { list },
+    } = (await adminApi.api.postDamGetallsetting()) as any;
+    const data = mergeDataByKey(list, "sourceType") as any;
 
+    tableData.value = data;
+    return data;
+  },
+  handleDialog,
+  handleDelete,
+});
 </script>
 
-
 <template>
-     <div class="pageContainer--padding">
-        <DamListTable ref="tableRef">
-            <template #toolbar_buttons>
-                <div class="filter-container">
-                    <KeywordFilter :list="tableData" attr="sourceType"
-                        @filter="handleKeywordFilter"></KeywordFilter>
-                    <el-button class="button-add" type="primary"
-                        @click="handleDialog()">{{$t('common_add')}}</el-button>
-                </div>
-            </template>
-        </DamListTable>
-        <DamDialog ref="DamDialogRef" @refresh="tableRef?.reload"></DamDialog>
-
-    </div>   
+  <div class="pageContainer--padding">
+    <DamListTable ref="tableRef">
+      <template #toolbar_buttons>
+        <div class="filter-container">
+          <KeywordFilter
+            :list="tableData"
+            attr="sourceType"
+            @filter="handleKeywordFilter"
+          ></KeywordFilter>
+          <el-button class="button-add" type="primary" @click="handleDialog()">{{
+            $t("common_add")
+          }}</el-button>
+        </div>
+      </template>
+    </DamListTable>
+    <DamDialog ref="DamDialogRef" @refresh="refresh"></DamDialog>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
