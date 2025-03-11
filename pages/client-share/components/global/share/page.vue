30f6f77dfb3b5df18e9a90e7039e9712b10ff245
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-03-11 14:41:03 +0800

    [UPDATE] 更新 Client - External Sharing i18n 語言包

--- pages/client-share/components/global/share/page.vue
+++ pages/client-share/components/global/share/page.vue
@@ -1,108 +1,116 @@
 <template>
-  <div class="pageContainer--padding">
-    <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
-    </VxeGrid>
-    <ShareDialog ref="shareInfoDialogRef" @submit="handleSubmit"></ShareDialog>
-  </div>
+    <div class="pageContainer--padding">
+        <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
+        </VxeGrid>
+        <ShareDialog ref="shareInfoDialogRef" @submit="handleSubmit"></ShareDialog>
+    </div>
 </template>
 <script lang="ts" setup>
-import { ElMessageBox } from 'element-plus'
-import { clientApi } from "api";
+import {ElMessage, ElMessageBox} from 'element-plus'
+import {clientApi} from "api";
 import dayjs from "dayjs";
 
-const { t } = useI18n();
+const {t} = useI18n();
 let extraParams: any = {};
 const {
-  tableConfig,
-  tableEvent,
-  tableRef,
-  query,
-  reload,
-  cleanSelectedRows,
+    tableConfig,
+    tableEvent,
+    tableRef,
+    query,
+    reload,
+    cleanSelectedRows,
 } = useVxeTable({
-  id: "c-share",
-  api: async(pageParams: any) => {
-    const params = {
-      ...pageParams, 
-      page: pageParams.pageNum, 
-      size: pageParams.pageSize,
-    }
-    delete params.pageNum
-    delete params.pageSize
-    const res: any = await clientApi.api.postNuxeoSharePage({  ...params, ...extraParams }).then(res => res.data)
-    return {
-      data: {
-        entryList: res.list,
-        totalSize: res.total
-      }
-    }
-  },
-  columns: [
-    { field: "emailList", title: "tableHeader_emailList", fixed: "left" },
-    { field: "documentSize", title: "tableHeader_numberOfFiles" },
-    {
-      field: "created",
-      title: "tableHeader_creationDate",
-      formatter({ cellValue }: any) {
-        return formatDate(cellValue)
-      },
+    id: "c-share",
+    api: async (pageParams: any) => {
+        const params = {
+            ...pageParams,
+            page: pageParams.pageNum,
+            size: pageParams.pageSize,
+        }
+        delete params.pageNum
+        delete params.pageSize
+        const res: any = await clientApi.api.postNuxeoSharePage({...params, ...extraParams}).then(res => res.data)
+        return {
+            data: {
+                entryList: res.list,
+                totalSize: res.total
+            }
+        }
     },
-    {
-      field: "expiredDate",
-      title: "tableHeader_dueDate",
-      formatter({ cellValue }: any) {
-        return formatDate(cellValue)
-      },
-    },
-  ],
-  bodyActions: [
-    [
-      {
-        code: "delete",
-        name: t("common_edit"),
-        visible: true,
-        disabled: false,
-        action: ({ row }: any) => {
-          handleDblclick(row);
+    columns: [
+        {field: "emailList", title: "tableHeader_emailList", fixed: "left"},
+        {field: "documentSize", title: "tableHeader_numberOfFiles"},
+        {
+            field: "created",
+            title: "externalSharing_creationDate",
+            formatter({cellValue}: any) {
+                return formatDate(cellValue)
+            },
         },
-      },
-      {
-        code: "delete",
-        name: t("common_delete"),
-        visible: true,
-        disabled: false,
-        action: ({ row }: any) => {
-          handleDisabled(row);
+        {
+            field: "expiredDate",
+            title: "externalSharing_expiryDate",
+            formatter({cellValue}: any) {
+                return formatDate(cellValue)
+            },
         },
-      },
     ],
-  ],
-  dblClickAction: ({ row, column, event }:any) => {
-    handleDblclick(row)
-  },
+    bodyActions: [
+        [
+            {
+                code: "delete",
+                name: t("externalSharing_edit"),
+                visible: true,
+                disabled: false,
+                action: ({row}: any) => {
+                    handleDblclick(row);
+                },
+            },
+            {
+                code: "delete",
+                name: t("externalSharing_delete"),
+                visible: true,
+                disabled: false,
+                action: ({row}: any) => {
+                    handleDisabled(row);
+                },
+            },
+        ],
+    ],
+    dblClickAction: ({row, column, event}: any) => {
+        handleDblclick(row)
+    },
 });
 
 async function handleDisabled(row: any) {
-  const action = await ElMessageBox.confirm(`${t('msg_confirmWhetherToDelete')}`)
-  if(action !== 'confirm') return
-  const param = [];
-  param.push(row.shareID);
-  await clientApi.api.deleteNuxeoShare(param);
-  query();
+    const action = await ElMessageBox.confirm(
+        `${t('externalSharing_deleteMsg')}`,
+        {
+            confirmButtonText: t('common_confirmDelete'),
+        }
+    )
+    if (action !== 'confirm') return
+    const param = [];
+    param.push(row.shareID);
+    await clientApi.api.deleteNuxeoShare(param);
+    ElMessage.success(t('externalSharing_deleteSuccessMsg'))
+    query();
 }
+
 const shareInfoDialogRef = ref()
 
-function handleDblclick (row: any) {
+function handleDblclick(row: any) {
     shareInfoDialogRef.value.handleOpen(row)
 }
-async function handleSubmit (shareInfo: any) {
-  await clientApi.api.patchNuxeoShare(shareInfo)
-  query()
+
+async function handleSubmit(shareInfo: any) {
+    await clientApi.api.patchNuxeoShare(shareInfo)
+    query()
 }
 
 </script>
 <style lang="scss" scoped>
 :deep .el-input {
-  width: 200px;
+    width: 200px;
 }
 </style>
