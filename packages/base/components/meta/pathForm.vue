e5960b5c20ff15c031b2dd31e911cc78bc3db862
Seantsang <9394wing@gmail.com>
2025-03-12 12:35:04 +0800

    [0001112] fix save as

---
+++ packages/base/components/meta/pathForm.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import formJson from './filePath.vform.json';
+const props = defineProps<{
+    defaultPath?: any
+}>()
+const formEl = ref()
+
+const state = reactive({
+    path: '',
+})
+
+function getData(){
+    return formEl.value.vFormRenderRef.getFormData()
+}
+
+onMounted(() => {
+    if(props.defaultPath) state.path = props.defaultPath
+    nextTick(() => {
+        formEl.value.vFormRenderRef.setFormData(state)
+    })
+})
+
+defineExpose({ getData })
+</script>
+
+<template>
+    <FormRenderer ref="formEl" :form-json="formJson">
+    </FormRenderer>
+</template>
