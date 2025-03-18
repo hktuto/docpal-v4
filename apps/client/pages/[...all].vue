343c14e14c3b4f2e21e3c5e2e54a1c8dd98e0369
oy <OY.Ou@weltronics.com>
2025-03-18 13:56:42 +0800

    [1138]DocPal 4.0 Testing - Internal sharing - Shared link not working

--- apps/client/pages/[...all].vue
+++ apps/client/pages/[...all].vue
@@ -1,53 +1,55 @@
 <template>
-    <div>
-        loading
-    </div>
+  <div>loading</div>
 </template>
 
-
 <script lang="ts" setup>
-import { clientApi } from '../../../libraries/api/src';
+import { clientApi } from "../../../libraries/api/src";
 
-import {ElMessage} from 'element-plus'
+import { ElMessage } from "element-plus";
 // TODO : add custom router control
-const route = useRoute()
-const router =useRouter()
-
-const hanldeWorkflow = async(processInstanceId:string) => {
-    try {
-       const taskList = await clientApi.api.getWorkflowTasks({processInstanceId}).then(res => res.data)
-        if (taskList && taskList.length > 0) {
-            const taskId = taskList[0].id
-            const newTab = routeWorkflowDetail({
-                id: taskId
-            })
-            localStorage.setItem('additionalPath', JSON.stringify(newTab))
-            router.push('/')
-        }
-    } catch (error) {
-        ElMessage.error('task no valid');
-    }
+const route = useRoute();
+const router = useRouter();
+function openTab(tabItem: any) {
+  let storageTabs = localStorage.getItem("app-tab");
+  let newLayout = [];
+  if (storageTabs) {
+    newLayout = JSON.parse(storageTabs);
+    tabItem.parent = newLayout[0].id;
+    // TODO : check if storageTabs is array, and handle restore other tabs
+    newLayout[0].tabs = [tabItem];
+  } else {
+    newLayout = [
+      {
+        id: "dummy-tab-container",
+        parent: "root",
+        showingTabIndex: 0,
+        size: 100,
+        tabs: [{ ...tabItem, parent: "dummy-tab-container" }],
+      },
+    ];
+  }
+  localStorage.setItem("app-tab", JSON.stringify(newLayout));
+  setTimeout(() => {
+    router.push("/");
+  });
 }
-onMounted(() => {
-    // step1 normalize route path by removing trailing slash
-    const path = route.path.replace(/\/$/, '')
-    console.log(path);
-    
-    switch(path){
-        case '/browse':
-            const path = ((route.query.id || route.query.path) || '/') as string
-            const newTab = createBrowseListPageParams({
-                idOrPath: decodeURI(path)
-            })
-            localStorage.setItem('additionalPath', JSON.stringify(newTab))
-            router.push('/')
-            break;
-        case '/workflow/link':
-            // TODO: add workflow page
-            hanldeWorkflow(route.query.processInstanceId as string)
-            break;
-        default:
-            router.push('/')
-    }
-})
+onMounted(async() => {
+  // step1 normalize route path by removing trailing slash
+  const path = route.path.replace(/\/$/, "");
+  switch (path) {
+    case "/browse":
+      const idOrPath = (route.query.id || route.query.path || "/") as string;
+      const newTab = createBrowseListPageParams({
+        idOrPath: decodeURI(idOrPath),
+      });
+      openTab(newTab);
+      break;
+    case "/workflow/link":
+      const workflowItem = await getWorkflowRoute(route.query.processInstanceId as string);
+      openTab(workflowItem);
+      break;
+    default:
+      router.push("/");
+  }
+});
 </script>
