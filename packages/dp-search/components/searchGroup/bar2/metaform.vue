3fbc139802c536189aacb37de581da2c441f25e7
oy <OY.Ou@weltronics.com>
2025-03-18 11:19:57 +0800

    [1143]	Search bug, the condition window hide and expend will show abnormal

--- packages/dp-search/components/searchGroup/bar2/metaform.vue
+++ packages/dp-search/components/searchGroup/bar2/metaform.vue
@@ -6,7 +6,7 @@
   size="small" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
   @change="handleChange"
 />
-<el-input v-else v-model="state.value" :placeholder="$t('tip.input')" clearable size="small" />
+<el-input v-else v-model="state.value" :placeholder="$t('tip.input')" clearable size="small" @change="handleChange" />
 </template>
 <script lang="ts" setup>
 const props = defineProps(['config'])
@@ -14,7 +14,7 @@ const emits = defineEmits(['formChange'])
 const state = reactive<any>({
   value: ''
 })
-function handleChange(value) {
+function handleChange(value: any) {
   let _value = value
   if(props.config.type === 'date') {
     const dateRange = {
@@ -26,7 +26,7 @@ function handleChange(value) {
   }
   emits('formChange', _value)
 }
-function setValue(value) {
+function setValue(value: any) {
   state.value = value
 }
 defineExpose({
