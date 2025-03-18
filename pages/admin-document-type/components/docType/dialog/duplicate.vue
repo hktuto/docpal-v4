d0b64d2d7d60243bb3bca61c590e3aea102da79d
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-03-18 17:47:29 +0800

    [BUGFIX] 修復接口被屏蔽

--- pages/admin-document-type/components/docType/dialog/duplicate.vue
+++ pages/admin-document-type/components/docType/dialog/duplicate.vue
@@ -1,20 +1,20 @@
 <template>
-    <el-dialog v-model="state.visible" :title="$t('docType.duplicate')"
-               class="scroll-dialog"
-               append-to-body
-               :close-on-click-modal="false"
-               @close="handleClose"
-    >
-        <FormRenderer ref="FormRendererRef" :form-json="formJson">
+  <el-dialog v-model="state.visible" :title="$t('docType.duplicate')"
+             class="scroll-dialog"
+             append-to-body
+             :close-on-click-modal="false"
+             @close="handleClose"
+  >
+    <FormRenderer ref="FormRendererRef" :form-json="formJson">
 
-        </FormRenderer>
-        <template #footer>
-            <div class="footer-grid">
-                <el-button type="primary" :loading="state.loading" @click="handleSubmit">{{ $t('common_submit') }}
-                </el-button>
-            </div>
-        </template>
-    </el-dialog>
+    </FormRenderer>
+    <template #footer>
+      <div class="footer-grid">
+        <el-button type="primary" :loading="state.loading" @click="handleSubmit">{{ $t('common_submit') }}
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <script lang="ts" setup>
 import {adminApi} from 'api';
@@ -22,42 +22,42 @@ import formJson from './duplicate.vform.json'
 import {ElMessage} from "element-plus";
 
 const emits = defineEmits([
-    'refresh', 'delete'
+  'refresh', 'delete'
 ])
 const {t} = useI18n()
 const state = reactive({
-    loading: false,
-    visible: false,
-    setting: {},
+  loading: false,
+  visible: false,
+  setting: {},
 })
 const FormRendererRef = ref()
 
 async function handleSubmit() {
-    const data = await FormRendererRef.value.vFormRenderRef.getFormData()
-    state.loading = true
-    try {
-        await adminApi.api.postDocpaltypeSettingsCopyName(data.fromName, {...data})
-        ElMessage.success(t('documentType_duplicateSuccessMsg'))
-        emits('refresh')
-        state.visible = false
-    } catch (error) {
-    } finally {
-        state.loading = false
-    }
+  const data = await FormRendererRef.value.vFormRenderRef.getFormData()
+  state.loading = true
+  try {
+    await adminApi.api.postDocpaltypeSettingsCopyName(data.fromName, {...data})
+    ElMessage.success(t('documentType_duplicateSuccessMsg'))
+    emits('refresh')
+    state.visible = false
+  } catch (error) {
+  } finally {
+    state.loading = false
+  }
 }
 
 function handleOpen(setting) {
-    state.visible = true
+  state.visible = true
 
-    setTimeout(async () => {
-        state.setting = setting
-        await FormRendererRef.value.vFormRenderRef.setFormData({
-            fromName: setting.name,
-            category: setting.category,
-            dataType: setting.dataType,
-        })
-        state.loading = false
+  setTimeout(async () => {
+    state.setting = setting
+    await FormRendererRef.value.vFormRenderRef.setFormData({
+      fromName: setting.name,
+      category: setting.category,
+      dataType: setting.dataType,
     })
+    state.loading = false
+  })
 }
 
 defineExpose({handleOpen})
