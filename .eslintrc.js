e9b77df958e9c7ce6b79240210c67dda4647a8a0
oy <OY.Ou@weltronics.com>
2025-03-25 14:07:28 +0800

    prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
+  ],
+  // add your custom rules here
+  //it is base on https://github.com/vuejs/eslint-config-vue
+  rules: {
+
+  }
+}
--- pages/admin-masterTable/components/global/masterTable/page.vue
+++ pages/admin-masterTable/components/global/masterTable/page.vue
@@ -1,65 +1,64 @@
 <script lang="ts" setup>
-import { adminApi } from 'api';
-import { MasterTableProviderKey } from '~/utils/masterTableProvider';
-import { routeMasterTableDetail, routeMasterTableNew } from '~/utils/routerHelper';
+import { adminApi } from 'api'
+import { MasterTableProviderKey } from '~/utils/masterTableProvider'
+import { routeMasterTableDetail, routeMasterTableNew } from '~/utils/routerHelper'
 const tabProvider = inject(TabManagerKey)
 const routerProvider = inject(MenuRouterKey)
-if( !routerProvider) {
-    throw new Error('MenuRouterKey is not provided')
+if (!routerProvider) {
+  throw new Error('MenuRouterKey is not provided')
 }
 const state = reactive<any>({
-  activeName: "tabels",
+  activeName: 'tabels',
   tableFilter: {},
   logFilter: {},
   masterTable: {
-    name: "",
-    fields: [],
-  },
-});
+    name: '',
+    fields: []
+  }
+})
 function openNew() {
   const newItem = routeMasterTableNew({
     name: $t('adminMenu.masterTable') + '/new'
-  });
-  console.log("newItem", newItem)
-  routerProvider?.navigateTo({...newItem})
+  })
+  console.log('newItem', newItem)
+  routerProvider?.navigateTo({ ...newItem })
 }
-function openDetail(row:any , openInNewTab = false){
-    const newItem = routeMasterTableDetail(row);
-    routerProvider?.navigateTo({...newItem}, openInNewTab)
+function openDetail(row: any, openInNewTab = false) {
+  const newItem = routeMasterTableDetail(row)
+  routerProvider?.navigateTo({ ...newItem }, openInNewTab)
 }
 const logRef = ref()
 const MasterTableTabRecordsRef = ref()
-function handleTableFilterChange(formModel:any) {
+function handleTableFilterChange(formModel: any) {
   state.tableFilter = formModel
   MasterTableTabRecordsRef.value.reload()
 }
-function handleLogFilterChange(formModel:any) {
+function handleLogFilterChange(formModel: any) {
   state.logFilter = formModel
   logRef.value.refresh()
 }
 provide(MasterTableProviderKey, {
-  GetMasterTablesPageApi: (params:any) => {
-    return adminApi.api.postMasterTablesPage({...params, ...state.tableFilter})
+  GetMasterTablesPageApi: (params: any) => {
+    return adminApi.api.postMasterTablesPage({ ...params, ...state.tableFilter })
   },
-  DeleteMasterTablesApi: (params:any) => {
+  DeleteMasterTablesApi: (params: any) => {
     return adminApi.api.deleteMasterTablesId(params)
   },
-  GetMasterTablesPageConditionApi: (params:any) => {
+  GetMasterTablesPageConditionApi: (params: any) => {
     return adminApi.api.getMasterTablesPageConditions(params)
   },
-  UpdateMasterTableApi: (params:any) => {
+  UpdateMasterTableApi: (params: any) => {
     return adminApi.api.putMasterTables(params)
   },
   openDetail,
   openNew
-}
-)
+})
 </script>
 <template>
   <div class="pageContainer--padding">
     <el-tabs class="dp-tabs--auto" v-model="state.activeName">
       <el-tab-pane :label="$t('masterTable.Tables')" name="tabels">
-        <MasterTableTabTables ref="MasterTableTabRecordsRef"  @filter-change="handleTableFilterChange"></MasterTableTabTables>
+        <MasterTableTabTables ref="MasterTableTabRecordsRef" @filter-change="handleTableFilterChange"> </MasterTableTabTables>
       </el-tab-pane>
       <el-tab-pane :label="$t('masterTable.log')" name="log">
         <MasterTableTabLog ref="logRef" @filter-change="handleLogFilterChange"></MasterTableTabLog>
@@ -70,6 +69,7 @@ provide(MasterTableProviderKey, {
 <style lang="scss" scoped>
 .dp-tabs--auto {
   height: 100%;
+
   .el-tab-pane {
     height: 100%;
   }
