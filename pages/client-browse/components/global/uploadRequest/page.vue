f75c4154a658245d8df79193d01a4fbb54087c4f
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-03-28 10:59:55 +0800

    [UPDATE] 添加篩選欄

--- pages/client-browse/components/global/uploadRequest/page.vue
+++ pages/client-browse/components/global/uploadRequest/page.vue
@@ -1,88 +1,95 @@
 <template>
   <div class="pageContainer--padding">
     <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
-      <template #toolbar_buttons> </template>
+      <template #toolbar_buttons>
+        <ResponsiveFilter
+          ref="ResponsiveFilterRef"
+          @form-change="handleFilterFormChange"
+        />
+      </template>
     </VxeGrid>
   </div>
 </template>
 <script lang="ts" setup>
-import { clientApi } from "api";
+import { clientApi } from 'api'
 
-import { createUploadRequestDetailParams, createBrowseListPageParams } from "../../../utils/browseMenuHelper";
-import { MenuRouterKey } from "#imports";
-const routerProvider = inject(MenuRouterKey);
-let extraParams = {};
-const { t } = useI18n();
+import { createBrowseListPageParams, createUploadRequestDetailParams } from '../../../utils/browseMenuHelper'
+import { MenuRouterKey } from '#imports'
+
+const ResponsiveFilterRef = ref()
+const routerProvider = inject(MenuRouterKey)
+let extraParams: any = {}
+const { t } = useI18n()
 const { tableConfig, tableEvent, tableRef, reload, query } = useVxeTable({
-  id: "clientUploadRequestList",
+  id: 'clientUploadRequestList',
   api: async (pageParams: any) => {
     const _pageParams = {
       ...pageParams,
       pageIndex: pageParams.pageNum,
-      pageSize: pageParams.pageSize,
-    };
-    delete pageParams.pageNum;
+      pageSize: pageParams.pageSize
+    }
+    delete pageParams.pageNum
     const { data } = await clientApi.api.getNuxeoFilerequest({
       ..._pageParams,
-      ...extraParams,
-    });
+      ...extraParams
+    })
     return {
       data: {
         entryList: data?.requests,
-        totalSize: data?.total,
-      },
-    };
+        totalSize: data?.total
+      }
+    }
   },
   columns: [
-    { field: "email", title: "user_email" },
-    { field: "logicalPath", title: "document_folderPath" },
-    { field: "message", title: "dpTable_message" },
+    { field: 'email', title: 'user_email' },
+    { field: 'logicalPath', title: 'document_folderPath' },
+    { field: 'message', title: 'dpTable_message' },
     {
-      field: "status",
-      title: "document_uploadStatus",
+      field: 'status',
+      title: 'document_uploadStatus',
       formatter({ cellValue }: any) {
-        if(cellValue === 'completed') return t(`pending_${cellValue}`);
-        return t(cellValue);
-      },
+        if (cellValue === 'completed') return t(`pending_${cellValue}`)
+        return t(cellValue)
+      }
     },
     {
-      field: "createdDate",
-      title: "document_uploadDate",
+      field: 'createdDate',
+      title: 'document_uploadDate',
       formatter({ cellValue }: any) {
         return formatDate(cellValue)
-      },
-    },
+      }
+    }
   ],
   bodyActions: [
     [
       {
-        code: "preview",
-        name: "common_preview",
+        code: 'preview',
+        name: 'common_preview',
         visible: true,
         disabled: false,
         action: ({ row }: any) => {
-          handleDblclick(row);
-        },
+          handleDblclick(row)
+        }
       },
       {
-        code: "toFolder",
-        name: "document_view",
+        code: 'toFolder',
+        name: 'document_view',
         visible: true,
         disabled: false,
         action: ({ row }: any) => {
           toFolder(row)
-        },
-      },
-    ],
+        }
+      }
+    ]
   ],
   permissionMethod: (args: PermissionMethodParams) => {
     // options 是 menuConfig 中的 body 配置
     switch (args.code) {
-      case "preview":
+      case 'preview':
         return {
-          visible: args.row.status === "pending_approval",
-          disabled: false,
-        };
+          visible: args.row.status === 'pending_approval',
+          disabled: false
+        }
 
       // case "toFolder":
       //   return {
@@ -92,40 +99,81 @@ const { tableConfig, tableEvent, tableRef, reload, query } = useVxeTable({
     }
     return {
       visible: true,
-      disabled: false,
-    };
+      disabled: false
+    }
   },
   dblClickAction: ({ row, column, event }: any) => {
-    handleDblclick(row);
+    handleDblclick(row)
   },
   optionalConfig: {
     cellClassName({ row, column }) {
       if (row.status === 'completed') return 'completed-row'
-      else if (row.status !== "pending_approval")  return "disabled-row";
-      return null;
-    },
-  },
-});
+      else if (row.status !== 'pending_approval') return 'disabled-row'
+      return null
+    }
+  }
+})
 
 function handleDblclick(row: any) {
-  if (row.status !== "pending_approval") return;
+  if (row.status !== 'pending_approval') return
   routerProvider?.navigateTo(
-    createUploadRequestDetailParams({ ...row, paramKey: "taskId" }),
+    createUploadRequestDetailParams({ ...row, paramKey: 'taskId' }),
     false
-  );
+  )
 }
+
 function toFolder(row: any) {
   routerProvider?.navigateTo(
     createBrowseListPageParams({ idOrPath: row.documentId }),
     false
-  );
+  )
 }
+
+function handleFilterFormChange(formModel: any) {
+  if (!formModel.isDesc) formModel.isDesc = true
+  if (!!formModel.isDesc) formModel.isDesc = formModel.isDesc !== 'false'
+  extraParams = formModel
+  reload()
+}
+
+function getFilter() {
+  const data = [
+    {
+      key: 'orderBy',
+      label: 'tableHeader.sortBy',
+      type: 'string',
+      isMultiple: false,
+      options: [
+        { label: 'user_email', value: 'email' },
+        { label: 'document_folderPath', value: 'logicalPath' },
+        { label: 'document_uploadStatus', value: 'status' },
+        { label: 'document_uploadDate', value: 'createdDate' }
+      ]
+    },
+    {
+      key: 'isDesc',
+      label: 'tableHeader.sortOrder',
+      type: 'string',
+      isMultiple: false,
+      options: [
+        { label: 'tableHeader.asc', value: false },
+        { label: 'tableHeader.desc', value: true }
+      ]
+    }
+  ]
+  ResponsiveFilterRef.value.init(data)
+}
+
+onMounted(() => {
+  // getFilter()
+})
 </script>
 
 <style lang="scss" scoped>
 :deep(.el-input) {
   width: 200px;
 }
+
 :deep(.disabled-row) {
   background-color: var(--app-grey-800);
   // color: var(--app-grey-900)
