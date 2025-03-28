919c0b13e54a88b54a4c622e9b803934208a50a1
Seantsang <9394wing@gmail.com>
2025-03-28 17:06:17 +0800

    add copy

--- pages/admin-case-management/components/caseManagement/saveAsDialog.vue
+++ pages/admin-case-management/components/caseManagement/saveAsDialog.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ElDialog } from 'element-plus';
 import { adminApi } from 'api';
+import formJson from './copy.vform.json'
 
 const opened  = ref(false)
 const loading = ref(false)
@@ -20,40 +21,47 @@ function close(){
     opened.value = false
     emits('close')
 }
-
-const rules = {
-    name: [
-        { required: true, message: '$t("workflowEditor.nameRequired")', trigger: 'blur' },
-        { min: 2, max: 50, message: '$t("workflowEditor.nameLengthLimit")', trigger: 'blur' },
-        { validator: validateName, trigger: 'blur' }
-    ]
-}
-
-function validateName(rule:any, value:string, callback:any) {
-    if(value === '') return callback(new Error('$t("workflowEditor.nameRequired")'))
-    if(value.length < 2) return callback(new Error('$t("workflowEditor.nameLengthLimit")'))
-    callback()
-}
+const FormRendererRef = ref()
 
 let versionList:any[] = [];
 async function getVersionList() {
     // get version list
     const response = await adminApi.api.postCaseTypesVersionPage({pageNum:0, pageSize:1000, caseTypeId: data.id || data.draftId})
-    versionList = response.data?.entryList || []
+    versionList = (response.data?.entryList || [])
+    const copy = FormRendererRef.value.vFormRenderRef.getWidgetRef('copyVersion')
+    copy.loadOptions(versionList.map(item => ({
+        label: item.versionNumber,
+        value: item.versionNumber
+    })))
+    const defaultValue = {
+      name: data.name + "_copy",
+      copyVersion: data.latestVersion
+    }
+    FormRendererRef.value.vFormRenderRef.setFormData(defaultValue)
 }
 
 async function open(){
-    await getVersionList()
-    // 因為 props 有可能未更新，所以在 nextTick 中再次設置 form 的值
-    nextTick(() => {
-        form.name = data.name
-        form.copyVersion = copyVersion
-        opened.value = true
-    })
+  opened.value = true;
+  nextTick(() => {
+     getVersionList()
+  }) 
 }
 
 async function save(){
     // TODO : handle save as logic
+    let formData = await FormRendererRef.value.vFormRenderRef.getFormData()
+    const versionId = versionList.find(item => item.versionNumber === formData.copyVersion).id
+    const params = {
+      caseIdPrefix: formData.caseIdPrefix,
+      caseIdDigit: formData.caseIdDigit,
+      startNumber: formData.startNumber,
+      name: formData.name,
+      versionId: versionId
+    }
+    const res = await adminApi.api.postCaseTypesIdCopy(data.id, params)
+    // TODO : copy form data
+    opened.value = false;
+    emits('close')
 }
 
 defineExpose({ open })
@@ -61,16 +69,7 @@ defineExpose({ open })
 
 <template>
 <ElDialog v-model="opened" append-to-body>
-        <el-form ref="formRef" v-loading="loading" :model="form" :rules="rules" label-position="top" class="demo-ruleForm" status-icon>
-            <el-form-item :label="$t('workflowEditor.name')" prop="name">
-                <el-input v-model="form.name" :placeholder="$t('workflowEditor.name')" />
-            </el-form-item>
-            <el-form-item :label="$t('workflowEditor.copyVersion')" prop="copyVersion">
-                <el-select v-model="form.copyVersion" placeholder="Select">
-                    <el-option v-for="item in versionList" :key="item.versionNumber" :label="item.versionNumber" :value="item.versionNumber" />
-                </el-select>
-            </el-form-item>
-        </el-form>
+  <FormRenderer ref="FormRendererRef" :form-json="formJson" />
     <template #footer>
             <el-button @click="close">{{$t('cancelText')}}</el-button>
             <el-button type="primary" @click="save">{{$t('common_save')}}</el-button>
