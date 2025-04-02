7379ac95e18a148dc04a978aed4a776d6a9ff085
Seantsang <9394wing@gmail.com>
2025-04-02 16:32:13 +0800

    fix update id when create empty

--- packages/dp-cmmn-x6/components/cmmn/sidePanel/draggable/dialog.vue
+++ packages/dp-cmmn-x6/components/cmmn/sidePanel/draggable/dialog.vue
@@ -57,8 +57,13 @@ const FormRendererRef = ref()
 // const formJson = getJsonApi('admin/adminAclForm.json')
 async function handleSubmit() {
   const data = await FormRendererRef.value.vFormRenderRef.getFormData()
-  if (state.isEdit) emits('edit', {...data})
-  else emits('create', {...data})
+  if (state.isEdit){ 
+    emits('edit', deepCopy(data))
+  } else{ 
+    data.id = data.name.replaceAll(' ', '_') + '_' + new Date().valueOf().toString()
+    console.log("data", deepCopy(data))
+    emits('create', deepCopy(data))
+  }
   state.visible = false
 }
 
