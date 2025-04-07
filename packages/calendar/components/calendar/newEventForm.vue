263c15d4d011f6bdbb98e38812deed7568803598
Seantsang <9394wing@gmail.com>
2025-04-07 18:26:37 +0800

    fix status ion create form

--- packages/calendar/components/calendar/newEventForm.vue
+++ packages/calendar/components/calendar/newEventForm.vue
@@ -40,7 +40,7 @@ async function getFilterOptions(){
   console.log("getFilterOptions", options.createUserFilter)
   if(options.createUserFilter){
     const res = await clientApi.api.getPermissionUserGroupGroupidUsers(options.createUserFilter).then(res => res.data)
-    user = res.users
+    user = res.users.filter(item => item.status)
     console.log("user", user)
   }else{
     user = await clientApi.api.postNuxeoIdentityUsers({}).then(res => res.data)
