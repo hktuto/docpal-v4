f36b22f92ba0dd536f399f0bdcd9df351c0e1e4b
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-04-09 17:36:54 +0800

    [UPDATE] 更新測試用例

---
+++ pages/client-share/test/shareAction.nuxt.spec.ts
@@ -0,0 +1,61 @@
+import { describe, it, expect, vi, beforeEach, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { SharePage,ShareDialog } from '#components'
+import { VxeGrid } from 'vxe-table'
+import { clientApi } from './mock/api'
+import { ElMessageBox, ElMessage } from 'element-plus'
+
+// 模擬 Element Plus
+vi.mock('element-plus', () => ({
+  ElMessageBox: {
+    confirm: vi.fn(() => Promise.resolve('confirm'))
+  },
+  ElMessage: {
+    success: vi.fn()
+  }
+}))
+
+describe('ShareAction', () => {
+  let shareInfoDialogRef: any
+  let wrapper: any
+  beforeEach(() => {
+    vi.clearAllMocks()
+    shareInfoDialogRef = ref({
+      handleOpen: vi.fn()
+    })
+
+    wrapper = mount(SharePage, {
+      global: {
+        components: { VxeGrid },
+        mocks: {
+          $t: (msg: string) => msg,
+          $i18n: { t: (key: string) => key }
+        }
+      }
+    })
+  })
+
+  it('should call handleOpen with the correct', async () => {
+    const mockOpen = vi.fn()
+    wrapper.vm.shareInfoDialogRef = { handleOpen: mockOpen }
+    const handleOpenSpy = vi.spyOn(wrapper.vm.shareInfoDialogRef, 'handleOpen')
+    await wrapper.vm.handleDblclick()
+    expect(handleOpenSpy).toHaveBeenCalled()
+  })
+
+  it('should call handleDisabled with the correct', async () => {
+    const row = { id: 1, shareID: 1 }
+    await wrapper.vm.handleDisabled(row)
+    expect(ElMessageBox.confirm).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.any(Object)
+    )
+
+    expect(clientApi.api.deleteNuxeoShare).toHaveBeenCalled()
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    expect(ElMessage.success).toHaveBeenCalled()
+  })
+
+})
