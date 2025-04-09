6c1ce904ec79eff59da3e446f560e6dd812be31e
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-04-09 14:55:07 +0800

    [UPDATE] 更新測試用例

---
+++ pages/client-browse/Test/mock/api.ts
@@ -0,0 +1,9 @@
+import { vi } from 'vitest'
+
+export const clientApi = {
+  api: {
+    getNuxeoCollection: vi.fn(() => Promise.resolve(true)),
+    deleteNuxeoDocument: vi.fn(() => Promise.resolve(true)),
+    postNuxeoCollectionAlldocuments: vi.fn(() => Promise.resolve(true)),
+  }
+}
---
+++ pages/client-trash/test/trashPage.nuxt.spec.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect, vi, beforeEach, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { TrashPage } from '#components'
+import { VxeGrid } from 'vxe-table'
+
+const mockRouterProvider = {
+  navigateTo: vi.fn()
+}
+
+describe('TrashEmptyTrash', () => {
+  let wrapper: any
+  beforeEach(() => {
+    vi.clearAllMocks()
+    wrapper = mount(TrashPage, {
+      global: {
+        components: { VxeGrid },
+        provide: {
+          [MenuRouterKey]: mockRouterProvider
+        },
+        mocks: {
+          $t: (msg: string) => msg,
+          $i18n: { t: (key: string) => key }
+        }
+      }
+    })
+  })
+
+  it('should render the component correctly', () => {
+    expect(wrapper.findComponent(VxeGrid).exists()).toBe(true)
+    expect(wrapper.find('#Trash__EmptyTrash').exists()).toBe(true)
+  })
+
+  it('should select button component correctly', () => {
+    const wrapper = mount(TrashPage, {
+      global: {
+        components: { VxeGrid },
+      },
+      data() {
+        return {
+          state: {
+            selectList: [{ id: 1 }, { id: 2 }] // 有選中項目
+          }
+        }
+      }
+    })
+
+    expect(wrapper.find('#Trash__RestoreSelected').exists()).toBe(true);
+    expect(wrapper.find('#Trash__PermanentlyDeleteSelected').exists()).toBe(true);
+  })
+
+})
