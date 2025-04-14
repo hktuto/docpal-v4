63e0968d30d6e616a3cac62003847b02e2d3f1f9
oy <OY.Ou@weltronics.com>
2025-04-14 17:40:38 +0800

    [muultiple-select]fix 2

--- libraries/eventbus/src/index.ts
+++ libraries/eventbus/src/index.ts
@@ -1,35 +1,36 @@
 export { useEventBus } from "@vueuse/core";
 import { useEventBus } from "@vueuse/core";
 export enum EventType {
-    TABLE_CONTEXT_MENU_OPEN = 'table-context-menu-open', // open table action column dialog
-    TABLE_CONTEXT_MENU_CLOSE = 'table-context-menu-close', // close table action column dialog
-    TABLE_ZOOM_MAX = 'table-zoom-max', // zoom max
-    TABLE_ZOOM_REVERT = 'table-zoom-revert', // zoom in
-    USER_PREFERENCE_CHANGE__TIME = 'user-preference-change--time', // user preference changed
-    USER_LOGIN__SUCCESS = 'user-login--success',
-    USER_LOGIN__EXPIRE = 'user-login--expire',
-    NETWORK_REQUEST__FAIL = 'network-request--fail',
-    OPEN_SETTINGS = 'open-settings',
-    CLOSE_SETTINGS = 'close-settings',
-    FILE_NEED_REFRESH = 'file-need-refresh',
-    FILE_DELETED = 'FILE_DELETED',
-    FILE_PREVIEW_OPEN = 'file-preview-open',
-    FILE_PREVIEW_CLOSE = 'file-preview-close',
-    CASE_NEED_REFRESH = 'case-need-refresh',
+  TABLE_CONTEXT_MENU_OPEN = 'table-context-menu-open', // open table action column dialog
+  TABLE_CONTEXT_MENU_CLOSE = 'table-context-menu-close', // close table action column dialog
+  TABLE_ZOOM_MAX = 'table-zoom-max', // zoom max
+  TABLE_ZOOM_REVERT = 'table-zoom-revert', // zoom in
+  USER_PREFERENCE_CHANGE__TIME = 'user-preference-change--time', // user preference changed
+  USER_LOGIN__SUCCESS = 'user-login--success',
+  USER_LOGIN__EXPIRE = 'user-login--expire',
+  NETWORK_REQUEST__FAIL = 'network-request--fail',
+  OPEN_SETTINGS = 'open-settings',
+  CLOSE_SETTINGS = 'close-settings',
+  FILE_NEED_REFRESH = 'file-need-refresh',
+  FILE_DELETED = 'FILE_DELETED',
+  FILE_PREVIEW_OPEN = 'file-preview-open',
+  FILE_PREVIEW_CLOSE = 'file-preview-close',
+  CASE_NEED_REFRESH = 'case-need-refresh',
+  FILE_CLEAN_SELECTED_ROWS = 'file-clean-selected-rows'
 }
 
 export enum GlobalPasteEvent {
-    TAB_COPY_PATH = 'tab-copy-path',
-    TAB_PASTE_PATH = 'tab-paste-path',
+  TAB_COPY_PATH = 'tab-copy-path',
+  TAB_PASTE_PATH = 'tab-paste-path',
 }
 
 export type GlobalPasteItem = {
-    type: GlobalPasteEvent,
-    data: any
-}
+  type: GlobalPasteEvent,
+  data: any;
+};
 
 
-export function emitBus(key:EventType, ...args:any) {
-    const bus = useEventBus(key)
-    bus.emit(...args)
+export function emitBus(key: EventType, ...args: any) {
+  const bus = useEventBus(key);
+  bus.emit(...args);
 }
