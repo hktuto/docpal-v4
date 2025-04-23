3d66202e783d945411519779b8888781e37018ee
oy <OY.Ou@weltronics.com>
2025-04-23 10:10:27 +0800

    [Workflow-Unique Id Generator]

---
+++ packages/bpmn/components/global/bpmn/context/generateId/form.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import { adminApi } from 'api'
+const condition = defineModel<any>('condition', { required: true })
+const { disabled } = defineProps<{
+  disabled: boolean
+}>()
+const graphProvider = inject(BPMN_PROVIDER)
+if (!graphProvider) {
+  throw new Error('Missing provider')
+}
+const emits = defineEmits(['remove'])
+const allFields = computed(() => {
+  return Object.keys(graphProvider.allFormField.value).map((key: string) => {
+    return graphProvider.allFormField.value[key]
+  })
+})
+function getTypeFields(type = 'string') {
+  return allFields.value.filter((item) => item.attr_type === type)
+}
+const generateIdTemplateList = ref([])
+function remove() {
+  emits('remove')
+}
+function handleIdTemplateChange(id) {
+  const idTem = generateIdTemplateList.value.find((item) => item.value === id)
+  condition.value.variables = idTem.variables
+}
+async function getGenerateIdTemplateList() {
+  const { data } = await adminApi.api.getIdTemplatesList()
+  generateIdTemplateList.value =
+    data?.map((item) => ({
+      label: item.name,
+      value: item.id,
+      variables: getVariables(item)
+    })) || []
+  function getVariables(row) {
+    const variables: any = []
+    const variableList = [...row.prefix, ...row.suffix]
+    const pattern = /(?<=var\()(.+?)(?=\))/g
+    variableList.forEach((item) => {
+      const values: RegExpExecArray | null = item.expression.match(pattern)
+      if (values && !!values[0]) {
+        variables.push({
+          label: values[0],
+          type: item.type
+        })
+      }
+    })
+    return variables
+  }
+}
+onMounted(() => {
+  getGenerateIdTemplateList()
+})
+</script>
+
+<template>
+  <ElForm :model="condition" label-position="top" width="100%">
+    <ElFormItem :label="$t('caseManagement.idGenerator')">
+      <ElSelect v-model="condition.templateId" :placeholder="$t('caseManagement.idGenerator')" :disabled="disabled" filterable @change="handleIdTemplateChange">
+        <ElOption v-for="item in generateIdTemplateList" :key="item.value" :label="item.label" :value="item.value" />
+      </ElSelect>
+    </ElFormItem>
+    <ElFormItem label="Form Info">
+      <ElSelect v-model="condition.worklowInfo" placeholder="Form Info" :disabled="disabled" filterable>
+        <ElOption v-for="item in getTypeFields('string')" :key="item.attr_id" :label="item.attr_name" :value="item.attr_id" />
+      </ElSelect>
+    </ElFormItem>
+    <template v-if="condition.variables.length > 0">
+      <el-divider content-position="left">{{ $t('caseManagement.idTemplateVariables') }}</el-divider>
+      <ElFormItem v-for="item in condition.variables" :key="item.label" :label="item.label">
+        <ElSelect v-model="item.value" :placeholder="item.label" :disabled="disabled" filterable>
+          <ElOption v-for="item in allFields" :key="item.attr_id" :label="item.attr_name" :value="item.attr_id" />
+        </ElSelect>
+      </ElFormItem>
+    </template>
+  </ElForm>
+</template>
+
+<style lang="scss" scoped>
+.cardContainer {
+  width: 100%;
+  padding: var(--app-space-s);
+  border-radius: var(--app-border-radius-m);
+  border: 1px solid var(--app-grey-825);
+}
+</style>
---
+++ packages/bpmn/components/global/bpmn/context/generateId/index.vue
@@ -0,0 +1,119 @@
+<script lang="ts" setup>
+import type { Node } from '@antv/x6'
+const { node } = defineProps<{
+  node: Node
+}>()
+const graphProvider = inject(BPMN_PROVIDER)
+const editorProvider = inject(EDITOR_PROVIDER)
+if (!graphProvider || !editorProvider) {
+  throw createError('graph provider not found')
+}
+const condition = ref({})
+function refreshData() {
+  const nodeData = node.getData()
+  condition.value = nodeData.data.extensionElements['flowable:field'].reduce((prev, item) => {
+    if (item.attr_name !== 'variables') {
+      prev[item.attr_name] = item['flowable:expression']['__cdata']
+    } else {
+      let _variablesCData = item['flowable:expression']['__cdata']
+      try {
+        _variablesCData = JSON.parse(_variablesCData)
+        console.log({_variablesCData})
+      } catch (error) {
+        _variablesCData = {}
+      }
+      if (!_variablesCData) return
+      const pattern = /(?<=get\()(.+?)(?=\))/g
+      prev[item.attr_name] = Object.keys(_variablesCData).map((key) => {
+        const value = _variablesCData[key]
+        const values: RegExpExecArray | null = value.match(pattern)
+        return {
+          label: key,
+          value: values[0]
+        }
+      })
+    }
+    return prev
+  }, {})
+}
+function updateCondition() {
+  const nodeData = node.getData()
+  const newData = {
+    ...nodeData,
+    version: nodeData.version + 1 || 1,
+    data: {
+      ...nodeData.data,
+      extensionElements: {
+        ...nodeData.data.extensionElements,
+        'flowable:field': getCondition()
+      }
+    }
+  }
+  node.setData(newData, { overwrite: true, deep: true })
+  function getCondition() {
+    return Object.keys(condition.value).map((key) => {
+      const value = condition.value[key]
+      let __cdata = ''
+      if(key === 'variables') {
+        const data = value.reduce((prev, item) => {
+          prev[item.label] = item.value ? '${variables:get(' + item.value + ')}' : ''
+          return prev
+        }, {})
+        __cdata = JSON.stringify(data)
+      } else {
+        __cdata = value
+      }
+      return {
+        attr_name: key,
+        ['flowable:expression']: {
+          __cdata
+        }
+      }
+    })
+  }
+}
+
+function setUpListener() {
+  graphProvider?.graph.value?.on('history:undo', () => {
+    refreshData()
+  })
+  graphProvider?.graph.value?.on('history:redo', () => {
+    refreshData()
+  })
+}
+
+watch(
+  condition,
+  (newVal) => {
+    if (newVal) {
+      updateCondition()
+    }
+  },
+  {
+    deep: true
+  }
+)
+watch(
+  () => node,
+  async () => {
+    console.log('node changed')
+    refreshData()
+  },
+  {
+    immediate: true,
+    deep: true
+  }
+)
+onMounted(async () => {
+  setUpListener()
+  // refreshData()
+})
+</script>
+<template>
+  <div>
+    <BpmnSidebarEditLabel :node="node" />
+    <BpmnContextGenerateIdForm :condition="condition" :disabled="editorProvider.readonly.value"></BpmnContextGenerateIdForm>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
