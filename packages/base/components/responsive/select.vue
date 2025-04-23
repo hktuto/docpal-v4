01f0099f64bab30dd6828d2c1eb32eceadfbde3f
oy <OY.Ou@weltronics.com>
2025-04-23 17:40:51 +0800

    [Response filter]fix boolean i18n

--- packages/base/components/responsive/select.vue
+++ packages/base/components/responsive/select.vue
@@ -1,85 +1,91 @@
 <template>
-<div>
+  <div>
     <el-button ref="buttonRef" text :index="index" v-click-outside="onClickOutside">
-        {{$t(selectData.label)}}
-        <el-tag v-if="selectData.value && selectData.value.length > 0" class="el-icon--right" effect="dark" round size="small">{{selectData.value.length > 9 ? '9+' : selectData.value.length}}</el-tag>
-        <el-icon class="el-icon--right"><ArrowDownBold /></el-icon>
+      {{ $t(selectData.label) }}
+      <el-tag v-if="selectData.value && selectData.value.length > 0" class="el-icon--right" effect="dark" round size="small">{{
+        selectData.value.length > 9 ? '9+' : selectData.value.length
+      }}</el-tag>
+      <el-icon class="el-icon--right"><ArrowDownBold /></el-icon>
     </el-button>
+    <!-- {{ selectData }} -->
     <el-popover
-        ref="popoverRef"
-        :virtual-ref="buttonRef"
-        placement="bottom"
-        :width="300"
-        trigger="click"
-        popper-class="auto-popper"
-        virtual-triggering
-        :popper-append-to-body="false"
+      ref="popoverRef"
+      :virtual-ref="buttonRef"
+      placement="bottom"
+      :width="300"
+      trigger="click"
+      popper-class="auto-popper"
+      virtual-triggering
+      :popper-append-to-body="false"
     >
-        <template #default>
-            <template v-if="selectData.options && selectData.options.length > 0">
-                <el-checkbox-group
-                    :max="selectData.isMultiple ? 100 : 1 "
-                    v-model="selectData.value"
-                    @change="handleChange(selectData)">
-                    <el-checkbox v-for="item in selectData.options" :label="item.value" :key="item.value" 
-                        :data-testid="`filter-${item.value}`" :title="item.label" :value="item.value">
-                        <template v-if="selectData.type === 'date'">
-                            {{formatDate(item.label)}}
-                        </template>
-                        <template v-else> {{$t(item.label)}} </template>
-                    </el-checkbox>
-                </el-checkbox-group>
+      <template #default>
+        <el-checkbox-group
+          v-if="selectData.options && selectData.options.length > 0"
+          :max="selectData.isMultiple ? 100 : 1"
+          v-model="selectData.value"
+          @change="handleChange(selectData)"
+        >
+          <el-checkbox
+            v-for="item in selectData.options"
+            :key="item.value"
+            :data-testid="`filter-${item.value}`"
+            :value="item.value"
+          >
+            <template v-if="selectData.type === 'date'">
+              {{ formatDate(item.label) }}
             </template>
-            <div v-else>
-                {{$t('noData')}}
-            </div>
-        </template>
+            <template v-else> {{ $t(String(item.label)) }} </template>
+          </el-checkbox>
+        </el-checkbox-group>
+        <div v-else>
+          {{ $t('noData') }}
+        </div>
+      </template>
     </el-popover>
-</div>
+  </div>
 </template>
 
-<script  lang="ts" setup>
-import { deepCopy } from '#imports';
+<script lang="ts" setup>
+import { deepCopy } from '#imports'
 import type ResSelectData from './filter.vue'
 import { ArrowDownBold } from '@element-plus/icons-vue'
 import { ClickOutside as vClickOutside } from 'element-plus'
 const props = defineProps<{
-    selectData: typeof ResSelectData,
-    index: number
-}>();
+  selectData: typeof ResSelectData
+  index: number
+}>()
 const emits = defineEmits(['change'])
 const buttonRef = ref()
 const popoverRef = ref()
 const onClickOutside = () => {
-
-//   unref(popoverRef).popperRef?.delayHide?.()
+  //   unref(popoverRef).popperRef?.delayHide?.()
 }
-function handleChange (data: typeof ResSelectData) {
-    emits('change', {
-        fieldName: data.key,
-        value: deepCopy(data.value)
-    })
+function handleChange(data: typeof ResSelectData) {
+  emits('change', {
+    fieldName: data.key,
+    value: deepCopy(data.value)
+  })
 }
 </script>
 <style lang="scss" scoped>
 .el-checkbox {
-    width: calc(100% - 30px);
-    white-space: pre-wrap;
+  width: calc(100% - 30px);
+  white-space: pre-wrap;
+  overflow: hidden;
+  :deep .el-checkbox__label {
     overflow: hidden;
-    :deep .el-checkbox__label {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        width: 100%;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 1;
-        word-break: break-all;
-    }
+    text-overflow: ellipsis;
+    width: 100%;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    word-break: break-all;
+  }
 }
 </style>
 <style>
 .auto-popper {
-    max-height: 80vh;
-    overflow-y: auto;
+  max-height: 80vh;
+  overflow-y: auto;
 }
 </style>
