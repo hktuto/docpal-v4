dee0bf45493ee64106a552156747a5865d1c084f
oy <OY.Ou@weltronics.com>
2025-04-24 14:55:26 +0800

    userId

--- packages/base/components/browse/info/WorkflowHoldSection.vue
+++ packages/base/components/browse/info/WorkflowHoldSection.vue
@@ -1,45 +1,45 @@
 <template>
-<div class="infoSection vertical-top">
+  <div class="infoSection vertical-top">
     <div class="infoTitle">
-        {{ $t('workflow_holdPolicy') }}
+      {{ $t('workflow_holdPolicy') }}
     </div>
     <div class="infoContetn">
-        <div class="block">
-            <template v-if="state.canApproval">
-                <el-button type="primary" size="mini" :loading="state.loading" @click="handelAudit('A')">{{ $t('workflow_startAdhocWorkflow_approve') }}</el-button>
-                <el-button type="danger" size="mini" :loading="state.loading" @click="handelAudit('D')">{{ $t('workflow_startAdhocWorkflow_reject') }}</el-button>
-            </template>
-        </div>
-        <small>
-            44
-        </small>
+      <div class="block">
+        <template v-if="state.canApproval">
+          <el-button type="primary" size="mini" :loading="state.loading" @click="handelAudit('A')">{{ $t('workflow_startAdhocWorkflow_approve') }}</el-button>
+          <el-button type="danger" size="mini" :loading="state.loading" @click="handelAudit('D')">{{ $t('workflow_startAdhocWorkflow_reject') }}</el-button>
+        </template>
+      </div>
     </div>
-</div> 
-
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { clientApi } from 'api'
 import { ElMessage } from 'element-plus'
-const props = defineProps<{ doc: any}>()
-const userId:string = useUser().getUserId()
+const props = defineProps<{ doc: any }>()
+const userId: string = useUserId().value
 const state = reactive({
-    loading: false,
-    canApproval: false
+  loading: false,
+  canApproval: false
 })
 
 // #region module: audit
-    async function handelAudit (approved:'A' | 'D' | 'R') {
-        state.loading = true
-        const result = await clientApi.api.patchPolicyDocumentsIdStatusStatus(props.doc.holdDetail.id, approved as any).then(res => res.data)
-        state.loading = false
-    }
+async function handelAudit(approved: 'A' | 'D' | 'R') {
+  state.loading = true
+  const result = await clientApi.api.patchPolicyDocumentsIdStatusStatus(props.doc.holdDetail.id, approved as any).then((res) => res.data)
+  state.loading = false
+}
 // #endregion
 
-watch(()=>props.doc, async(newValue) => {
-    if(newValue.holdDetail && newValue.holdDetail.applyApprovedBy === userId) state.canApproval = true
+watch(
+  () => props.doc,
+  async (newValue) => {
+    if (newValue.holdDetail && newValue.holdDetail.applyApprovedBy === userId) state.canApproval = true
     else state.canApproval = true
-}, { immediate: true })
+  },
+  { immediate: true }
+)
 
 onMounted(() => {})
 </script>
