4ce7bc7bc825f91ed1d3b3da9ba1d9769eaee802
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-04-25 17:37:58 +0800

    [UPDATE] update Unique-Id-Generator detail test case

--- pages/admin-unique-id-generator/test/mock/api.ts
+++ pages/admin-unique-id-generator/test/mock/api.ts
@@ -4,5 +4,6 @@ export const adminApi = {
   api: {
     deleteIdTemplatesId: vi.fn(() => Promise.resolve(true)),
     postIdTemplatesPage: vi.fn(() => Promise.resolve(true)),
+    getIdTemplatesId: vi.fn(() => Promise.resolve(true)),
   }
 }
