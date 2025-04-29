9bf588bdd62c6299927b852be897cfa82dc50b6b
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-04-29 17:19:17 +0800

    [UPDATE] update client-Share test case

--- pages/client-share/test/mock/api.ts
+++ pages/client-share/test/mock/api.ts
@@ -3,5 +3,9 @@ import { vi } from 'vitest'
 export const clientApi = {
   api: {
     deleteNuxeoShare: vi.fn(() => Promise.resolve(true)),
+    patchNuxeoShare: vi.fn(() => Promise.resolve(true)),
+    postNuxeoSharePage: vi.fn(() => Promise.resolve(true)),
+    postInternalshareOthers: vi.fn(() => Promise.resolve(true)),
+    postInternalshareMe: vi.fn(() => Promise.resolve(true)),
   }
 }
