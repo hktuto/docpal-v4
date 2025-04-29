9687037afba06627a65b2864538f3a0254f53d5e
oy <OY.Ou@weltronics.com>
2025-04-29 08:56:51 +0800

    [unit-test]admin-file-policy

--- pages/admin-file-policies/test/retentionDetail.nuxt.spec.ts
+++ pages/admin-file-policies/test/retentionDetail.nuxt.spec.ts
@@ -3,7 +3,7 @@ import { describe, it, test, vi, expect, beforeEach, afterEach } from 'vitest'
 import { adminApi } from './mock/api'
 import { VxeGrid } from 'vxe-table'
 import { mockRouterProvider } from './util'
-import { RetentionDetail, ResponsiveFilter } from '#components'
+import { RetentionDetail, ResponsiveFilter, RetentionAddDialog } from '#components'
 import { ElMessageBox, ElNotification, ElMessage } from 'element-plus'
 import { mockQuery, mockTable } from './setup'
 vi.mock('element-plus', () => ({
@@ -112,3 +112,85 @@ describe('[admin-retention]RetentionDetail', () => {
     expect(mockRouterProvider.message.success).toHaveBeenCalledWith('dpMsg_success')
   })
 })
+describe('[admin-retention]RetentionAddDialog', () => {
+  let wrapper: any
+  const mockTabProvider = {}
+
+  beforeEach(async () => {
+    wrapper = mount(RetentionAddDialog, {
+      props: {
+        id: 1
+      },
+      global: {
+        components: { VxeGrid, ResponsiveFilter, FormRenderer, VFormRender, ReaderDialog, Editorjs },
+        provide: {
+          [TabManagerKey]: mockTabProvider,
+          [MenuRouterKey]: mockRouterProvider,
+        },
+        mocks: {
+          $t: (msg: string) => msg, // Mock translation function
+          $i18n: { t: (key: string) => key }
+        }
+      }
+    })
+    await wrapper.vm.handleOpen()
+    await wrapper.vm.$nextTick()
+    // const dialogRef = wrapper.vm.$refs.RetentionAddDialogRef;
+    // dialogRef.handleOpen = vi.fn();
+    // const tableRef = wrapper.vm.$refs.tableRef;
+    // tableRef.initBar = vi.fn();
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+    vi.clearAllMocks()
+  })
+  it('renders correctly', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('handles form submission', async () => {
+    wrapper.vm.state.setting = { id: 1 }
+    wrapper.vm.FormRendererRef = {
+      vFormRenderRef: {
+        getFormData: vi.fn().mockResolvedValue({ actionType: 'A' }),
+      },
+    }
+
+    await wrapper.vm.handleSubmit()
+
+    expect(adminApi.api.postPolicyRetentions).toHaveBeenCalledWith({
+      id: 1,
+      actionType: 'D',
+    })
+    expect(ElMessage.success).toHaveBeenCalledWith('tip_createdSuccessMsg')
+    expect(wrapper.vm.state.visible).toBe(false)
+  })
+  it('does not submit if form data is not valid', async () => {
+    wrapper.vm.FormRendererRef = {
+      vFormRenderRef: {
+        getFormData: vi.fn().mockResolvedValue(null),
+      },
+    }
+
+    await wrapper.vm.handleSubmit()
+
+    expect(adminApi.api.postPolicyRetentions).not.toHaveBeenCalled()
+  })
+
+  it('handles submission error', async () => {
+    wrapper.vm.state.setting = { id: 1 }
+    wrapper.vm.FormRendererRef = {
+      vFormRenderRef: {
+        getFormData: vi.fn().mockResolvedValue({ actionType: 'A' }),
+      },
+    }
+    adminApi.api.postPolicyRetentions.mockRejectedValue(new Error('Error'))
+
+    await wrapper.vm.handleSubmit()
+
+    expect(wrapper.vm.state.loading).toBe(false)
+    expect(wrapper.vm.state.visible).toBe(true) 
+  })
+})
+
