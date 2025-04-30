45524990789ae53b69d96506bf15c88e3f6a0280
oy <OY.Ou@weltronics.com>
2025-04-30 10:46:41 +0800

    [client-browse] Try to use id as the parameter of idOrPath

--- packages/workflow/components/global/formWidget/browse.vue
+++ packages/workflow/components/global/formWidget/browse.vue
@@ -1,95 +1,94 @@
 <script lang="ts" setup>
-
 import { clientApi } from 'api'
-const home = ref({secondId:'/'})
+const home = ref({ secondId: '/' })
 const idOrPath = ref('')
 
 const tableRef = ref()
-function handleRefresh(){
-    if(tableRef.value) {
-        tableRef.value.reload()
-    }
+function handleRefresh() {
+  if (tableRef.value) {
+    tableRef.value.reload()
+  }
 }
 
 const props = defineProps<{
-    disabled: boolean,
-    formData: any
-    options: any,
-}>();
-const {formData, options} = toRefs(props)
+  disabled: boolean
+  formData: any
+  options: any
+}>()
+const { formData, options } = toRefs(props)
 
-function getFormData(){
-    console.log("getFormData")
-    // if no data , return empty
-    return {}
+function getFormData() {
+  console.log('getFormData')
+  // if no data , return empty
+  return {}
 }
 
-function changeRoute(path:string) {
-    idOrPath.value = path
+function changeRoute(path: string) {
+  idOrPath.value = path
 }
 
-function getInfo(){
-    // get options
-    if(props.options.data.folderCabinetId && props.formData[props.options.data.folderCabinetId]) {
-        // console.log({formData});
-        home.value = {
-            secondId: props.formData[props.options.data.folderCabinetId]
-        }
-        idOrPath.value = props.formData[props.options.data.folderCabinetId]
-    }else{
-      idOrPath.value = ""
-      home.value = {
-          secondId: ""
-      }
+function getInfo() {
+  // get options
+  if (props.options.data.folderCabinetId && props.formData[props.options.data.folderCabinetId]) {
+    // console.log({formData});
+    home.value = {
+      secondId: props.formData[props.options.data.folderCabinetId]
     }
+    idOrPath.value = props.formData[props.options.data.folderCabinetId]
+  } else {
+    idOrPath.value = ''
+    home.value = {
+      secondId: ''
+    }
+  }
 }
 
 defineExpose({ getFormData })
 
-provide(BrowseListProviderKey,{
-    getchildApi:(pageParams:any) => {
-        return clientApi.api.postNuxeoDocumentChildrenThumbnailV2(pageParams)
-    },
-    idOrPath,
-    changeRoute,
+provide(BrowseListProviderKey, {
+  getchildApi: (pageParams: any) => {
+    return clientApi.api.postNuxeoDocumentChildrenThumbnailV2(pageParams)
+  },
+  idOrPath,
+  changeRoute
 })
 
-watch(formData, () => {
+watch(
+  formData,
+  () => {
     getInfo()
-},{
+  },
+  {
     immediate: true,
     deep: true
-})
+  }
+)
 
 onMounted(() => {
-    getInfo()
+  getInfo()
 })
 </script>
 
 <template>
-    <div class="browse-cabinet-container">
-      <!-- {{idOrPath}} -->
-     <BrowseMiniTable v-if="home.secondId && idOrPath" 
-        ref="tableRef"
-        :hideColumns="['fileSize', 'mimeType', 'documentType']"
-      >
-        <template #toolbar_buttons> 
-            <BrowseBreadcrumb :idOrPath="idOrPath" :home="home" />
-        </template>
+  <div class="browse-cabinet-container">
+    <!-- {{idOrPath}} -->
+    <BrowseMiniTable v-if="home.secondId && idOrPath" ref="tableRef" :hideColumns="['fileSize', 'mimeType', 'documentType']">
+      <template #toolbar_buttons>
+        <BrowseBreadcrumb :idOrPath="idOrPath" :home="home" />
+      </template>
     </BrowseMiniTable>
-    <div v-else class="el-card"> 
-      <el-skeleton :rows="5"  />
+    <div v-else class="el-card">
+      <el-skeleton :rows="5" />
     </div>
-    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.browse-cabinet-container{
-    height: 600px;
-    position: relative;
+.browse-cabinet-container {
+  height: 600px;
+  position: relative;
 }
-.el-card{
+.el-card {
   padding: var(--app-space-m);
-  
 }
 </style>
