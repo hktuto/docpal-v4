5deaca7826334d09da4f1f24f3b901124980f718
Seantsang <9394wing@gmail.com>
2025-05-02 11:03:29 +0800

    temp add doc-template

---
+++ apps/server/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  ssr: true,
+  devtools: {
+    enabled: true
+  },
+  modules: ['@nuxt/test-utils/module'],
+  extends:[
+    "../../packages/doc-template",
+  ],
+  features:{
+    inlineStyles: true,
+  },
+  runtimeConfig:{
+    public:{
+      platform:'public'
+    }
+  },
+})
