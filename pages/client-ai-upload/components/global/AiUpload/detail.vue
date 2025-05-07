de5bb9451cf85fbcdb7640bf8c3b39129aeb7e34
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-05-07 17:48:32 +0800

    [UPDATE] update Ai-upload test case

--- pages/client-ai-upload/components/global/AiUpload/detail.vue
+++ pages/client-ai-upload/components/global/AiUpload/detail.vue
@@ -222,7 +222,6 @@ async function handleNodeClick(row) {
       documentType: row.fileType,
       docName: getFileName(state.selectedDoc.name, row.isFolder)
     })
-    console.log(222)
   })
 }
 
