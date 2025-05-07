53288e25130c3168a45599d9e424c96e1e0fc4d7
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-05-07 15:35:07 +0800

    [UPDATE] update Ai-upload test case

--- pages/client-ai-upload/test/mock/api.ts
+++ pages/client-ai-upload/test/mock/api.ts
@@ -3,6 +3,11 @@ import { vi } from 'vitest'
 export const clientApi = {
   api: {
     postNuxeoDocumentQueryuploadfiledtopage: vi.fn(() => Promise.resolve(true)),
+    postNuxeoDocumentQueryuploadfiledetaildtolist: vi.fn(() => Promise.resolve(true)),
+    postNuxeoDocumentCheckfileexist: vi.fn(() => Promise.resolve(true)),
+    deleteNuxeoDocumentTempfileId: vi.fn(() => Promise.resolve(true)),
+    getNuxeoDocumentRetryClassificationUploadid: vi.fn(() => Promise.resolve(true)),
+    postNuxeoDocumentBatchconfirm: vi.fn(() => Promise.resolve(true))
   },
   instance: {
     post: vi.fn()
