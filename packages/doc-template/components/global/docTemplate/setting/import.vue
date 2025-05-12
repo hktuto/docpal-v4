b3811e8a4786aadec91359f69331c6211aaba616
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-05-12 13:21:12 +0800

    [UPDATE] update

--- packages/doc-template/components/global/docTemplate/setting/import.vue
+++ packages/doc-template/components/global/docTemplate/setting/import.vue
@@ -1,13 +1,66 @@
 <script lang="ts" setup>
-import { DocTemplateProveKey } from '~/utils/docTempalteHelper';
+import { DocTemplateProveKey } from "~/utils/docTempalteHelper";
+import formJson from "./docJson.json";
 
+const { editor, editorOptions } = inject(DocTemplateProveKey);
+const { t } = useI18n();
 
+const state = reactive({
+  loading: false,
+  visible: false,
+  textContent: ""
+});
 
-const { editor, editorOptions } = inject(DocTemplateProveKey)
+const FormRendererRef = ref();
 
+async function handleSubmit() {
+  try {
+    const data = await FormRendererRef.value.vFormRenderRef.getFormData();
+    const json = JSON.parse(data.textContent);
+    setOptionsFoJson(json.Options);
+    setContentFoJson(json.Content);
+    state.visible = false;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function setOptionsFoJson(json) {
+  console.log(22, editor.value);
+  try {
+    editor.value.setOptions(json);
+  } catch (e) {
+    console.log("set options is error", e);
+  }
+}
+
+function setContentFoJson(json) {
+  try {
+    editor.value.commands.setContent(json);
+  } catch (e) {
+    console.log("set content is error", e);
+  }
+}
+
+function setContentFoHtml(html: string) {
+  try {
+    editor.value.commands.setContent(html);
+  } catch (e) {
+    console.log("html parse is error", e);
+  }
+}
 
 </script>
 
 <template>
-  <ElButton >Import</ElButton>
+  <ElButton @click="state.visible = true">Import</ElButton>
+
+  <el-dialog v-model="state.visible" :title="t('Import')">
+    <FormRenderer ref="FormRendererRef" :form-json="formJson" />
+    <template #footer>
+      <el-button type="primary" :loading="state.loading" @click="handleSubmit">
+        {{ $t("common_submit") }}
+      </el-button>
+    </template>
+  </el-dialog>
 </template>
