22d65213d98eabc1ef92763b08046a8096047d0a
oy <OY.Ou@weltronics.com>
2025-05-13 17:11:42 +0800

    [dp-dashboard]doc-coCount

--- packages/dp-dashboard/components/global/doc/coCount/size.vue
+++ packages/dp-dashboard/components/global/doc/coCount/size.vue
@@ -1,158 +1,107 @@
 <template>
-    <div ref="cardRef" class="co-count co-count-chart">
-        <div id="myEcharts" ref="chartRef" class="echart"></div>
-    </div>
+  <div ref="cardRef" class="co-count co-count-chart">
+    <div id="myEcharts" ref="chartRef" class="echart"></div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import * as echarts from "echarts";
-import { useEventListener, watchDebounced } from '@vueuse/core'
-import { publicApi } from "api";
+import { publicApi } from 'api'
 const { t } = useI18n()
 
-const props = withDefaults( defineProps<{
-    dates?: any;
-    documentType?: string,
-    user?: string;
-    setting?: any;
-}>() , {
+const props = withDefaults(
+  defineProps<{
+    dates?: any
+    documentType?: string
+    user?: string
+    setting?: any
+  }>(),
+  {
     documentType: '',
     user: ''
-})
-type EChartsOption = echarts.EChartsOption;
-const chartRef = ref()
-const cardRef = ref()
-let echartInstance
-const emits = defineEmits([
-    'refreshSetting', 'delete'
-])
-const setting = {
-    defaultSetting: {
-        options: {
-            xAxis: {
-                type: 'category',
-                boundaryGap: false,
-                data: []
-            },
-            title: {
-                text: t('dashboard.fileSizes'),
-                left: "left",
-            },
-            yAxis: {
-                type: 'value'
-            },
-            tooltip: {
-                appendToBody: true,
-                trigger: 'item'
-            },
-            legend: {
-                bottom: '5%',
-                left: 'center',
-                itemWidth: 10,
-                itemHeight: 10,
-            }
-        },
-        series: {
-            type: 'line',
-            smooth: true
-        }
+  }
+)
+const emits = defineEmits(['refreshSetting', 'delete'])
+let { chartRef, cardRef, settingRef, resize } = useDashboardCard({
+  props,
+  initStyleAction: () => {
+    initStyle()
+  },
+  getOptions: async (chartSetting) => {
+    let resultOptions: any = {
+      xAxis: { type: 'category', boundaryGap: false, data: [] },
+      title: { text: t('dashboard.fileSizes'), left: 'left' },
+      yAxis: { type: 'value' },
+      tooltip: { appendToBody: true, trigger: 'item' },
+      legend: {
+        bottom: '5%',
+        left: 'center',
+        itemWidth: 10,
+        itemHeight: 10
+      }
     }
+    const { data, xAxis } = await getData(chartSetting.documentType)
+    resultOptions.xAxis.data = xAxis
+    resultOptions.series = {
+      type: 'line',
+      smooth: true,
+      data: data
+    }
+    return resultOptions
+  }
+})
+// #region module: set
+function initStyle() {
+  const pHeight = cardRef.value.offsetHeight
+  const pWidth = cardRef.value.offsetWidth
+  // 需要扣除 .el-card 的 padding
+  chartRef.value.style = `height: ${pHeight}px; width: ${pWidth - 20}px`
 }
-const state = reactive({
+// #endregion
+const GetCoCountSizeApi = async (params: any, creator?: string) => {
+  if (creator) {
+    return await publicApi.api
+      .postDashboardNewfilesofspecifyusersizebydtypebymonthlycumulation({
+        ...params,
+        creator
+      })
+      .then((res) => res.data)
+  }
+  return await publicApi.api.postDashboardNewfilesofuserssizebydtypebymonthlycumulation(params).then((res) => res.data)
+}
+async function getData(documentType: string) {
+  const resultData: any = {
     data: [],
     xAxis: []
-})
-let options: any = {}
+  }
+  try {
+    const params: any = {
+      primaryType: documentType
+    }
+    if (props.dates) {
+      params.isQueryList = true
+      params.dateRange = {
+        from: props.dates[0],
+        to: props.dates[1]
+      }
+    }
+    const res: any = await GetCoCountSizeApi(params, props.user)
+    const initData = res.group_document_type.buckets[0].group_by_time.buckets
+    resultData.data = initData.reduce((prev, item) => {
+      prev.push(item.cumulative_sum_mb.value)
+      resultData.xAxis.push(item.key_as_string)
+      return prev
+    }, [])
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    return resultData
+  }
+}
+// #endregion
 
-// #region module: set
-    function initStyle () {
-        const pHeight = cardRef.value.offsetHeight
-        const pWidth = cardRef.value.offsetWidth
-        // 需要扣除 .el-card 的 padding
-        chartRef.value.style = `height: ${pHeight}px; width: ${pWidth - 20}px`
-    }
-// #endregion
-async function initChart() {
-    if (echartInstance) echartInstance.clear()
-    echartInstance = echarts.init(chartRef.value);
-    echartInstance.setOption(options);
-}
-function resize() {
-    setTimeout(async() => {
-        initStyle()
-        if(!!echartInstance) echartInstance.resize()
-    })
-}
-// #region module: setting
-    async function handleInitChart(documentType) {
-        options = { 
-            ...setting.defaultSetting.options
-        }
-        // data
-        await getData(documentType)
-        options.xAxis.data = state.xAxis
-        options.series = {
-            ...setting.defaultSetting.series,
-            data: state.data
-        }
-        initChart()
-    }
-    const GetCoCountSizeApi = async(params: any, creator?: string) => {
-        if (creator) {
-            return await publicApi.api.postDashboardNewfilesofspecifyusersizebydtypebymonthlycumulation({
-                ...params,
-                creator
-            }).then(res => res.data)
-        }
-        return await publicApi.api.postDashboardNewfilesofuserssizebydtypebymonthlycumulation(params).then(res => res.data)
-    }
-    async function getData(documentType: string) {
-        try {
-            const params: any = {
-                primaryType: documentType,
-            }
-            if (props.dates) {
-                params.isQueryList = true
-                params.dateRange = {
-                    from: props.dates[0],
-                    to: props.dates[1]
-                }
-            }
-            const res: any = await GetCoCountSizeApi(params, props.user)
-            state.xAxis = []
-            const initData = res.group_document_type.buckets[0].group_by_time.buckets
-            state.data = initData.reduce((prev,item) => {
-                prev.push(item.cumulative_sum_mb.value)
-                state.xAxis.push(item.key_as_string)
-                return prev
-            }, [])
-        } catch (error) {
-            state.data = []
-        }
-    }
-// #endregion
-// #region module: 
-// #endregion
-onMounted(async() => {
-    nextTick(async() => {
-        initStyle()
-        // 随着屏幕大小调节图表
-        useEventListener(window, 'resize', resize)
-    })
-})
-onUnmounted(() => {
-    if(!!echartInstance) echartInstance.dispose()
-})
-watchDebounced(props, (newValue, oldValue) => {
-    if (!newValue) return
-    if (!oldValue || JSON.stringify(newValue) !== JSON.stringify(oldValue)) {
-        handleInitChart(newValue.documentType)
-    }
-},{ debounce: 200, maxWait: 500, immediate: true })
 defineExpose({
-    resize
+  resize
 })
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
