6f9309e164cd3700bf75532163a455c1eae86d48
oy <OY.Ou@weltronics.com>
2025-05-19 10:49:57 +0800

    DPL-5546 [Client-Browse] Should not select temp file

--- pages/client-browse/components/browse/list/table.vue
+++ pages/client-browse/components/browse/list/table.vue
@@ -498,7 +498,7 @@ const { tableConfig, tableEvent, tableRef, reload, cleanSelectedRows } = useVxeT
       highlight: true,
       range: false,
       trigger: 'cell',
-      visibleMethod: ({ row }: any) => !row.isFolder
+      visibleMethod: ({ row }: any) => !row.isFolder && row.source !== 'tempFile'
     },
     rowConfig: {
       height: 42,
