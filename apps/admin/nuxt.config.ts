81ad76e68b88992056ebdc624f53bc0664503806
Seantsang <9394wing@gmail.com>
2025-05-20 16:35:12 +0800

    restore ssr in admin

--- apps/admin/nuxt.config.ts
+++ apps/admin/nuxt.config.ts
@@ -4,7 +4,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr:true,
+  ssr:false,
   modules: ['@nuxt/test-utils/module'],
   extends:[
     "../../packages/icon",
