d44854d43c836a529c72db83ea5636baf9b6f73b
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-05-20 11:40:11 +0800

    [UPDATE] error msg

--- packages/base/components/tab/router.vue
+++ packages/base/components/tab/router.vue
@@ -1,257 +1,261 @@
 <script lang="ts" setup>
-import type {TabItem, RouterParams} from '#imports';
-import {ElMessage, ElNotification } from 'element-plus';
+import type { TabItem, RouterParams } from '#imports'
+import { ElMessage, ElNotification } from 'element-plus'
+import { Refresh } from '@element-plus/icons-vue'
 
-import {MenuRouterKey, TabManagerKey, panelRouteUpdate} from '#imports'
+import { MenuRouterKey, TabManagerKey, panelRouteUpdate } from '#imports'
+
 const { current } = useMagicKeys()
-const {allComponents} = useTabsManager()
-const {layout} = useTabsManager()
+const { allComponents } = useTabsManager()
+const { layout } = useTabsManager()
 
 const hightLightPanel = useCurrentTargetPanel()
 
-const historyLimit = 10;
+const historyLimit = 10
 
-const tab = defineModel<TabItem>('tab', { required: true });
+const tab = defineModel<TabItem>('tab', { required: true })
 const tabManager = inject(TabManagerKey)
-if(!tabManager) {    
-    throw createError('no '+ TabManagerKey.toString + "provided")
+if (!tabManager) {
+  throw createError('no ' + TabManagerKey.toString + 'provided')
 }
 const { t } = useI18n()
 const history = ref<RouterParams[]>([])
 const forwardHistory = ref<RouterParams[]>([])
 
 
-
 const isFullscreen = computed(() => {
-    if(!tabManager.fullscreenItem.value) return false
-    return tabManager.fullscreenItem.value?.id === tab.value.id
+  if (!tabManager.fullscreenItem.value) return false
+  return tabManager.fullscreenItem.value?.id === tab.value.id
 })
 
 
-const refeshActions = ref<Function[]>([]);
+const refeshActions = ref<Function[]>([])
 
 async function handleRefresh() {
-  try{
+  try {
     await Promise.all(refeshActions.value.map(item => item()))
-  }catch(err){
+  } catch (err) {
     console.log(err)
   }
 }
 
-function navigateTo(param: RouterParams, openInNewTab:boolean = false, ignoreExist:boolean = false) {
-    if(current.has('meta') || current.has('control') || openInNewTab){
-        tabManager?.openInNewTab(param)
-        return;
+function navigateTo(param: RouterParams, openInNewTab: boolean = false, ignoreExist: boolean = false) {
+  if (current.has('meta') || current.has('control') || openInNewTab) {
+    tabManager?.openInNewTab(param)
+    return
+  }
+  if (!ignoreExist) {
+    const existingTab = allComponents.value.find(item => item.name === param.name)
+    if (existingTab) {
+      tabManager?.openTab(param)
+      return
     }
-    if(!ignoreExist){
-        const existingTab = allComponents.value.find(item => item.name === param.name)
-        if(existingTab){
-            tabManager?.openTab(param)
-            return;
-        }
-    }
-    refeshActions.value = []
-    // forwardHistory.value = [];
-    const lastId = tab.value.id
-    history.value.push({
-        ...tab.value,
-    })
-    tab.value = {
-        ...param,
-        parent: tab.value.parent,
-        id: tab.value.id,
-        initized: true,
-    }
-    componentKey.value ++
-    panelRouteUpdate(tab.value.parent, lastId, tab.value)
+  }
+  refeshActions.value = []
+  // forwardHistory.value = [];
+  const lastId = tab.value.id
+  history.value.push({
+    ...tab.value
+  })
+  tab.value = {
+    ...param,
+    parent: tab.value.parent,
+    id: tab.value.id,
+    initized: true
+  }
+  componentKey.value++
+  panelRouteUpdate(tab.value.parent, lastId, tab.value)
 }
 
 function getHistory() {
-    return history.value
+  return history.value
 }
 
-function addToHistory(param:RouterParams){
-    history.value.push(param)
-    if(history.value.length > historyLimit){
-        history.value.shift()
-    }
+function addToHistory(param: RouterParams) {
+  history.value.push(param)
+  if (history.value.length > historyLimit) {
+    history.value.shift()
+  }
 }
 
-function back(fallback?:any){
+function back(fallback?: any) {
   refeshActions.value = []
-    if(history.value.length === 0) {
-        if(fallback){
-            console.log("fallback", fallback)
-            navigateTo(fallback)
-        }    
-        return
+  if (history.value.length === 0) {
+    if (fallback) {
+      console.log('fallback', fallback)
+      navigateTo(fallback)
     }
-    const lastItem = history.value.pop()
-    if(lastItem){
+    return
+  }
+  const lastItem = history.value.pop()
+  if (lastItem) {
     const lastId = tab.value.id
 
-        forwardHistory.value.push({
-            name: tab.value.name,
-            id: tab.value.id,
-            icon: tab.value.icon,
-            label: tab.value.label,
-            component: tab.value.component,
-            props: tab.value.props
-        })
-        if(forwardHistory.value.length > historyLimit){
-            forwardHistory.value.shift()
-        }
-        tab.value = {
-            ...lastItem,
-            parent: tab.value.parent,
-            id: tab.value.id,
-            initized: true,
-        }
-        componentKey.value ++
-        panelRouteUpdate(tab.value.parent, lastId, tab.value)
+    forwardHistory.value.push({
+      name: tab.value.name,
+      id: tab.value.id,
+      icon: tab.value.icon,
+      label: tab.value.label,
+      component: tab.value.component,
+      props: tab.value.props
+    })
+    if (forwardHistory.value.length > historyLimit) {
+      forwardHistory.value.shift()
     }
+    tab.value = {
+      ...lastItem,
+      parent: tab.value.parent,
+      id: tab.value.id,
+      initized: true
+    }
+    componentKey.value++
+    panelRouteUpdate(tab.value.parent, lastId, tab.value)
+  }
 }
 
 function forward() {
-    if(forwardHistory.value.length === 0) return
-    const lastItem = forwardHistory.value.pop()
+  if (forwardHistory.value.length === 0) return
+  const lastItem = forwardHistory.value.pop()
 
-    if(lastItem){
-      refeshActions.value = []
-        const lastId = tab.value.id
+  if (lastItem) {
+    refeshActions.value = []
+    const lastId = tab.value.id
 
-        history.value.push({
-            ...tab.value,
-        })
-        if(history.value.length > historyLimit){
-            history.value.shift()
-        }
-        tab.value = {
-            ...lastItem,
-            parent: tab.value.parent,
-            id: tab.value.id,
-            initized: true
-        }
-        componentKey.value ++
-        panelRouteUpdate(tab.value.parent, lastId, tab.value)
-    }
-}
-
-function updateProps(newProps:any){
-    tab.value.props = { ...tab.value.props, ...newProps }
-    panelRouteUpdate(tab.value.parent, tab.value.id, tab.value)
-}
-
-function updateTabName(newName:string){
-    tab.value.label = newName
-    // add tab name to allComponents 
-    allComponents.value.forEach( item => {
-        if(item.id === tab.value.id){
-            item.label = newName
-        }
+    history.value.push({
+      ...tab.value
     })
-   
+    if (history.value.length > historyLimit) {
+      history.value.shift()
+    }
+    tab.value = {
+      ...lastItem,
+      parent: tab.value.parent,
+      id: tab.value.id,
+      initized: true
+    }
+    componentKey.value++
+    panelRouteUpdate(tab.value.parent, lastId, tab.value)
+  }
 }
-const componentKey = ref(0);
-const showError = ref(false);
-const errorMessage = ref("");
-function showErrorPage(error:Error){
-    showError.value = true
-    errorMessage.value = error.message
+
+function updateProps(newProps: any) {
+  tab.value.props = { ...tab.value.props, ...newProps }
+  panelRouteUpdate(tab.value.parent, tab.value.id, tab.value)
 }
-function retryError(){
-    showError.value = false
+
+function updateTabName(newName: string) {
+  tab.value.label = newName
+  // add tab name to allComponents
+  allComponents.value.forEach(item => {
+    if (item.id === tab.value.id) {
+      item.label = newName
+    }
+  })
+
 }
-const routerContainer = computed(( ) => {
-    return document.getElementById(tab.value.parent + "_" + tab.value.id)
+
+const componentKey = ref(0)
+const showError = ref(false)
+const errorMessage = ref('')
+
+function showErrorPage(error: Error) {
+  showError.value = true
+  errorMessage.value = error.message
+}
+
+function retryError() {
+  showError.value = false
+}
+
+const routerContainer = computed(() => {
+  return document.getElementById(tab.value.parent + '_' + tab.value.id)
 })
 
-function createMessage(type:string, ...args:any[]){
-    if(args.length === 1 && typeof args[0] === 'string'){
-        ElMessage({
-            type,
-            message: args[0],
-            appendTo: '#'+ tab.value.parent + "_" + tab.value.id,
-        } as any)
-    }else{
-        ElMessage({
-            type,
-            appendTo: routerContainer.value,
-            ...args,
-        } as any)
-    }
+function createMessage(type: string, ...args: any[]) {
+  if (args.length === 1 && typeof args[0] === 'string') {
+    ElMessage({
+      type,
+      message: args[0],
+      appendTo: '#' + tab.value.parent + '_' + tab.value.id
+    } as any)
+  } else {
+    ElMessage({
+      type,
+      appendTo: routerContainer.value,
+      ...args
+    } as any)
+  }
 }
 
-function createNotification(type:string, ...args:any[]){
-    if(args.length === 1 && typeof args[0] === 'string'){
-        ElNotification({
-            type,
-            message:args[0]
-        } as any,{
-            appendTo: routerContainer.value,
-        }as any)
-    }else{
-        ElNotification({
-            type,
-            ...args
-        } as any,{
-            appendTo: routerContainer.value,
-        } as any)
-    }
+function createNotification(type: string, ...args: any[]) {
+  if (args.length === 1 && typeof args[0] === 'string') {
+    ElNotification({
+      type,
+      message: args[0]
+    } as any, {
+      appendTo: routerContainer.value
+    } as any)
+  } else {
+    ElNotification({
+      type,
+      ...args
+    } as any, {
+      appendTo: routerContainer.value
+    } as any)
+  }
 }
 
 
 watch(() => [layout, hightLightPanel], () => {
   const currentPanel = layout.value.find(lay => lay.id === hightLightPanel.value)
-  
-  if(!currentPanel) return
-  if(currentPanel.id === tab.value.parent && currentPanel.tabs[currentPanel.showingTabIndex].id === tab.value.id) {
+
+  if (!currentPanel) return
+  if (currentPanel.id === tab.value.parent && currentPanel.tabs[currentPanel.showingTabIndex].id === tab.value.id) {
     handleRefresh()
   }
-},{
+}, {
   deep: true
 })
 
 
-
-provide(MenuRouterKey,{
-    navigateTo,
-    updateProps,
-    updateTabName,
-    routerContainer,
-    back,
-    getHistory,
-    addToHistory,
-    refeshActions,
-    message:{
-        success: (...args) => createMessage('success', ...args),
-        error: (...args) => createMessage('error', ...args),
-        warning: (...args) => createMessage('warning', ...args),
-        info: (...args) => createMessage('info', ...args),
-        loading: (...args) => createMessage('loading', ...args)
-    },
-    notification:{
-        success: (...args) => createNotification('success', ...args),
-        error: (...args) => createNotification('error', ...args),
-        warning: (...args) => createNotification('warning', ...args),
-        info: (...args) => createNotification('info', ...args),
-        loading: (...args) => createNotification('loading', ...args)
-    },
-    showErrorPage,
-    tabData: tab
+provide(MenuRouterKey, {
+  navigateTo,
+  updateProps,
+  updateTabName,
+  routerContainer,
+  back,
+  getHistory,
+  addToHistory,
+  refeshActions,
+  message: {
+    success: (...args) => createMessage('success', ...args),
+    error: (...args) => createMessage('error', ...args),
+    warning: (...args) => createMessage('warning', ...args),
+    info: (...args) => createMessage('info', ...args),
+    loading: (...args) => createMessage('loading', ...args)
+  },
+  notification: {
+    success: (...args) => createNotification('success', ...args),
+    error: (...args) => createNotification('error', ...args),
+    warning: (...args) => createNotification('warning', ...args),
+    info: (...args) => createNotification('info', ...args),
+    loading: (...args) => createNotification('loading', ...args)
+  },
+  showErrorPage,
+  tabData: tab
 })
 
 function reloadComponent() {
-    componentKey.value ++
+  componentKey.value++
 }
 
 const historyClass = computed(() => {
-    if(isFullscreen.value) return `#fullscreen-tab-header-${tab.value.parent}-${tab.value.id} > .tabLeftTeleportContainer`
-    return `#tab-header-${tab.value.parent}-${tab.value.id} > .tabLeftTeleportContainer`
+  if (isFullscreen.value) return `#fullscreen-tab-header-${tab.value.parent}-${tab.value.id} > .tabLeftTeleportContainer`
+  return `#tab-header-${tab.value.parent}-${tab.value.id} > .tabLeftTeleportContainer`
 })
 
 defineExpose({
-    navigateTo,
+  navigateTo
 })
 
 onUnmounted(() => {
@@ -263,95 +267,112 @@ onUnmounted(() => {
 
 <template>
 
-<div :class="['routerContainer', [tab.name]]">
+  <div :class="['routerContainer', [tab.name]]">
     <Teleport :to="historyClass" defer>
-        <div class="historyContainer">
-            <Icon name="lucide:chevron-left" :class="{historyBtn:true, active: history.length !== 0}" @click="() => back()"/>
-            <Icon name="lucide:chevron-right" :class="{historyBtn:true, active: forwardHistory.length !== 0}" @click="() => forward()"/>
-            <Icon name="lucide:refresh-ccw" :class="{historyBtn:true, active: true}" @click="reloadComponent"/>
-        </div>
+      <div class="historyContainer">
+        <Icon name="lucide:chevron-left" :class="{historyBtn:true, active: history.length !== 0}"
+              @click="() => back()" />
+        <Icon name="lucide:chevron-right" :class="{historyBtn:true, active: forwardHistory.length !== 0}"
+              @click="() => forward()" />
+        <Icon name="lucide:refresh-ccw" :class="{historyBtn:true, active: true}" @click="reloadComponent" />
+      </div>
     </Teleport>
-    <Teleport  v-if="tab.icon" defer :to="`#${isFullscreen? 'fullscreen-':''}tab-header-${tab.parent}-${tab.id} > .icon`">
-        
-        <Icon :name="tab.icon" />
+    <Teleport v-if="tab.icon" defer
+              :to="`#${isFullscreen? 'fullscreen-':''}tab-header-${tab.parent}-${tab.id} > .icon`">
+
+      <Icon :name="tab.icon" />
     </Teleport>
-    <Teleport  v-if="tab.label" defer :to="`#${isFullscreen? 'fullscreen-':''}tab-header-${tab.parent}-${tab.id} > .label`">
-        <div class="label">
-            {{  t(tab.label) }} 
-        </div>  
+    <Teleport v-if="tab.label" defer
+              :to="`#${isFullscreen? 'fullscreen-':''}tab-header-${tab.parent}-${tab.id} > .label`">
+      <div class="label">
+        {{ t(tab.label) }}
+      </div>
     </Teleport>
-    
+
     <template v-if="tab.initized && !showError">
-        <Transition >
-            <KeepAlive :exclude="/Dead/" :max="2">
-                <Suspense>
-                  <NuxtErrorBoundary @error="reloadComponent">
-                      <component :is="tab.component" :key="tab.name + componentKey" :tab="tab" v-bind="tab.props"/>
-                      <template #error="{ error, clearError }">
-                          <div class="template-container--main">
-                            error {{ error }}
-                            <el-button size="small" :icon="Refresh" circle  @click="clearError"></el-button>
-                          </div>
-                        </template>
-                  </NuxtErrorBoundary>
-                    <template #fallback>
-                        <LoadingBgInline />
-                    </template>
-                </Suspense>
-            </KeepAlive>
-        </Transition>
+      <Transition>
+        <KeepAlive :exclude="/Dead/" :max="2">
+          <Suspense>
+            <NuxtErrorBoundary @error="reloadComponent">
+              <component :is="tab.component" :key="tab.name + componentKey" :tab="tab" v-bind="tab.props" />
+              <template #error="{ error, clearError }">
+                <div class="errorBoundaryContainer">
+                  <div class="messageContainer">
+                    <h3 class="errorTitle">ERROR :  {{ $t(tab.label) }}</h3>
+                    <pre>
+                       {{ error }}
+                    </pre>
+                    <pre>
+                      {{ tab }}
+                    </pre>
+                    <el-button  :icon="Refresh" @click="clearError">
+                      {{ $t('common_refresh') }}
+                    </el-button>
+                  </div>
+                </div>
+              </template>
+            </NuxtErrorBoundary>
+            <template #fallback>
+              <LoadingBgInline />
+            </template>
+          </Suspense>
+        </KeepAlive>
+      </Transition>
     </template>
     <template v-else>
-        <div class="errorContainer">
-            <div class="errorIcon">
-                <Icon name="lucide:alert-triangle" size="24" />
-            </div>
-            <div class="errorMessage">
-                {{ errorMessage }}
-            </div>
-            <div class="errorRetry" @click="retryError">
-                Retry
-            </div>
+      <div class="errorContainer">
+        <div class="errorIcon">
+          <Icon name="lucide:alert-triangle" size="24" />
         </div>
+        <div class="errorMessage">
+          {{ errorMessage }}
+        </div>
+        <div class="errorRetry" @click="retryError">
+          Retry
+        </div>
+      </div>
     </template>
-</div>
+  </div>
 </template>
 
 <style scoped>
-.loadingContainer{
-    position: relative;
-    width:100%;
-    height: 100%;
-    display: grid;
-    place-items: center;
-}
-.historyContainer{
-    line-height: 1;
-    display: flex;
-    flex-flow: row nowrap;
-    gap: var(--app-space-xxs);
-    align-items: center;
-    justify-content: flex-start;
+.loadingContainer {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
 }
 
-.historyBtn{
-    --btn-color:var(--app-grey-800);
-    color: var(--btn-color);
-    font-size: var(--app-font-size-m);
-    &.active{
-        --btn-color: var(--app-grey-300);
-        &:hover {
-            --btn-color: var(--app-success-color);
-        } 
+.historyContainer {
+  line-height: 1;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: var(--app-space-xxs);
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.historyBtn {
+  --btn-color: var(--app-grey-800);
+  color: var(--btn-color);
+  font-size: var(--app-font-size-m);
+
+  &.active {
+    --btn-color: var(--app-grey-300);
+
+    &:hover {
+      --btn-color: var(--app-success-color);
     }
-}
-.routerContainer{
-    width: 100%;
-    height: 100%;
-    position: relative;
-    overflow: hidden;
+  }
 }
 
+.routerContainer {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
 
 .v-enter-active,
 .v-leave-active {
