8034f3ee66daa0c61f7596c151687c019789e356
Seantsang <9394wing@gmail.com>
2025-05-20 10:11:27 +0800

    add reload to browse

--- pages/client-browse/components/browse/pageComponent.vue
+++ pages/client-browse/components/browse/pageComponent.vue
@@ -6,14 +6,17 @@ import { BrowseListTable } from '#components'
 import { EventType, useEventBus, emitBus } from 'eventbus'
 import { actions, ActionsFilter } from '../../../../packages/base/utils/browseActions'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   idOrPath: string
-  filter: any
-  home: any
+  home?: any
   commentId?: string
-}>()
+  expandedItems: any[]
+}>(), {
+  idOrPath: '/',
+  expandedItems: []
+})
 
-const { idOrPath, commentId } = toRefs(props)
+const { idOrPath, commentId, expandedItems } = toRefs(props)
 const tabProvider = inject(TabManagerKey)
 const routerProvider = inject(MenuRouterKey)
 const selectedItem = ref<any[]>([])
@@ -37,7 +40,6 @@ function changeRoute(path: string) {
   // clean filter
   routerProvider?.updateProps({
     idOrPath: path,
-    filter: {}
   })
 }
 
@@ -210,7 +212,11 @@ useEventListener(document, 'closeFilePreview', closePreview)
   <div ref="browseContainer" class="browseContainer">
     <splitpanes>
       <Pane>
-        <BrowseListTable ref="tableRef" :class="{ selected: selectedList.length > 0 }" :selectedRows="selectedItem" @selectedChange="selectedChangeHandler" >
+        <BrowseListTable ref="tableRef" 
+          :class="{ selected: selectedList.length > 0 }" 
+          :selectedRows="selectedItem" 
+          :expandedItems="expandedItems"
+          @selectedChange="selectedChangeHandler" >
           <template #toolbar_buttons>
             <slot name="toolbar_buttons">
               <div class="toolsBarContainer">
