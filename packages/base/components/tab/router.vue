5d08e77b3b7a019480544623d3a0cef604b423f9
Seantsang <9394wing@gmail.com>
2025-05-21 10:05:47 +0800

    fix component

--- packages/base/components/tab/router.vue
+++ packages/base/components/tab/router.vue
@@ -298,34 +298,11 @@ onUnmounted(() => {
       </div>
     </Teleport>
 
-    <template v-if="tab.initized">
+    <template v-if="tab.initized && renderComponent">
       <Transition>
         <KeepAlive :exclude="/Dead/" :max="2">
           <Suspense>
-            <template v-if="!tab.handleError" >
-            <NuxtErrorBoundary ref="errorBoundary" @error="handleError">
-              <component v-if="renderComponent" :is="tab.component" :tab="tab" v-bind="tab.props" />
-              <template #error="{ error, clearError }">
-                <div class="errorBoundaryContainer">
-                  <div class="messageContainer">
-                    <h3 class="errorTitle">ERROR :  {{ $t(tab.label) }}</h3>
-                    <pre>
-                       {{ error }}
-                    </pre>
-                    <pre>
-                      {{ tab }}
-                    </pre>
-                    <el-button  :icon="Refresh" @click="clearError">
-                      {{ $t('common_refresh') }}
-                    </el-button>
-                  </div>
-                </div>
-              </template>
-            </NuxtErrorBoundary>
-            </template>
-            <template v-else>
-              <component v-if="renderComponent"  :is="tab.component" :tab="tab" v-bind="tab.props" />
-            </template>
+            <component :is="tab.component" :tab="tab" v-bind="tab.props" />
             <template #fallback>
               <LoadingBgInline />
             </template>
