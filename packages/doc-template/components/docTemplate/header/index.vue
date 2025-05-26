cf4e9af85ae2470d049320b38c9421c45270ff84
Seantsang <9394wing@gmail.com>
2025-05-26 16:27:04 +0800

    udpate

--- packages/doc-template/components/docTemplate/header/index.vue
+++ packages/doc-template/components/docTemplate/header/index.vue
@@ -3,7 +3,7 @@ import { DocTemplateProveKey } from '~/utils/docTempalteHelper';
 import VariableManager from '../VariableManager.vue'
 import { ref, toRefs } from 'vue'
 import type { ToolSection } from '~/utils/docTempalteHelper'
-import type { TipTapOptions } from 'docpal-document-editor'
+import type { TipTapOptions } from 'docpal-document-editor/src/types'
 
 const editorProvider = inject(DocTemplateProveKey)
 if(!editorProvider) {
