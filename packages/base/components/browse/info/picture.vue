38969b886ab04b32b854cc9c7b29725f9fadd013
OY <oy.ou@wclsolution.com>
2025-05-28 16:25:42 +0800

    fileSizeFilter

--- packages/base/components/browse/info/picture.vue
+++ packages/base/components/browse/info/picture.vue
@@ -44,7 +44,7 @@ function fileSizeFilter(bytes: any) {
   bytes = Number(bytes)
   const units = ['B', 'KB', 'MB', 'GB', 'TB']
   let unit = ''
-  for (let i = 0; bytes / 1024 >= 1; i++) {
+  for (let i = 1; bytes / 1024 >= 1; i++) {
     unit = units[i]
     bytes = bytes / 1024
   }
