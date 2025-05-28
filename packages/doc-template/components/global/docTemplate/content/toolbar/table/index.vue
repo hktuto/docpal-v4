a6daf42b71c9a958e5844144885bed441fe4739d
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-05-28 15:26:18 +0800

    [update] 更新頁面佈局 ，新增table的輔助按鈕框，選中多個單元格允許合并與拆分
    (cherry picked from commit 7e81e74eba1cc57b1e37c7c7083a6e0c1f2df004)

--- packages/doc-template/components/global/docTemplate/content/toolbar/table/index.vue
+++ packages/doc-template/components/global/docTemplate/content/toolbar/table/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { DocTemplateProveKey } from '~/utils/docTempalteHelper'
 
-const { editor, options, initEditor } = inject(DocTemplateProveKey)
+const { editor, options } = inject(DocTemplateProveKey)
 const { t } = useI18n()
 const fontPredefineBackgroundColors = ref(['#FFFFFF', '#97FC00', '#00DDFF'])
 const fontPredefineThBackgroundColors = ref(['#ADADAD', '#fc8f00', '#00b2ff'])
@@ -127,76 +127,115 @@ function handleBackgroundColor(backgroundColor: string) {
 </script>
 
 <template>
-  <div>
-    <el-popover :visible="state.createTablePopoverVisible" placement="bottom" :width="300" title="Create Table"
-                trigger="click">
-      <template #reference>
-        <el-button style="margin-right: 16px" @click="state.createTablePopoverVisible = true">
-          {{ $t('Create table') }}
-        </el-button>
-      </template>
-      <el-form ref="createTableFormRef" label-position="left" label-width="auto">
-        <el-form-item label="Row" prop="desc">
-          <el-input v-model="state.tableRow" />
-        </el-form-item>
-        <el-form-item label="Col" prop="desc">
-          <el-input v-model="state.tableCol" />
-        </el-form-item>
-        <el-form-item label="With Header Row" prop="delivery">
-          <el-switch v-model="state.withHeaderRow" />
-        </el-form-item>
-        <div style="display: flex; justify-content: flex-end; margin: 0">
-          <el-button @click="state.createTablePopoverVisible = false">
-            Cancel
+  <el-row>
+    <el-col :span="24">
+      <el-popover :visible="state.createTablePopoverVisible" placement="bottom" :width="300" :title="t('Create Table')"
+                  trigger="click">
+        <template #reference>
+          <el-button @click="state.createTablePopoverVisible = true">
+            {{ $t('Create Table') }}
           </el-button>
-          <el-button type="primary" @click="handlePopoverCreateTable">
-            Create
-          </el-button>
-        </div>
-      </el-form>
-    </el-popover>
+        </template>
+        <el-form ref="createTableFormRef" label-position="left" label-width="auto">
+          <el-form-item label="Row" prop="desc">
+            <el-input v-model="state.tableRow" />
+          </el-form-item>
+          <el-form-item label="Col" prop="desc">
+            <el-input v-model="state.tableCol" />
+          </el-form-item>
+          <el-form-item label="With Header Row" prop="delivery">
+            <el-switch v-model="state.withHeaderRow" />
+          </el-form-item>
+          <div style="display: flex; justify-content: flex-end; margin: 0">
+            <el-button @click="state.createTablePopoverVisible = false">
+              {{ $t('Cancel') }}
+            </el-button>
+            <el-button type="primary" @click="handlePopoverCreateTable">
+              {{ $t('Create') }}
+            </el-button>
+          </div>
+        </el-form>
+      </el-popover>
 
-    <div>
-      <el-button-group>
-        <el-button @click="handleDeleteTable">
-          {{ t('Delete table') }}
-        </el-button>
-        <el-button @click="handleTableDeleteRowOrCol(true)">
-          {{ t('Delete Row') }}
-        </el-button>
-        <el-button @click="handleTableDeleteRowOrCol(false)">
-          {{ t('Delete Col') }}
-        </el-button>
-      </el-button-group>
+      <el-divider direction="vertical" />
 
       <el-button-group>
         <el-button @click="handleTableCreateRow(true)">
-          {{ t('Add row up') }}
+          {{ $t('Add Row Up') }}
         </el-button>
         <el-button @click="handleTableCreateRow(false)">
-          {{ t('Add row down') }}
+          {{ $t('Add Row Down') }}
         </el-button>
       </el-button-group>
+
+      <el-divider direction="vertical" />
 
       <el-button-group>
         <el-button @click="handleTableCreateCol(true)">
-          {{ t('Add col left') }}
+          {{ $t('Add col left') }}
         </el-button>
         <el-button @click="handleTableCreateCol(false)">
-          {{ t('Add col right') }}
+          {{ $t('Add col right') }}
         </el-button>
       </el-button-group>
-    </div>
 
-    <div>
+      <el-divider direction="vertical" />
+
+      <el-button-group>
+        <el-button @click="handleDeleteTable">
+          {{ $t('Delete table') }}
+        </el-button>
+        <el-button @click="handleTableDeleteRowOrCol(true)">
+          {{ $t('Delete Row') }}
+        </el-button>
+        <el-button @click="handleTableDeleteRowOrCol(false)">
+          {{ $t('Delete Col') }}
+        </el-button>
+      </el-button-group>
+
+      <el-divider direction="vertical" />
+
+      <el-button-group>
+        <el-button @click="handleTableCellsMergeOrSplit(true)">
+          {{ $t('Merge cells') }}
+        </el-button>
+        <el-button @click="handleTableCellsMergeOrSplit(false)">
+          {{ $t('Split cell') }}
+        </el-button>
+      </el-button-group>
+
+
+      <el-divider direction="vertical" />
+      <el-button-group>
+        <el-button @click="handleBackgroundColor(state.tableBackgroundColor)">
+          {{ $t('Set Table Background Color') }}
+        </el-button>
+        <el-button style="width: 22px;">
+          <el-color-picker v-model="state.tableBackgroundColor" :predefine="fontPredefineBackgroundColors"
+                           @blur="handleBackgroundColor(state.tableBackgroundColor)" />
+        </el-button>
+      </el-button-group>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="24">
       <el-button-group>
         <el-button @click="handleTableToggleHeaderRowOrColumn(true)">
-          Toggle header row
+          {{ $t('Toggle header row') }}
         </el-button>
         <el-button @click="handleTableToggleHeaderRowOrColumn(false)">
-          Toggle header column
+          {{ $t('Toggle header column') }}
         </el-button>
-        <el-button style="width: 9%;">
+      </el-button-group>
+
+      <el-divider direction="vertical" />
+
+      <el-button-group>
+        <el-button @click="handleThBackgroundColor(state.tableThBackgroundColor)">
+          {{ $t('Set Header Background Color') }}
+        </el-button>
+
+        <el-button style="width: 22px;">
           <el-color-picker v-model="state.tableThBackgroundColor" :predefine="fontPredefineThBackgroundColors"
                            @blur="handleThBackgroundColor(state.tableThBackgroundColor)" />
         </el-button>
@@ -205,51 +244,51 @@ function handleBackgroundColor(backgroundColor: string) {
       <el-divider direction="vertical" />
 
       <el-button-group>
-        <el-button @click="handleTableCellsMergeOrSplit(true)">
-          Merge cells
+        <el-button @click="handleClearOuterBoundaries">
+          {{ $t('Clear Outer Boundaries') }}
         </el-button>
-        <el-button @click="handleTableCellsMergeOrSplit(false)">
-          Split cell
+
+        <el-button @click="handleShowOuterBoundaries">
+          {{ $t('Show Outer Boundaries') }}
         </el-button>
       </el-button-group>
-    </div>
 
-    <!--    <el-button @click="handleFixTables">-->
-    <!--      Fix-->
-    <!--    </el-button>-->
+      <el-divider direction="vertical" />
 
-    <el-button @click="handleClearOuterBoundaries">
-      Clear Outer Boundaries
-    </el-button>
+      <el-button-group>
+        <el-button @click="handleClearInnerBoundaries">
+          {{ $t('Clear Inner Boundaries') }}
+        </el-button>
 
-    <el-button @click="handleShowOuterBoundaries">
-      Show Outer Boundaries
-    </el-button>
+        <el-button @click="handleShowInnerBoundaries">
+          {{ $t('Show Inner Boundaries') }}
+        </el-button>
+      </el-button-group>
+    </el-col>
 
-     <el-button @click="handleClearInnerBoundaries">
-       Clear Inner Boundaries
-     </el-button>
-
-     <el-button @click="handleShowInnerBoundaries">
-       Show Inner Boundaries
-     </el-button>
-
-    <el-button-group>
-      <el-button @click="handleBackgroundColor(state.tableBackgroundColor)">
-        <p>Set Table Background Color</p>
-      </el-button>
-      <el-button style="width: 12%;">
-        <el-color-picker v-model="state.tableBackgroundColor" :predefine="fontPredefineBackgroundColors"
-                         @blur="handleBackgroundColor(state.tableBackgroundColor)" />
-      </el-button>
-    </el-button-group>
-  </div>
+    <el-col :span="24">
+      <!--    <el-button @click="handleFixTables">-->
+      <!--      Fix-->
+      <!--    </el-button>-->
+    </el-col>
+  </el-row>
 
   <div style="text-align: center; margin-top: 9px;">
-    {{ t('Table') }}
+    {{ $t('Table') }}
   </div>
 </template>
 
 <style scoped lang="scss">
+.el-row {
+  margin-bottom: 10px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 2px;
+}
 
 </style>
