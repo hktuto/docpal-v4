4c8da16a74848b3041ec2622e5804e25c0daf473
OY <oy.ou@wclsolution.com>
2025-05-28 16:13:29 +0800

    test

--- pages/admin-document-type/components/docType/relatedTypeTable.vue
+++ pages/admin-document-type/components/docType/relatedTypeTable.vue
@@ -122,6 +122,8 @@ function handleFilterFormChange(formModel: any) {
 
 async function getList() {
   ResponsiveFilterRef.value.handleFilter()
+  console.log(adminApi.api);
+  
   _list = await adminApi.api
     .getDocpaltypeSettingsNameNameRelated(props.name)
     .then((res) => res.data)
