c10b516daa2a385c15c50d4afd04b82c218b7ed8
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-06-03 00:20:56 +0800

    [update] 更新dashboard的browse
    (cherry picked from commit 0f9092d0e24db508083e4892392b3c6901195229)

--- packages/dp-dashboard/components/global/personal/browse/index.vue
+++ packages/dp-dashboard/components/global/personal/browse/index.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
-import { provide } from 'vue'
-import { clientApi } from 'api'
-import { BrowseTable } from '#components'
-import { actions, ActionsFilter, BrowseListProviderKey } from '#imports'
+import { BrowseTablePage, PersonalBrowseSetting } from '#components'
 
 const emits = defineEmits(['delete', 'refreshSetting'])
 const { t } = useI18n()
@@ -18,22 +15,19 @@ const props = withDefaults(defineProps<{
   home: [],
   commentId: '',
   setting: {},
-  hideSetting: true
+  hideSetting: true,
 })
 
-const { idOrPath, commentId } = toRefs(props)
-const tabProvider = inject(TabManagerKey)
-const routerProvider = inject(MenuRouterKey)
-const selectedItem = ref<any[]>([])
-const infoOpened = ref(false)
-if (!tabProvider || !routerProvider) {
-  throw createError('provider not found')
-}
+const state = reactive({
+  path: '/Domain'
+})
 
-const { cardRef, refresh, loading, } = useDashboardCard({
+const tableRef = ref()
+
+const { cardRef, settingRef, refresh, loading } = useDashboardCard({
   props,
   handleRefreshAction: (setting: any) => {
-    settingRef.value.handleOpen({})
+    // tableRef.value.query({})
   }
 })
 
@@ -41,83 +35,9 @@ async function handleDelete() {
   emits('delete')
 }
 
-function handleRefresh(chartSetting: any) {
-  emits('refreshSetting', chartSetting)
-}
-
-const tableRef = ref<InstanceType<typeof BrowseTable>>()
-
-function addToSelection(items: any[]) {
-  selectedItem.value.push(...items)
-}
-
-function removeFromSelection(items: any[]) {
-  selectedItem.value = selectedItem.value.filter((item) => !items.includes(item))
-}
-
-function changeRoute(path: string) {
-  routerProvider?.updateProps({
-    idOrPath: path
-  })
-}
-
-const docDetail = ref()
-const docPermission = ref()
-const selectedList = ref<any[]>([])
-
-
-function selectedChangeHandler(selectedRows: any[]) {
-  selectedList.value = selectedRows
-}
-
-function closePreview({ detail }: any) {
-  if (!detail) return
-  if (detail.id === docDetail.value.id) {
-    const newItem = createBrowseListPageParams({
-      idOrPath: docDetail.value.parentRef
-    })
-    routerProvider?.navigateTo(newItem)
-  }
-}
-
-const docActions = computed(() => {
-  if (!docDetail.value || !docPermission.value) return {}
-  if (selectedList.value.length > 0) {
-    return ActionsFilter(actions, docPermission.value, 'showInShare')
-  }
-  return ActionsFilter(actions, docPermission.value, 'showInFolder')
-})
-
-function handleClearSelected() {
-  if (tableRef.value) {
-    tableRef.value.cleanSelected()
-  }
-}
-
-function itemDeleted() {
-}
-
-const settingRef = ref()
-
-provide(BrowseListProviderKey, {
-  getchildApi: (pageParams: any) => {
-    return clientApi.api.postNuxeoDocumentChildrenThumbnailV2(pageParams)
-  },
-  idOrPath,
-  docDetail,
-  docPermission,
-  changeRoute,
-  addToSelection,
-  removeFromSelection
-})
-
-function handleRefresh1(setting: any){
-  // props.setting.idOrPath = setting.path
-  // refresh()
-}
-
-function openSetting(){
-  settingRef.value.handleOpen(props.setting)
+function handleRefresh(data: any) {
+  state.path = data.path
+  refresh()
 }
 
 </script>
@@ -134,51 +54,8 @@ function openSetting(){
     @delete="handleDelete"
     @refresh="refresh"
   >
-    <BrowseTable ref="tableRef"
-                 :class="{ selected: selectedList.length > 0 }"
-                 :selectedRows="selectedItem"
-                 @selectedChange="selectedChangeHandler">
-      <template #toolbar_buttons>
-        <slot name="toolbar_buttons">
-          <div class="toolsBarContainer">
-            <template v-if="selectedList.length === 0">
-              <BrowseBreadcrumb :idOrPath="idOrPath" :home="home" />
-            </template>
-            <template v-else>
-              <div class="selectedNoteContainer">
-                {{ $t('dpDocument_fileSelected') }}: {{ selectedList.length }}
-                <Icon name="mdi:close" @click="handleClearSelected" />
-              </div>
-            </template>
-          </div>
-        </slot>
-        <slot name="toolbarTools">
-          <CollapseMenu v-if="idOrPath !== '/'">
-            <template #default="{ collapse }">
-              <template v-for="(group, key) in docActions" :key="key">
-                <template v-for="item in group" :key="item.name">
-                  <component
-                    :is="item.component"
-                    :doc="docDetail"
-                    :permission="docPermission"
-                    :selectedList="selectedList"
-                    @clearSelected="handleClearSelected"
-                    @success="handleRefresh"
-                    @delete="itemDeleted"
-                  />
-                </template>
-                <div :class="{ actionDivider: true, collapse }"></div>
-              </template>
-            </template>
-          </CollapseMenu>
-          <BrowseActionsInfo v-if="idOrPath !== '/'" :doc="docDetail" :permission="docPermission"
-                             @itemClicked="infoOpened = !infoOpened" />
-        </slot>
-      </template>
-    </BrowseTable>
-
-    <BrowseSetting ref="settingRef" @refresh="handleRefresh" />
-
+    <BrowseTablePage ref="tableRef" :idOrPath="state.path" />
+    <PersonalBrowseSetting ref="settingRef" :path="state.path" @refresh="handleRefresh" />
   </DashboardCard>
 </template>
 
--- packages/dp-dashboard/components/global/personal/browse/setting.vue
+++ packages/dp-dashboard/components/global/personal/browse/setting.vue
@@ -1,62 +1,62 @@
 <script lang="ts" setup>
 import formJson from './setting.vform.json'
 
+const emits = defineEmits(['refresh', 'delete'])
 const { t } = useI18n()
 const state = reactive({
   visible: false,
   loading: false,
-  formJson: {}, 
+  formJson: {}
 })
+const props = defineProps<{
+  path: string
+}>()
 
-function handleOpen(setting: any){
+function handleOpen() {
   state.visible = true
   setTimeout(async () => {
-    state.setting = setting;
-    await FormRendererRef.value.vFormRenderRef.setFormData(setting);
-    state.loading = false;
-  });
+    await FormRendererRef.value.vFormRenderRef.setFormData( {path: props.path} )
+    state.loading = false
+  })
 }
 
-const FormRendererRef = ref();
+const FormRendererRef = ref()
 
-async function handleSubmit(){
+async function handleSubmit() {
   state.loading = true
   try {
-    const data = await FormRendererRef.value.getFormData();
-    if(!data.path){
+    const data = await FormRendererRef.value.getFormData()
+    if (!data.path) {
       data.path = '/'
     }
-    // TODO 保存設定的路徑
-    emits("refresh", handleReload(data));
+    emits('refresh', data)
   } catch (error) {
-    state.loading = false;
+    state.loading = false
   }
   state.loading = false
   state.visible = false
 }
 
-defineExpose({handleOpen});
+defineExpose({ handleOpen })
 </script>
 
 <template>
-<el-dialog
+  <el-dialog
     v-model="state.visible"
     :title="t('dashboard.setting')"
     class="scroll-dialog"
     append-to-body
     :close-on-click-modal="false"
-    @close="handleClose"
   >
-    <FormRenderer ref="FormRendererRef" :form-json="formJson"/>
+    <FormRenderer ref="FormRendererRef" :form-json="formJson" />
     <template #footer>
       <div class="footer-grid">
         <el-button id="Browse__Setting__Submit" type="primary" :loading="state.loading" @click="handleSubmit">
-          {{ $t("common_submit") }}
+          {{ $t('common_submit') }}
         </el-button>
       </div>
     </template>
   </el-dialog>
-
 </template>
 
 <style scoped lang="scss">
