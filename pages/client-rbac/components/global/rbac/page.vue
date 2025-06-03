e85e4093a4d867787fd4897af9cb191788ed9a8d
Seantsang <9394wing@gmail.com>
2025-06-03 14:07:01 +0800

    temp client rbac page

---
+++ pages/client-rbac/components/global/rbac/page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pageContainer--padding">
+    <!-- TODO : add user rold id to chart -->
+    <RbacOrgChart />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const activeName = ref('chart')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
