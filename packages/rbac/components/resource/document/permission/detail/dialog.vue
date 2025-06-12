c12a9c0719cae36da8c127494a5e80ae40687652
OY <oy.ou@wclsolution.com>
2025-06-12 13:41:48 +0800

    format

--- packages/rbac/components/resource/document/permission/detail/dialog.vue
+++ packages/rbac/components/resource/document/permission/detail/dialog.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-dialog
-    v-model="dialogVisible"
-    title="权限详情"
-    width="80%"
-    :close-on-click-modal="false"
-    @close="handleClose"
-  >
+  <el-dialog v-model="dialogVisible" title="权限详情" width="80%" :close-on-click-modal="false" @close="handleClose">
     <div class="permission-detail-content">
       <!-- 权限详情内容 -->
       <ResourceDocumentPermissionDetailForm ref="formRef" />
@@ -27,44 +21,44 @@ const dialogVisible = ref(false)
 const permissionId = ref('')
 const formRef = ref()
 type FormData = {
-  resourceId: string,
-  resourceType: number, // (1=Document)
-  targetType: number, // (1=User, 2=Role, 3=Group, 4=User Set)
-  targetId: string, 
-  permissionLevel: number, //(1=Read, 2=ReadWrite, 3=Manage, 4=Custom, 5=Configuration Set)
-  permissionIds: number[],
-  configurationRuleName: string,
-  members: any[], //TODO : create type
+  resourceId: string
+  resourceType: number // (1=Document)
+  targetType: number // (1=User, 2=Role, 3=Group, 4=User Set)
+  targetId: string
+  permissionLevel: number //(1=Read, 2=ReadWrite, 3=Manage, 4=Custom, 5=Configuration Set)
+  permissionIds: number[]
+  configurationRuleName: string
+  members: any[] //TODO : create type
   rules: any[] //TODO : create type
 }
 
 async function getFormData(id: string) {
   // @ts-ignore
-  const res = await adminApi.api.getAclResourcePermissionsResourceid(id).then((res) => res) as any
+  const res = (await adminApi.api.getAclResourcePermissionsResourceid(id).then((res) => res)) as any
   return res
 }
 
-async function open (id: string, documentId: string)  {
-  if(!documentId) {
+async function open(id: string, documentId: string) {
+  if (!documentId) {
     throw new Error('documentId is required')
   }
   permissionId.value = id
   dialogVisible.value = true
-  let formData: FormData;
+  let formData: FormData
   // if id is not null, get form data from api
   if (id) {
     formData = await getFormData(id)
-  }else{
+  } else {
     formData = {
       resourceId: documentId,
       resourceType: 1,
       targetType: 1,
       targetId: '',
       permissionLevel: 3,
-      permissionIds:[],
-      configurationRuleName: "",
+      permissionIds: [],
+      configurationRuleName: '',
       members: [],
-      rules:[]
+      rules: []
     }
   }
   formRef.value?.setFormData(formData)
