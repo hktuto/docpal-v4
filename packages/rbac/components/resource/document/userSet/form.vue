f64a4773036ec3c4aaf18615473507dd3cc82d31
Seantsang <9394wing@gmail.com>
2025-06-13 17:59:58 +0800

    udpate sorting

---
+++ packages/rbac/components/resource/document/userSet/form.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+const formData = ref()
+function setFormData(data:any){
+}
+
+function getFormData(){
+
+}
+
+defineExpose({
+  setFormData,
+  getFormData
+})
+</script>
+<template>
+  <ElForm :model="formData" label-position="top">
+    
+  </ElForm>
+</template>
