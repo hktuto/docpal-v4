7c2b5bdf5078f75057580ca84e9622b7cceb3594
oy <OY.Ou@weltronics.com>
2025-07-01 16:04:46 +0800

    [password-policy-config-admin] init

--- apps/admin/app.config.ts
+++ apps/admin/app.config.ts
@@ -1,178 +1,179 @@
-
 export default defineAppConfig({
-    appMenu:[
+  appMenu: [
+    {
+      name: '',
+      icon: 'tabler:align-box-right-bottom',
+      hoverIcon: 'tabler:align-box-right-bottom-filled',
+      label: 'adminMenu.User',
+      children: [
         {
-            name:"",
-            icon: "tabler:align-box-right-bottom",
-            hoverIcon: "tabler:align-box-right-bottom-filled",
-            label:"adminMenu.User",
-            children:[
-                {
-                    name:"admin-user"
-                },
-                {
-                    name:"admin-group"
-                },
-                {
-                  name: "RBAC-editor"
-                },
-                {
-                  name: "RBAC-page"
-                }
-            ]
+          name: 'admin-user'
         },
         {
-            name:"admin-bulk-import"
+          name: 'admin-group'
         },
         {
-            label: "adminMenu.shareInternal",
-            icon: "lucide:file-symlink",
-            hoverIcon: "lucide:file-symlink",
-            children:[
-                {
-                    name: 'admin-internal-share'
-                },
-                {
-                    name: 'admin-external-share'
-                },
-                // {
-                //     name: 'admin-share-list'
-                // }
-            ]
-
+          name: 'RBAC-editor'
         },
         {
-            label: "adminMenu.browse",
-            icon: "lucide:file-pen",
-            hoverIcon: "lucide:file-symlink",
-            children:[
-                {
-                    name:"admin-smart-folder"
-                },
-                {
-                    name:"admin-document-type"
-                },
-                {
-                    name:"admin-folder-cabinet"
-                },
-                {
-                    name: "admin-dam"
-                }
-            ]
+          name: 'RBAC-page'
+        }
+      ]
+    },
+    {
+      name: 'admin-bulk-import'
+    },
+    {
+      label: 'adminMenu.shareInternal',
+      icon: 'lucide:file-symlink',
+      hoverIcon: 'lucide:file-symlink',
+      children: [
+        {
+          name: 'admin-internal-share'
         },
         {
-            name:"admin-watermark"
+          name: 'admin-external-share'
+        }
+        // {
+        //     name: 'admin-share-list'
+        // }
+      ]
+    },
+    {
+      label: 'adminMenu.browse',
+      icon: 'lucide:file-pen',
+      hoverIcon: 'lucide:file-symlink',
+      children: [
+        {
+          name: 'admin-smart-folder'
         },
         {
-            label: "adminMenu.workflow",
-            icon:"dp-icon:flow-outline",
-            children:[
-                {
-                    name:"admin-workflow-manage"
-                },
-                {
-                    name:"admin-workflow-retry"
-                },
-                {
-                    name:"admin-workflow-editor"
-                },
-                // {
-                //     name: "super-workflow-form"
-                // }
-            ]
+          name: 'admin-document-type'
         },
         {
-            label:"adminMenu.dashboard",
-            icon: "material-symbols:dashboard-customize-outline-rounded",
-            children:[
-                {
-                    name:"admin-dashboard"
-                },
-                {
-                    name:"admin-work-panel"
-                }
-            ]
+          name: 'admin-folder-cabinet'
         },
         {
-            label:"adminMenu.templateManagement",
-            icon: "lucide:layout-template",
-            children:[
-                {
-                    name: "admin-document-template"
-                },
-                {
-                  name : "admin-doc-template-collaboration"
-                },
-                {
-                    name: "admin-email-template"
-                },
-                {
-                    name: 'admin-message-template'
-                }
-            ]
+          name: 'admin-dam'
+        }
+      ]
+    },
+    {
+      name: 'admin-watermark'
+    },
+    {
+      label: 'adminMenu.workflow',
+      icon: 'dp-icon:flow-outline',
+      children: [
+        {
+          name: 'admin-workflow-manage'
         },
         {
-            label:"adminMenu.policy",
-            icon: "lucide:book-lock",
-            children:[
-                {
-                    name: "admin-hold-policies"
-                },
-                {
-                    name: "admin-retention-policies"
-                }
-            ]
+          name: 'admin-workflow-retry'
         },
         {
-            name:"admin-master-table"
+          name: 'admin-workflow-editor'
+        }
+        // {
+        //     name: "super-workflow-form"
+        // }
+      ]
+    },
+    {
+      label: 'adminMenu.dashboard',
+      icon: 'material-symbols:dashboard-customize-outline-rounded',
+      children: [
+        {
+          name: 'admin-dashboard'
         },
         {
-            label:"adminMenu.log",
-            icon: "lucide:logs",
-            children:[
-                {
-                    name: "admin-log-manage"
-                },
-                {
-                    name: "admin-audit"
-                },
-                {
-                    name: 'admin-message-queue'
-                },
-                {
-                    name: "admin-mail-config"
-                }
-            ]
+          name: 'admin-work-panel'
+        }
+      ]
+    },
+    {
+      label: 'adminMenu.templateManagement',
+      icon: 'lucide:layout-template',
+      children: [
+        {
+          name: 'admin-document-template'
         },
         {
-            label:"adminMenu.system",
-            icon :"lucide:server",
-            children:[
-                {
-                    name:"admin-acl"
-                },
-                {
-                    name:"admin-external-connection"
-                },
-            ]
+          name: 'admin-doc-template-collaboration'
         },
         {
-            name:'admin-azure'
+          name: 'admin-email-template'
         },
         {
-            name:"admin-case-management"
+          name: 'admin-message-template'
+        }
+      ]
+    },
+    {
+      label: 'adminMenu.policy',
+      icon: 'lucide:book-lock',
+      children: [
+        {
+          name: 'admin-hold-policies'
         },
         {
-            name:"admin-easy-form"
+          name: 'admin-retention-policies'
+        }
+      ]
+    },
+    {
+      name: 'admin-master-table'
+    },
+    {
+      label: 'adminMenu.log',
+      icon: 'lucide:logs',
+      children: [
+        {
+          name: 'admin-log-manage'
         },
         {
-            name:"admin-calendar-setting"
+          name: 'admin-audit'
         },
         {
-            name: "admin-setting"
+          name: 'admin-message-queue'
         },
         {
-            name: 'admin-unique-id-generator'
+          name: 'admin-mail-config'
+        }
+      ]
+    },
+    {
+      label: 'adminMenu.system',
+      icon: 'lucide:server',
+      children: [
+        {
+          name: 'admin-acl'
         },
-    ]
+        {
+          name: 'admin-external-connection'
+        },
+        {
+          name: 'admin-password-policy'
+        }
+      ]
+    },
+    {
+      name: 'admin-azure'
+    },
+    {
+      name: 'admin-case-management'
+    },
+    {
+      name: 'admin-easy-form'
+    },
+    {
+      name: 'admin-calendar-setting'
+    },
+    {
+      name: 'admin-setting'
+    },
+    {
+      name: 'admin-unique-id-generator'
+    }
+  ]
 })
