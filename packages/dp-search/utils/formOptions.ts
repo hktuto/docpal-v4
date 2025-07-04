49466c18fce1d55925b053f39b443d8fb3cb79fa
oy <OY.Ou@weltronics.com>
2025-07-04 14:50:13 +0800

    [search]path

--- packages/dp-search/utils/formOptions.ts
+++ packages/dp-search/utils/formOptions.ts
@@ -1,5 +1,4 @@
-
-import { clientApi, adminApi } from "api";
+import { clientApi, adminApi } from 'api'
 
 export const conditionType = [
   { label: 'authors', value: 'authors' },
@@ -15,7 +14,8 @@ export const conditionType = [
   { label: 'size', value: 'size' },
   { label: 'createdDate', value: 'createdDate' },
   { label: 'modified', value: 'modified' },
-];
+  { label: 'path', value: 'path' }
+]
 export const languages = [
   { label: 'chi_sim', value: 'chi_sim' },
   { label: 'chi_tra', value: 'chi_tra' },
@@ -24,109 +24,110 @@ export const languages = [
   { label: 'jpn', value: 'jpn' },
   { label: 'kor', value: 'kor' },
   { label: 'tha', value: 'tha' },
-  { label: 'vie', value: 'vie' },
-];
+  { label: 'vie', value: 'vie' }
+]
 export const sizes = [
   { label: 'searchType.100', value: '100' },
   { label: 'searchType.1000', value: '1000' },
   { label: 'searchType.10000', value: '10000' },
   { label: 'searchType.100000', value: '100000' },
-  { label: 'searchType.1000000', value: '1000000' },
-];
+  { label: 'searchType.1000000', value: '1000000' }
+]
 
 export const mimeTypes = [
   {
-    label: "images",
+    label: 'images',
     value: 'images',
     options: [
-      { label: "All Image", value: "image/*" },
-      { label: "JPG", value: "image/jpeg" },
-      { label: "PNG", value: "image/png" },
-      { label: "GIF", value: "image/gif" },
-      { label: "PDF", value: "application/pdf" },
-      { label: "SVG", value: "image/svg+xml" },
-      { label: "TIFF", value: "image/tiff" },
-      { label: "WEBP", value: "image/webp" },
-      { label: "BMP", value: "image/bmp" },
+      { label: 'All Image', value: 'image/*' },
+      { label: 'JPG', value: 'image/jpeg' },
+      { label: 'PNG', value: 'image/png' },
+      { label: 'GIF', value: 'image/gif' },
+      { label: 'PDF', value: 'application/pdf' },
+      { label: 'SVG', value: 'image/svg+xml' },
+      { label: 'TIFF', value: 'image/tiff' },
+      { label: 'WEBP', value: 'image/webp' },
+      { label: 'BMP', value: 'image/bmp' }
     ]
   },
   {
-    label: "Documents",
+    label: 'Documents',
     value: 'Documents',
     options: [
-      { label: "All Document", value: "application/*" },
-      { label: "DOC", value: "application/msword" },
-      { label: "DOCX", value: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
-      { label: "XLS", value: "application/vnd.ms-excel" },
-      { label: "XLSX", value: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
-      { label: "PPT", value: "application/vnd.ms-powerpoint" },
-      { label: "PPTX", value: "application/vnd.openxmlformats-officedocument.presentationml.presentation" },
+      { label: 'All Document', value: 'application/*' },
+      { label: 'DOC', value: 'application/msword' },
+      { label: 'DOCX', value: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' },
+      { label: 'XLS', value: 'application/vnd.ms-excel' },
+      { label: 'XLSX', value: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' },
+      { label: 'PPT', value: 'application/vnd.ms-powerpoint' },
+      { label: 'PPTX', value: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' }
     ]
   },
   {
-    label: "Videos",
+    label: 'Videos',
     value: 'Videos',
     options: [
-      { label: "All Video", value: "video/*" },
-      { label: "MP4", value: "video/mp4" },
-      { label: "AVI", value: "video/x-msvideo" },
-      { label: "WMV", value: "video/x-ms-wmv" },
-      { label: "FLV", value: "video/x-flv" },
-      { label: "MKV", value: "video/x-matroska" },
+      { label: 'All Video', value: 'video/*' },
+      { label: 'MP4', value: 'video/mp4' },
+      { label: 'AVI', value: 'video/x-msvideo' },
+      { label: 'WMV', value: 'video/x-ms-wmv' },
+      { label: 'FLV', value: 'video/x-flv' },
+      { label: 'MKV', value: 'video/x-matroska' }
     ]
   },
   {
-    label: "Audios",
+    label: 'Audios',
     value: 'Audios',
     options: [
-      { label: "All Audio", value: "audio/*" },
-      { label: "MP3", value: "audio/mpeg" },
-      { label: "WAV", value: "audio/wav" },
-      { label: "OGG", value: "audio/ogg" },
-      { label: "FLAC", value: "audio/flac" },
-      { label: "AAC", value: "audio/aac" },
+      { label: 'All Audio', value: 'audio/*' },
+      { label: 'MP3', value: 'audio/mpeg' },
+      { label: 'WAV', value: 'audio/wav' },
+      { label: 'OGG', value: 'audio/ogg' },
+      { label: 'FLAC', value: 'audio/flac' },
+      { label: 'AAC', value: 'audio/aac' }
     ]
   }
-];
+]
 export function sortListWithI18n(list: any, prefix = '') {
   list = JSON.parse(JSON.stringify(list))
   // @ts-ignore
-  const t = window.$t;
+  const t = window.$t
   const _list = list.map((item: any) => {
-    item.label = t(prefix+item.label);
-    return item;
-  });
-  return _list.sort((a: any, b: any) => a.label.localeCompare(b.label));
+    item.label = t(prefix + item.label)
+    return item
+  })
+  return _list.sort((a: any, b: any) => a.label.localeCompare(b.label))
 }
 export const getMetadataOptions = async () => {
   try {
-    const { public: { platform } } = useRuntimeConfig();
+    const {
+      public: { platform }
+    } = useRuntimeConfig()
     /// TODO: depecate in next version
-    let globalType;
+    let globalType
     if (platform === 'admin') {
-      const { data }: any = await adminApi.api.getDocpaltypeSettingsMetadataDocumenttype('GlobalFile');
-      globalType = data.keywords;
+      const { data }: any = await adminApi.api.getDocpaltypeSettingsMetadataDocumenttype('GlobalFile')
+      globalType = data.keywords
     } else {
-      const { data } = await clientApi.api.getNuxeoTypesDocumenttype('GlobalFile');
-      globalType = data?.keywords;
-  
+      const { data } = await clientApi.api.getNuxeoTypesDocumenttype('GlobalFile')
+      globalType = data?.keywords
     }
     const optionList = globalType.map((item: any) => ({
       ...item,
       label: item.name,
       value: item.name
-    }));
-    return optionList;
+    }))
+    return optionList
   } catch (error) {
     return []
   }
-};
+}
 export const getGroupList = async () => {
-  const { data } = await clientApi.api.postNuxeoIdentityGroups() as any;
+  const { data } = (await clientApi.api.postNuxeoIdentityGroups()) as any
   const optionList = data.map((item: any) => ({
     ...item,
     label: item.name,
     value: item.id
-  }));
-  return optionList;
-};
+  }))
+  return optionList
+}
