475c83ff3ead45ff55f8dd22d954f25d125d91cb
oy <OY.Ou@weltronics.com>
2025-07-09 14:37:22 +0800

    DPL-6093 single case no echo displayColumns

--- packages/dp-dashboard/components/global/personal/case/single/index.vue
+++ packages/dp-dashboard/components/global/personal/case/single/index.vue
@@ -47,33 +47,33 @@ async function getCaseDetail(caseId: string) {
 // #region module: tableRef
 function handleShowColumn() {
   const displayColumns = props.setting.displayColumns.reduce((prev: any, columnId: any) => {
-    if (state?.detail?.primaryForm?.fields) {
-      const column = state.detail.primaryForm.fields.find((p: any) => p.id === columnId)
-      if (!!column) {
-        prev.push(column)
-      }
-      // check if the field is default fields
-      if (columnId === 'case_id') {
-        prev.push({ id: 'case_id', name: 'Case Id', minWidth: 200 })
-      } else if (columnId === 'created_date') {
-        prev.push({
-          id: 'created_date',
-          name: 'workflow_createDate',
-          minWidth: 200,
-          formatter({ cellValue }: any) {
-            return formatDate(cellValue)
-          }
-        })
-      } else if (columnId === 'modified_date') {
-        prev.push({
-          id: 'modified_date',
-          name: 'table_modifiedDate',
-          minWidth: 200,
-          formatter({ cellValue }: any) {
-            return formatDate(cellValue)
-          }
-        })
-      }
+    const column = state.detail?.informations?.find((p: any) => p.metadata === columnId)
+    // check if the field is default fields
+    if (columnId === 'case_id') {
+      prev.push({ id: 'case_id', name: 'Case Id', minWidth: 200 })
+    } else if (columnId === 'created_date') {
+      prev.push({
+        id: 'created_date',
+        name: 'workflow_createDate',
+        minWidth: 200,
+        formatter({ cellValue }: any) {
+          return formatDate(cellValue)
+        }
+      })
+    } else if (columnId === 'modified_date') {
+      prev.push({
+        id: 'modified_date',
+        name: 'table_modifiedDate',
+        minWidth: 200,
+        formatter({ cellValue }: any) {
+          return formatDate(cellValue)
+        }
+      })
+    } else if (!!column) {
+      prev.push({
+        id: columnId,
+        name: columnId
+      })
     }
     return prev
   }, [])
