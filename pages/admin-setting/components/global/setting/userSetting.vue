afb5a4fc65786a9fb414d839c505aa5febeadf7c
oy <OY.Ou@weltronics.com>
2025-07-10 14:48:09 +0800

    [unit-test]admin setting

--- pages/admin-setting/components/global/setting/userSetting.vue
+++ pages/admin-setting/components/global/setting/userSetting.vue
@@ -15,22 +15,22 @@ const rules = reactive({
     }
   ]
 })
-const state = reactive({
+const state = reactive<any>({
   visible: false,
   displayFieldList: [],
   systemFieldList: []
 })
-const editForm = reactive({})
+const editForm = reactive<any>({})
 
 function cloneItem(el: any) {
-  if (!state.displayFieldList.find(item => item.key === el.key)) {
+  if (!state.displayFieldList.find((item: any) => item.key === el.key)) {
     const newItem = deepCopy(el)
     state.displayFieldList.push(newItem)
   }
 }
 
 function removeItem(el: any) {
-  const index = state.displayFieldList.findIndex(item => item.key === el.key)
+  const index = state.displayFieldList.findIndex((item: any) => item.key === el.key)
   if (index !== -1) {
     state.displayFieldList.splice(index, 1)
   }
@@ -48,10 +48,10 @@ async function saveField() {
     return
   }
 
-  const displayTarget = state.displayFieldList.find(item => item.key === editForm.key)
+  const displayTarget = state.displayFieldList.find((item: any) => item.key === editForm.key)
   if (displayTarget) Object.assign(displayTarget, editForm)
 
-  const systemTarget = state.systemFieldList.find(item => item.key === editForm.key)
+  const systemTarget = state.systemFieldList.find((item: any) => item.key === editForm.key)
   if (systemTarget) Object.assign(systemTarget, editForm)
 
   state.visible = false
@@ -59,7 +59,7 @@ async function saveField() {
 
 async function handleSubmit() {
   try {
-    const newProperties = state.displayFieldList.reduce((acc, item, index) => {
+    const newProperties = state.displayFieldList.reduce((acc: any, item: any, index: number) => {
       acc[item.key] = { ...item, sort: index }
       return acc
     }, {})
@@ -69,18 +69,16 @@ async function handleSubmit() {
         routerProvider?.message.success(t('tip_updateSuccessMsg', { modelName: null, name: null }))
       }
     })
-
-
-  } catch (e) {
+  } catch (e: any) {
     throw createError(e)
   }
 }
 
 async function init() {
   try {
-    let { properties } = await adminApi.api.getUserProfileSetting().then(res => res.data)
+    let { properties }: any = await adminApi.api.getUserProfileSetting().then((res) => res.data)
     state.displayFieldList = Object.keys(properties)
-      .map(key => ({
+      .map((key) => ({
         key,
         type: properties[key].type,
         readyOnly: properties[key].readyOnly,
@@ -92,8 +90,8 @@ async function init() {
       .sort((a, b) => a.sort - b.sort)
       .map(({ sort, ...rest }) => rest)
 
-    state.systemFieldList = await adminApi.api.getUserSystemFields().then(res => res.data)
-  } catch (e) {
+    state.systemFieldList = await adminApi.api.getUserSystemFields().then((res) => res.data)
+  } catch (e: any) {
     throw createError(e)
   }
 }
@@ -113,23 +111,26 @@ onMounted(() => {
         <h3>{{ $t('user.setting.displayField') }}</h3>
         <el-divider />
 
-        <draggable
-          class="dragArea list-group"
-          :list="state.displayFieldList"
-          group="people"
-          item-key="id"
-        >
+        <draggable class="dragArea list-group" :list="state.displayFieldList" group="people" item-key="id">
           <template #item="{ element }">
             <div class="list-group-item">
               <div class="field-row">
                 <SvgIcon class="fa fa-align-justify" style="width: 12px" src="/icons/drag.svg" />
-                <span style="font-weight: bold;">{{ $t(`user.setting.${element.key}`) }}</span>
+                <span style="font-weight: bold">{{ $t(`user.setting.${element.key}`) }}</span>
 
                 <div class="icon-actions">
-                  <svg disabled="element.allowUserEdit" @click="openDialog(element)" xmlns="http://www.w3.org/2000/svg"
-                       width="15" height="15" viewBox="0 0 256 256">
-                    <path fill="currentColor"
-                          d="m232.49 55.51l-32-32a12 12 0 0 0-17 0l-96 96A12 12 0 0 0 84 128v32a12 12 0 0 0 12 12h32a12 12 0 0 0 8.49-3.51l96-96a12 12 0 0 0 0-16.98M192 49l15 15l-11 11l-15-15Zm-69 99h-15v-15l56-56l15 15Zm105-7.43V208a20 20 0 0 1-20 20H48a20 20 0 0 1-20-20V48a20 20 0 0 1 20-20h67.43a12 12 0 0 1 0 24H52v152h152v-63.43a12 12 0 0 1 24 0" />
+                  <svg
+                    disabled="element.allowUserEdit"
+                    @click="openDialog(element)"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    viewBox="0 0 256 256"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m232.49 55.51l-32-32a12 12 0 0 0-17 0l-96 96A12 12 0 0 0 84 128v32a12 12 0 0 0 12 12h32a12 12 0 0 0 8.49-3.51l96-96a12 12 0 0 0 0-16.98M192 49l15 15l-11 11l-15-15Zm-69 99h-15v-15l56-56l15 15Zm105-7.43V208a20 20 0 0 1-20 20H48a20 20 0 0 1-20-20V48a20 20 0 0 1 20-20h67.43a12 12 0 0 1 0 24H52v152h152v-63.43a12 12 0 0 1 24 0"
+                    />
                   </svg>
                   <el-icon @click="removeItem(element)">
                     <DeleteFilled />
@@ -156,7 +157,7 @@ onMounted(() => {
           <template #item="{ element }">
             <div class="list-group-item">
               <div class="field-row2">
-                <span class="list-group-item" style="font-weight: bold;">{{ $t(`user.setting.${element.key}`) }}</span>
+                <span class="list-group-item" style="font-weight: bold">{{ $t(`user.setting.${element.key}`) }}</span>
 
                 <div class="icon-actions">
                   <span style="font-size: 12px">{{ $t('user.setting.systemField') }}</span>
@@ -194,8 +195,7 @@ onMounted(() => {
         <ElButton id="UserProfileView__Dialog__Cancel" type="info" @click="state.visible = false">
           {{ $t('dpButtom_cancel') }}
         </ElButton>
-        <ElButton id="UserProfileView__Dialog__Save" type="primary" @click="saveField">{{ $t('common_save') }}
-        </ElButton>
+        <ElButton id="UserProfileView__Dialog__Save" type="primary" @click="saveField">{{ $t('common_save') }} </ElButton>
       </div>
     </template>
   </el-dialog>
