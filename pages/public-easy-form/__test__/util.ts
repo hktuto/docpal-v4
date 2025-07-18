3fbf4ffe0d72810558ae5aa67a73ebb25acbde42
oy <OY.Ou@weltronics.com>
2025-07-18 14:09:43 +0800

    [unit-test]public-easy-form

---
+++ pages/public-easy-form/__test__/util.ts
@@ -0,0 +1,59 @@
+import { vi } from 'vitest';
+export const FormRendererRef = {
+  getFormJson: () => ({ widgetList: [], formConfig: {} }),
+  init: () => {
+    return {};
+  },
+  vFormRenderRef: {
+    setFormData: () => {
+      return true;
+    },
+    getFormData: () => {
+      return {
+        fileName: 'testFile',
+      };
+    },
+    resetForm: () => {
+      return true;
+    },
+    getFormJson: () => ({ widgetList: [], formConfig: {} })
+  },
+
+  getData: () => {
+    return {};
+  }
+};
+export const MetaFormRef = {
+  init: (setting) => {
+    return setting;
+  },
+  getData: () => {
+    return {};
+  }
+};
+export const mockRouterProvider = {
+  navigateTo: vi.fn(),
+  updateProps: vi.fn(),
+  menuSymbol: 'mockMenuSymbol',
+  message: {
+    success: vi.fn(),
+    warning: vi.fn(),
+    error: vi.fn(),
+  }
+};
+
+export const FormRenderer = {
+  template: '<div class="FormRenderer">FormRenderer</div>',
+  methods: {
+    setFormJson: vi.fn(),
+    setFormData: vi.fn()
+  }
+}
+export const VFormRender = {
+  template: '<div class="FormRenderer">FormRenderer</div>',
+  methods: {}
+}
+export const ReaderDialog = {
+  template: '<div class="FormRenderer">FormRenderer</div>',
+  methods: {}
+}
