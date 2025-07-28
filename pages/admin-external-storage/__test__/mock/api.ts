b6ea1c97c697247bc0d4eb6c30c9e42ca4c5c4db
OY <oy.ou@wclsolution.com>
2025-07-28 16:10:28 +0800

    [externalStorage]add

--- pages/admin-external-storage/__test__/mock/api.ts
+++ pages/admin-external-storage/__test__/mock/api.ts
@@ -1,18 +1,42 @@
 import { vi } from 'vitest'
 export const adminApi = {
   api: {
-    deleteCompanyprofilesCompanyid: vi.fn(),
-    putCompanyprofilesCompanyid: vi.fn(),
-    postCompanyprofiles: vi.fn(),
-    getCompanyprofilesCompanyidChopsCompanychopidFile: vi.fn(),
-    postCompanyprofilesCompanyidChops: vi.fn(),
-    putCompanyprofilesCompanyidChopsCompanychopid: vi.fn(),
+    patchExternalstorageIdStatus: vi.fn(),
     putCompanyprofilesCompanyidChopsCompanychopidStatus: vi.fn(),
     deleteCompanyprofilesCompanyidChopsCompanychopid: vi.fn(),
+    // for ExternalStorageNewDialog test
+    postExternalstorage: vi.fn(),
+    putExternalstorageId: vi.fn(),
+    // for ExternalStorageDetail test
+    getExternalstorageId: vi.fn(),
+    postExternalstorageIdProfilesPage: vi.fn(),
+    patchExternalstorageIdProfilesProfileidStatus: vi.fn(),
+    deleteExternalstorageIdProfilesProfileid: vi.fn(),
+    // for ExternalStorageProfileDetail test
+    getExternalstorageIdProfilesProfileid: vi.fn(),
+    // for ExternalStorageProfilesGenerate test
+    patchExternalstorageIdProfilesProfileidGeneral: vi.fn(),
+    // for ExternalStorageProfilesImport test
+    patchExternalstorageIdProfilesProfileidImport: vi.fn(),
+    // for ExternalStorageProfilesProcess test
+    patchExternalstorageIdProfilesProfileidProcess: vi.fn(),
+    // for ExternalStorageProfilesCapture test
+    patchExternalstorageIdProfilesProfileidCapture: vi.fn(),
+    // for ExternalStorageProfilesOutput test
+    getExternalstorageProfilesProfileidOutputrecordList: vi.fn(),
+    patchExternalstorageProfilesProfileidOutputrecordOutputrecordidStatus: vi.fn(),
+    getExternalstorageProfilesProfileidOutputrecordOutputrecordid: vi.fn(),
+    deleteExternalstorageProfilesProfileidOutputrecordOutputrecordid: vi.fn(),
+    // for ExternalStorageProfilesDialog test
+    postExternalstorageIdProfiles: vi.fn(),
+    // for ExternalStorageProfilesOutputDialog test
+    postExternalstorageProfilesProfileidOutputrecord: vi.fn(),
+    patchExternalstorageProfilesProfileidOutputrecordOutputrecordid: vi.fn(),
+    // for WorkflowVariableMapping test
+    postWorkflowProperties: vi.fn(),
   }
 }
 export const clientApi = {
   api: {
-    postNuxeoIdentityUsers: vi.fn(() => Promise.resolve({ data: [{ username: 'username', userId: 'username' }] }))
   }
 }
