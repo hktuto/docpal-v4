0eefa35da37125e15adda926c86644b552def79f
Seantsang <9394wing@gmail.com>
2025-07-28 16:06:31 +0800

    remove ai tool

--- packages/authApp/app.vue
+++ packages/authApp/app.vue
@@ -1,15 +1,9 @@
 <script lang="ts" setup>
-import { StagewiseToolbar, type ToolbarConfig } from '@stagewise/toolbar-vue';
 
-const config: ToolbarConfig = {
-  plugins: [], // Add your custom plugins here
-};
 </script>
 
 <template>
-  <ClientOnly>
-      <StagewiseToolbar :config="config" />
-    </ClientOnly>
+
     <NuxtPage />
 </template>
 
