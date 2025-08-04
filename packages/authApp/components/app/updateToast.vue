7dd941cbc390f132b03d683d6c26324b336d7a5d
Seantsang <9394wing@gmail.com>
2025-08-04 13:09:53 +0800

    add update toast
    (cherry picked from commit 65a457611cc5c2fd2de2db7a7e8ab4b1668127cf)

---
+++ packages/authApp/components/app/updateToast.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ElNotification } from 'element-plus'
+const nuxtApp = useNuxtApp()
+const { t } = useI18n()
+// app:manifest:update'
+nuxtApp.hooks.hook('app:manifest:update', () => {
+  setTimeout(() => {
+    showUpdateToast()
+  }, 3000)
+})
+
+
+function showUpdateToast() {
+  ElNotification({
+    title: 'HTML String',
+    dangerouslyUseHTMLString: true,
+    message: `<strong>${t('appVersionUpdate.title')}</strong><br/><button class="el-button el-button--primary" onclick="window.location.reload()">${t('appVersionUpdate.button')}</button>`,
+    duration: 0,
+  })
+}
+
+
+
+
+</script>
+
+<template>
+</template>
