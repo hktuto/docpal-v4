479548514dbdb1fcedf6d85ddc58f3ab700c2b21
OY <oy.ou@wclsolution.com>
2025-08-06 10:48:30 +0800

    [MasterTable]component fix

---
+++ pages/admin-masterTable/components/global/AdminMasterTable/newDead.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pageContainer--padding">
+    <MasterTableNewTable ref="tableRef"></MasterTableNewTable>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'masterTbaleNewDead',
+}
+</script>
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
