ab42e429f9ee0cbac809de0f8e44b125fff3efab
OY <oy.ou@wclsolution.com>
2025-08-06 12:06:51 +0800

    update vform

--- packages/base/types/vform.extend.d.ts
+++ packages/base/types/vform.extend.d.ts
@@ -1,12 +1,49 @@
 export type VariableItem = {
-    name: string,
-    label: string,
-    type: 'date' | 'input' | 'switch' | 'textarea' | 'number' | 'select',
-    disabled: Boolean,
-    hidden: Boolean,
-    required: Boolean,
-    // format?: string,
-    options?: any,
-    // maxLength?: number,
-    // onValidate?: string
+  name: string
+  label: string
+  type: 'date' | 'input' | 'switch' | 'textarea' | 'number' | 'select'
+  disabled: Boolean
+  hidden: Boolean
+  required: Boolean
+  // format?: string,
+  options?: any
+  // maxLength?: number,
+  // onValidate?: string
+}
+export type MetadataType = MetadataMasterTable | MetadataNumber | MetadataBoolean | MetadataSelect | MetadataDate | MetadataText
+export type DocumentMetadata = Record<string, MetadataType>
+export type MetadataNumber = {
+  validationRuleName: 'number'
+  minimum?: number
+  maximum?: number
+  multipleOf?: number
+}
+export type MetadataText = {
+  validationRuleName: 'text'
+  maxLength?: number
+}
+export type MetadataBoolean = {
+  validationRuleName: 'boolean'
+}
+export type MetadataSelect = {
+  validationRuleName: 'select'
+  options?: string[]
+  isMultiple?: boolean
+}
+export type MetadataDate = {
+  validationRuleName: 'date'
+  dateOrDateTime?: 'date' | 'dateTime'
+  format?: string,
+  defaultValue ?: string
+}
+export type MetadataMasterTable = {
+  validationRuleName: 'mastertable'
+  masterTableName?: string
+  displayColumn?: string
+  valueColumn?: string
+  isMultiple?: boolean
+  options?: {
+    label: string
+    value: string
+  }[]
 }
