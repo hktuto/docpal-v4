e3fa80947d9210ccf8a33922672d575ccab235e5
OY <oy.ou@wclsolution.com>
2025-08-07 10:02:36 +0800

    [metadata-number] Convert decimal to multipleOf to comply with the standard

--- pages/admin-document-type/components/global/metadata/validator/number.vue
+++ pages/admin-document-type/components/global/metadata/validator/number.vue
@@ -1,6 +1,5 @@
 <template>
   <ElForm ref="formRef" :model="validation" :rules="validationRules" label-position="top">
-
     <ElFormItem :label="t('metadata.validation.number.minimum')" :required="false">
       <ElInput v-model="validation.minimum" :placeholder="t('metadata.validation.number.minimum')" type="number" />
     </ElFormItem>
@@ -10,15 +9,14 @@
     </ElFormItem>
 
     <ElFormItem :label="t('marsterTable.type.decimal')" :required="true">
-      <ElInput v-model="validation.decimal" :placeholder="t('marsterTable.type.decimal')" type="number" min="0" />
+      <ElInput v-model="validation.multipleOf" :placeholder="t('marsterTable.type.decimal')" type="number" min="0" />
     </ElFormItem>
-    
   </ElForm>
 </template>
 
 <script lang="ts" setup>
-import type{ NumberValidation } from '../../../../utils/metadataHelper';
-import type { FormInstance } from 'element-plus';
+import type { NumberValidation } from '../../../../utils/metadataHelper'
+import type { FormInstance } from 'element-plus'
 
 const validation = defineModel<NumberValidation>('validation', { required: true })
 
@@ -27,18 +25,18 @@ const { t } = useI18n()
 const validationRules = reactive({
   decimal: [
     {
-      required: true,
+      required: true
     }
   ]
 })
 
 const formRef = ref<FormInstance>()
 
-const validate = async () =>  formRef.value?.validate()
+const validate = async () => formRef.value?.validate()
 
 const getData = () => {
   return validation.value
 }
 
-defineExpose( { validate, getData })
-</script> 
+defineExpose({ validate, getData })
+</script>
