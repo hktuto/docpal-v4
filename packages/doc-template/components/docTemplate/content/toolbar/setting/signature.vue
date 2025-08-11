1d8e6c57978d69c78722b2c40f92b089e6d2324d
Seantsang <9394wing@gmail.com>
2025-08-11 20:49:41 +0800

    basic signature

--- packages/doc-template/components/docTemplate/content/toolbar/setting/signature.vue
+++ packages/doc-template/components/docTemplate/content/toolbar/setting/signature.vue
@@ -18,21 +18,9 @@ if (!provider) throw new Error('DocTemplateProvider not found')
 const { editor } = provider
 const { t } = useI18n()
 
-// Inline SVG dummy signature image (base64-encoded)
-const dummySignatureSVG = 'data:image/svg+xml;base64,' +
-  btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="200" height="60" viewBox="0 0 200 60">
-    <rect width="200" height="60" fill="#fff"/>
-    <path d="M20,40 Q40,10 60,40 Q80,70 100,40 Q120,10 140,40 Q160,70 180,40" stroke="#222" stroke-width="2" fill="none"/>
-    <text x="100" y="55" font-size="16" text-anchor="middle" fill="#888" font-family="cursive">Signature</text>
-  </svg>`);
-
-
 function handleInsertSignature() {
   editor.value.commands.insertSignature({
-    id: 'signature-1',
-    name: 'John Doe',
     prefix: 'Best regards,',
-    signature: dummySignatureSVG,
     suffix: 'CEO, Company Inc.'
   })
 }
