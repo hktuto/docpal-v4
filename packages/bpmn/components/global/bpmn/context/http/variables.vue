9c81bd08d1ead21c85d8afeff01cb251a3f3acfc
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-08-11 18:09:48 +0800

    [UPDATE] update http request task, create http params and header variables drawer

---
+++ packages/bpmn/components/global/bpmn/context/http/variables.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+
+const { t } = useI18n()
+const dialogVisible = ref()
+const props = defineProps<{
+  title: string
+}>()
+const emits = defineEmits(['updateVariable'])
+const state = reactive({
+  variables: []
+})
+
+function openDrawer(variables: any) {
+  dialogVisible.value = true
+  state.variables = deepCopy(variables)
+}
+
+function handleCreate() {
+  state.variables.push({ key: '', value: '' })
+}
+
+function emitValue(index: number) {
+
+}
+
+function handleDelete(index: number) {
+  state.variables.splice(index, 1)
+}
+
+function handleSubmit() {
+  dialogVisible.value = false
+  emits('updateVariable', state.variables)
+}
+
+defineExpose({ openDrawer })
+</script>
+
+<template>
+  <el-drawer v-model="dialogVisible" :title="props.title" size="100%">
+    <div>
+      <el-table :data="state.variables" style="width: 100%">
+        <el-table-column :label="t('Key')" prop="key">
+          <template #default="{ row, $index }">
+            <el-input v-model="row.key" @input="emitValue($index)" />
+          </template>
+        </el-table-column>
+        <el-table-column :label="t('Value')" prop="value">
+          <template #default="{ row, $index }">
+            <el-input v-model="row.value" @input="emitValue($index)" />
+          </template>
+        </el-table-column>
+        <el-table-column align="right" :label="t('dpTable_actions')">
+          <template #default="scope">
+            <el-button size="small" type="danger" @click="handleDelete(scope.$index)">
+              {{ t('common_delete') }}
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-button @click="handleCreate">
+        {{ t('docTemplate.variable.addRow') }}
+      </el-button>
+    </div>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="handleSubmit"> {{ t('common_submit') }}</el-button>
+      </div>
+    </template>
+  </el-drawer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
