1f2ec3d86cd87c432cf995f24b5c3d656e3a68df
Seantsang <9394wing@gmail.com>
2025-08-13 15:17:50 +0800

    remove log

--- packages/sql-store/.playground/nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// Nuxt config for the sql-store demo package
-// Enables COOP/COEP for cross-origin isolation and configures Vite to serve WASM
-import { defineNuxtConfig } from 'nuxt/config'
-
-export default defineNuxtConfig({
-  extends:['../../base','../'],
-})
