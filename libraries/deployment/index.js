3aecfa7e8742614bb7ff9a935e21f9c0c2131bf2
Seantsang <9394wing@gmail.com>
2025-08-19 16:42:33 +0800

    remove dependence

--- libraries/deployment/index.js
+++ libraries/deployment/index.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
+const path = require('path');
+function parseArgv() {
+    const args = {};
+    for (let i = 2; i < process.argv.length; i++) {
+        let arg = process.argv[i];
+        if (arg.startsWith('--')) {
+            const key = arg.slice(2);
+            let value = true;
+            if (i + 1 < process.argv.length && !process.argv[i + 1].startsWith('--')) {
+                value = process.argv[i + 1];
+                i++;
+            }
+            args[key] = value;
+        }
+    }
+    return args;
+}
+const argv = parseArgv();
 
 function normalizeSetting(){
     let path = argv.path || '../api/src'
@@ -19,7 +36,7 @@ function normalizeSetting(){
 
 function generateJson(){
     const config = normalizeSetting()
-    fs.writeFileSync(`${config.path}/setting.json`, JSON.stringify(config.setting))
+    fs.writeFileSync(path.resolve(__dirname, `${config.path}/setting.json`), JSON.stringify(config.setting))
     // update desktop app
     // if(argv.app === 'client') {
     //     const { DOCPAL_END_POINT } = argv
