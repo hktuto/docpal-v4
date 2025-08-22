48dd00cd24a9d9c3e82000b8d4d4777accc6b0d7
oy <OY.Ou@weltronics.com>
2025-08-22 16:54:33 +0800

    【workflow-editor】type

---
+++ packages/bpmn/components/bpmnRule/field/boolean.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  form: any
+}>()
+const { t } = useI18n()
+</script>
+
+<style lang="scss" scoped>
+// 组件样式可以根据需要添加
+</style>
---
+++ packages/bpmn/components/bpmnRule/field/userRoleUserGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ElFormItem :label="t('metadata.validation.role.allow')" prop="allow">
+      <ElSelect v-model="form.allow" :placeholder="t('metadata.validation.role.allow')">
+        <ElOption label="All" value="ALL" />
+        <ElOption label="User Role" value="USER_ROLE" />
+        <ElOption label="User Group" value="USER_GROUP" />
+      </ElSelect>
+    </ElFormItem>
+
+    <ElFormItem :label="t('meta.multiple')" prop="isMultiple">
+      <ElSwitch v-model="form.isMultiple" />
+    </ElFormItem>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  form: any
+}>()
+const { t } = useI18n()
+
+// Initialize default values if not exists
+if (!props.form.allow) {
+  props.form.allow = 'ALL'
+}
+if (!props.form.isMultiple) {
+  props.form.isMultiple = false
+}
+</script>
+
+<style lang="scss" scoped>
+// Component styles can be added as needed
+</style>
