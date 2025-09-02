7cb2bccc9f0fc32c5ddebcb0744cb7670993d46b
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-09-02 15:02:40 +0800

    [UPDATE] update add bpmn start Event StartCandidate and CandidateRoles

--- packages/bpmn/components/global/bpmn/context/startEvent.vue
+++ packages/bpmn/components/global/bpmn/context/startEvent.vue
@@ -4,12 +4,63 @@ const { node } = defineProps<{
     node:Node
 }>()
 const emits = defineEmits(['openForm'])
+
+const switchRef = ref(true)
+
+function handleSwitch() {
+  const nodeData = node.getData()
+  const newData = {
+    ...nodeData,
+    version: (nodeData.version || 0) + 1
+  }
+
+  if (switchRef.value) {
+    delete nodeData.data['attr_flowable:candidateRoles']
+
+    nodeData.data['attr_flowable:candidateGroups'] = ''
+    nodeData.data.extensionElements = {
+      ['modeler:activiti-idm-candidate-group']: {
+        ['attr_xmlns:modeler']: 'http://flowable.org/modeler',
+        ['__cdata']: true
+      },
+      ['modeler:initiator-can-complete']: {
+        ['attr_xmlns:modeler']: 'http://flowable.org/modeler',
+        ['__cdata']: false
+      },
+      ['modeler:group-info-name-Adhoc_Group']: {
+        ['attr_xmlns:modeler']: 'http://flowable.org/modeler',
+        ['__cdata']: ''
+      }
+    }
+  } else {
+    delete nodeData.data['attr_flowable:candidateGroups']
+
+    nodeData.data.extensionElements = {
+      ['flowable:taskListener']: {
+        ['attr_delegateExpression']: '${customTaskAssignmentListener}',
+        ['attr_event']: 'create'
+      }
+    }
+  }
+  node.setData(newData, { overwrite: true, deep: true, silent: false })
+}
+
+watch(() => node, () => {
+  if (node) {
+    switchRef.value = ['attr_flowable:candidateGroups'] in node.data.data
+  }
+}, {
+  immediate: true,
+  deep: true
+})
 </script>
 
 <template>
 <div class="fromContainer">
     <BpmnSidebarEditLabel :node="node" />
-    <BpmnSidebarEditStartCandidate :node="node" />
+  <el-switch v-model="switchRef" size="small" active-text="Group" inactive-text="Roles" @change="handleSwitch" />
+    <BpmnSidebarEditStartCandidate v-if="switchRef" :node="node"  />
+    <BpmnSidebarEditCandidateRoles v-else :node="node" />
     <BpmnSidebarStarterAdditionLogic :node="node" />
     <BpmnSidebarEditForm :node="node" />
     <BpmnSidebarPreviewDocument :node="node" />
