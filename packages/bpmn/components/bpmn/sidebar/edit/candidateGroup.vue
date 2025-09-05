4b8104d9984e1d512cf3da07699f75f2c8221d55
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-09-05 15:37:18 +0800

    [UPDATE] bugfix workflow user task and start task candidate Groups bpmn xml setting; update workflow message notification task user field setting, update message field add json editor to json format and json format check

--- packages/bpmn/components/bpmn/sidebar/edit/candidateGroup.vue
+++ packages/bpmn/components/bpmn/sidebar/edit/candidateGroup.vue
@@ -1,110 +1,105 @@
 <script lang="ts" setup>
 
 import type { Node } from '@antv/x6'
-import { adminApi } from 'api' 
+import { adminApi } from 'api'
+
 const { node } = defineProps<{
-    node:Node
+  node: Node
 }>()
 
 
 const graphProvider = inject(BPMN_PROVIDER)
-const editorProvider = inject(EDITOR_PROVIDER);
-if(!graphProvider || !editorProvider) {
-    throw createError('provider not found') 
+const editorProvider = inject(EDITOR_PROVIDER)
+if (!graphProvider || !editorProvider) {
+  throw createError('provider not found')
 }
 
 const allUserGroup = ref<any[]>([])
 const candidateGroup = ref<string>('')
 
 
-function refreshData(){
-    // get candidateGroup
-    const data = node.getData();
-    
-    candidateGroup.value = data.data['attr_flowable:candidateGroups'] || '';
+function refreshData() {
+  // get candidateGroup
+  const data = node.getData()
+
+  candidateGroup.value = data.data['attr_flowable:candidateGroups'] || ''
 
 }
 
 function candidateGroupChanged(newVal: string) {
-    const data = node.getData();
-    let groupKey = "";
-    Object.keys(data.data.extensionElements).forEach((key) => {
-        if(key.includes('modeler:group-info-name-')){
-            groupKey = key;
-        }
-    });
-    if(groupKey) {
-        delete data.data.extensionElements[groupKey];
+  const data = node.getData()
+  let groupKey = ''
+  Object.keys(data.data.extensionElements).forEach((key) => {
+    if (key.includes('modeler:group-info-name-')) {
+      groupKey = key
     }
-    // newVal is string, get gorup object from allUserGroup
-    const group = allUserGroup.value.find((item) => item.id === newVal);
-    if(group) {
-        data.data.extensionElements['modeler:group-info-name-' + group.id] = {
-            __cdata: group.id,
-            ['attr_xmlns:modeler']:"http://flowable.org/modeler"
-        }
-        data.data['attr_flowable:candidateGroups'] = group.id;
-    }else {
-        delete data.data['attr_flowable:candidateGroups'];
+  })
+  if (groupKey) {
+    delete data.data.extensionElements[groupKey]
+  }
+  // newVal is string, get gorup object from allUserGroup
+  const group = allUserGroup.value.find((item) => item.id === newVal)
+  if (group) {
+    data.data.extensionElements['modeler:group-info-name-' + group.id] = {
+      __cdata: group.id,
+      ['attr_xmlns:modeler']: 'http://flowable.org/modeler'
     }
-    node.setData({
-        ...data,
-        version: node.data.version+1 || 0,
-    }, {
-        overwrite: true,
-        deep: true,
-    })
-
+    data.data['attr_flowable:candidateGroups'] = group.id
+  } else {
+    delete data.data['attr_flowable:candidateGroups']
+  }
+  node.setData({
+    ...data,
+    version: node.data.version + 1 || 0
+  }, {
+    overwrite: true,
+    deep: true
+  })
 }
-
 
 async function getUserGroup() {
-    const data = await adminApi.api.postNuxeoIdentityGroups();
-    if(data.data){
-        allUserGroup.value = data.data.sort((a:any, b:any) => a.name.localeCompare(b.name))
-    }else {
-        allUserGroup.value = []
-    }
+  const data = await adminApi.api.postNuxeoIdentityGroups()
+  if (data.data) {
+    allUserGroup.value = data.data.sort((a: any, b: any) => a.name.localeCompare(b.name))
+  } else {
+    allUserGroup.value = []
+  }
 }
 
-function setUpListener(){
-    graphProvider?.graph.value?.on('history:undo', () => {
-      refreshData()
-    })
-    graphProvider?.graph.value?.on('history:redo', () => {
-      refreshData()
-    })
-}
-
-
-onMounted(async() => {
-    await getUserGroup()
+function setUpListener() {
+  graphProvider?.graph.value?.on('history:undo', () => {
     refreshData()
-    setUpListener()
+  })
+  graphProvider?.graph.value?.on('history:redo', () => {
+    refreshData()
+  })
+}
+
+onMounted(async () => {
+  await getUserGroup()
+  refreshData()
+  setUpListener()
 })
 
-
-watch(() => node, ()=> {
-  if(node) {
+watch(() => node, () => {
+  if (node) {
     refreshData()
   }
-},{
+}, {
   immediate: true,
   deep: true
 })
-
-
 </script>
 
 <template>
-    <div class="itemContainer">
-        <ElForm label-position="top" label-width="100px" size="small">
-            <ElFormItem label="Candidate Group">
-                  <ElSelect v-model="candidateGroup" placeholder="Select Group" :disabled="editorProvider.readonly.value" filterable @change="candidateGroupChanged">
-                    <ElOption v-for="item in allUserGroup" :key="item.id" :label="item.name" :value="item.id" />
-                  </ElSelect>
-            </ElFormItem>
-        </ElForm>
-    </div>
-
+  <div class="itemContainer">
+    <ElForm label-position="top" label-width="100px" size="small">
+      <ElFormItem label="Candidate Group">
+        <ElSelect v-model="candidateGroup" placeholder="Select Group" :disabled="editorProvider.readonly.value"
+                  filterable clearable @change="candidateGroupChanged">
+          <ElOption v-for="item in allUserGroup" :key="item.id" :label="item.name" :value="item.id" />
+        </ElSelect>
+      </ElFormItem>
+    </ElForm>
+  </div>
 </template>
--- packages/bpmn/components/bpmn/sidebar/starterAdditionLogic.vue
+++ packages/bpmn/components/bpmn/sidebar/starterAdditionLogic.vue
@@ -1,67 +1,67 @@
 <script lang="ts" setup>
 import type { Node } from '@antv/x6'
 
-
-const { node} = defineProps<{
-    node: Node
+const { node } = defineProps<{
+  node: Node
 }>()
-
-type Form = {
-    attr_openInNewPage: boolean
+const editorProvider = inject(EDITOR_PROVIDER)
+if (!editorProvider) {
+  throw createError('graph provider not found')
 }
-const defaultForm:Form = {
-    attr_openInNewPage: false,
+type Form = {
+  attr_openInNewPage: boolean
+}
+const defaultForm: Form = {
+  attr_openInNewPage: false
 }
 const form = ref<Form>({
-    attr_openInNewPage: false
+  attr_openInNewPage: false
 })
 
-function setForm(){
-    const nodeData = node.getData()
-    const newData = {
-        ...nodeData,
-        version: nodeData.version + 1 || 1,
-        data:{
-            ...nodeData.data,
-            extensionElements:{
-                ...nodeData.data.extensionElements,
-                'docpal:additionaSetting': JSON.parse(JSON.stringify(form.value))
-            }
-        }
+function setForm() {
+  const nodeData = node.getData()
+  const newData = {
+    ...nodeData,
+    version: nodeData.version + 1 || 1,
+    data: {
+      ...nodeData.data,
+      extensionElements: {
+        ...nodeData.data.extensionElements,
+        'docpal:additionaSetting': JSON.parse(JSON.stringify(form.value))
+      }
     }
-    node.setData(newData, {
-        deep: true,
-        overwrite: true
-    })
+  }
+  node.setData(newData, {
+    deep: true,
+    overwrite: true
+  })
 }
 
-function getForm(){
-    const nodeData = node.getData()
-    if(nodeData.data && nodeData.data.extensionElements && nodeData.data.extensionElements['docpal:additionaSetting']){
-        form.value = nodeData.data.extensionElements['docpal:additionaSetting'];
-    }else{
-        form.value = {...defaultForm}
-    }
+function getForm() {
+  const nodeData = node.getData()
+  if (nodeData.data && nodeData.data.extensionElements && nodeData.data.extensionElements['docpal:additionaSetting']) {
+    form.value = nodeData.data.extensionElements['docpal:additionaSetting']
+  } else {
+    form.value = { ...defaultForm }
+  }
 }
 
 useAdditionalContext(getForm)
 
 watch(form, () => {
-    setForm()
-},{
-    deep:true
+  setForm()
+}, {
+  deep: true
 })
-
-
 </script>
 
 <template>
- <div class="formContainer">
+  <div class="formContainer">
     <h4>Addtional Setting</h4>
     <ElForm label-position="top">
-        <ElFormItem label="Open Form in new page">
-            <ElSwitch v-model="form.attr_openInNewPage" />
-        </ElFormItem>
-    </ElForm> 
- </div>
+      <ElFormItem label="Open Form in new page">
+        <ElSwitch :disabled="editorProvider.readonly.value" v-model="form.attr_openInNewPage" />
+      </ElFormItem>
+    </ElForm>
+  </div>
 </template>
