9199c161a7a13cd67872fc2f51a1b10a905dd350
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-09-11 18:02:22 +0800

    [UPDATE] update dashboard calendar new Event setting Dynamic Form, test workflow calendar task create,update,delete,cancel workflow test

--- packages/dp-dashboard/components/global/calendar/widget/createDialogForm.vue
+++ packages/dp-dashboard/components/global/calendar/widget/createDialogForm.vue
@@ -2,16 +2,40 @@
 import { clientApi } from 'api'
 
 const { t } = useI18n()
-const shwoSelectUserDialog = ref(false)
-const form = reactive({
-  name: '',
-  location: '',
-  allDayEvent: false,
-  startDate: '',
-  endDate: '',
-  limitSeat: false,
-  availableSeat: 0
+const FormRendererRef = ref()
+const showSelectUserDialog = ref(false)
+const formRenderSlotsRef = ref<any>({})
+const state = reactive({
+  formData: {},
+  formJson: {},
+  writableIds: [],
+  readonly: false
 })
+const { formData, formJson } = toRefs(state)
+// @ts-ignore
+const { formRenderSlots } = useWorkflow()
+
+async function setForm(json: string | object, data?: object, properties: any[] = []) {
+  if (JSON.stringify(json) === '{}') {
+    FormRendererRef.value.setFormJson(defaultFormJson)
+    return
+  }
+  state.formJson = json
+  FormRendererRef.value.setFormJson(json)
+  // if (data && properties) {
+  // const _data = await handleData(data)
+  // // check if value in _data is undefine or null , if so remove it
+  // Object.keys(_data).forEach((key) => {
+  //   if (_data[key] === undefined || _data[key] === null) delete _data[key]
+  // })
+  // state.formData = { ..._data }
+  //
+  // FormRendererRef.value.setFormData(_data)
+  // handleTypeIds(properties)
+  // } else {
+  state.formData = { ...data }
+  // }
+}
 
 const saveForm = reactive({
   name: '',
@@ -64,87 +88,86 @@ const { tableConfig, tableEvent, tableRef, reload, query } = useVxeTable({
   ]
 })
 
+const userList = []
+const selectUser = reactive({
+  userList: [],
+  userString: ''
+})
+
 function openSelectUser() {
-  shwoSelectUserDialog.value = true
+  selectUser.userString = ''
+  showSelectUserDialog.value = true
 }
 
 function confirm() {
-  shwoSelectUserDialog.value = false
+
+
+  showSelectUserDialog.value = false
 }
 
-function handleAllDayEvent() {
-  if (form.allDayEvent) {
-    form.startDate = ''
-    form.endDate = ''
+async function getFormData(needValidation = true) {
+  try {
+    let formData = {}
+    if (!needValidation) formData = await FormRendererRef.value.getFormData(false)
+    else {
+      formData = await FormRendererRef.value
+        .getFormData()
+        .then((res: any) => {
+          return res
+        })
+        .catch((error: any) => {
+          return false
+        })
+    }
+    if (!formData) return false
+
+    formData.user = selectUser.userString
+    return formData
+  } catch (error) {
+    console.error(error)
   }
 }
 
-defineExpose({ form })
+function disableForm() {
+  state.readonly = true
+  FormRendererRef.value.vFormRenderRef.disableForm()
+}
+
+function enableForm() {
+  state.readonly = false
+  FormRendererRef.value.vFormRenderRef.enableForm()
+}
+
+defineExpose({ setForm, getFormData, disableForm, enableForm })
 </script>
 
 <template>
-  <el-form label-position="top">
-    <el-form-item :label="t('Name')">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item :label="t('Location')">
-      <el-select v-model="form.location">
-        <el-option />
-      </el-select>
-    </el-form-item>
-    <el-form-item :label="t('All Day Event')">
-      <el-switch v-model="form.allDayEvent" @change="handleAllDayEvent" />
-    </el-form-item>
-    <el-form-item v-if="!form.allDayEvent" :label="t('Start Date')">
-      <el-date-picker v-model="form.startDate" type="datetime" clearable placeholder="Select date and time"
-                      value-format="YYYY-MM-DD" />
-    </el-form-item>
-    <el-form-item v-if="!form.allDayEvent" :label="t('End Date')">
-      <el-date-picker v-model="form.endDate" type="datetime" clearable placeholder="Select date and time"
-                      value-format="YYYY-MM-DD" />
-    </el-form-item>
-    <el-divider />
-
-    <el-row>
-      <el-col :span="8">
-        <el-form-item :label="t('Limit Seat')">
-          <el-switch v-model="form.limitSeat" active-text="Yes" inactive-text="No" />
-        </el-form-item>
-      </el-col>
-      <el-col :span="16">
-        <el-form-item v-if="form.limitSeat" :label="t('Available Seat')">
-          <el-input-number v-model="form.availableSeat" controls-position="right" style="width: 100%" min="0"
-                           max="99999999" :step="1" step-strictly />
-        </el-form-item>
-      </el-col>
-    </el-row>
-    <el-divider />
-
-    <div>
+  <FormRenderer ref="FormRendererRef" :form-json="formJson">
+    <template v-slot:user>
       <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent">
         <template #toolbar_buttons>
-          <el-button @click="openSelectUser">{{ $t('Add Participants') }}</el-button>
+          <el-button @click="openSelectUser" type="primary">{{ $t('Add Participants') }}</el-button>
           <slot name="toolbar_buttons" />
         </template>
       </VxeGrid>
-    </div>
-  </el-form>
 
-  <el-dialog v-model="shwoSelectUserDialog" :title="t('Select User')" append-to-body align-center width="400px">
-    <el-form-item :label="t('User')" label-position="top">
-      <el-select>
-        <el-option />
-      </el-select>
-    </el-form-item>
-    <template #footer>
-      <el-button id="Home__Dashboard__Calendar__NewEvent__SelectUser__Cancel" @click="shwoSelectUserDialog = false">
-        {{ $t('vxe.button.cancel') }}
-      </el-button>
-      <el-button id="Home__Dashboard__Calendar__NewEvent__SelectUser__Confirm" type="primary" @click="confirm">
-        {{ $t('dpButtom_confirm') }}
-      </el-button>
+      <el-dialog v-model="showSelectUserDialog" :title="t('Select User')" append-to-body align-center width="400px">
+        <el-form-item :label="t('User')" label-position="top">
+          <el-select v-model="selectUser.userList" multiple clearable>
+            <el-option v-for="user in userList" :key="user.id" :label="user.name" :value="user.id" />
+          </el-select>
+        </el-form-item>
+        <template #footer>
+          <el-button id="Home__Dashboard__Calendar__NewEvent__SelectUser__Cancel" @click="showSelectUserDialog = false">
+            {{ $t('vxe.button.cancel') }}
+          </el-button>
+          <el-button id="Home__Dashboard__Calendar__NewEvent__SelectUser__Confirm" type="primary" @click="confirm">
+            {{ $t('dpButtom_confirm') }}
+          </el-button>
+        </template>
+      </el-dialog>
     </template>
-  </el-dialog>
+  </FormRenderer>
 </template>
 
 <style scoped lang="scss">
