fb389f10b641651d67486d9f4b6bb649deae13e0
Sean Tsang <9394wing@gmail.com>
2025-09-22 15:25:47 +0800

    remove package.json from package and pages

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+console.log("env",process.env.PROXY )
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
