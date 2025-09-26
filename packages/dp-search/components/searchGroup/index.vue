dcb523f7c801b9350056bb2f13800faa04a9ad14
Sean Tsang <9394wing@gmail.com>
2025-09-26 13:21:39 +0800

    [1456] fix search action page aleady open

--- packages/dp-search/components/searchGroup/index.vue
+++ packages/dp-search/components/searchGroup/index.vue
@@ -21,11 +21,11 @@
 const state = reactive<any>({
   aggregation: {}
 })
-const { tableId, searchParams } = defineProps<{
+const props = defineProps<{
   tableId?: string
   searchParams?: any
 }>()
-
+const { searchParams } = toRefs(props)
 const BarRef = ref()
 let searchState: 'firstSearch' | 'aggChange' | '' = ''
 const tableRef = ref()
@@ -49,11 +49,18 @@ function handleUpdateAgg(aggregation: any, aggParams: any = {}) {
 }
 
 onMounted(() => {
-  if (searchParams) {
-    // TODO : save query to tab if changed
-    BarRef.value.setQuery(searchParams)
-    tableRef.value.initBar(searchParams)
+  BarRef.value.setQuery(searchParams.value)
+    tableRef.value.initBar(searchParams.value)
+})
+
+watch(searchParams,(params) => {
+  if(params){
+
+    BarRef.value.setQuery(searchParams.value)
+    tableRef.value.initBar(searchParams.value)
   }
+},{
+  deep:true,
 })
 </script>
 <style lang="scss" scoped>
