e691073ac559d34175c641b89547f2e7e998ce1f
Seantsang <9394wing@gmail.com>
2025-09-27 21:57:30 +0800

    fix tab

--- packages/authApp/pages/index.vue
+++ packages/authApp/pages/index.vue
@@ -4,12 +4,11 @@ const localeReady = ref(false)
 const appPlatform = useAppPlatform()
 appPlatform.value = platform || 'client'
 const defaultTabStore = useAppDefaultTab()
-// defaultTabStore.value =  defaultTab
+defaultTabStore.value =  defaultTab
 </script>
 
 <template>
   <NuxtLayout name="auth" >
-    
   </NuxtLayout>
  
 </template>
