50cce352546ed6208570b11f11908f0a761b5882
Seantsang <9394wing@gmail.com>
2025-09-27 22:58:51 +0800

    update admin gb color

--- packages/authApp/pages/admin.vue
+++ packages/authApp/pages/admin.vue
@@ -28,9 +28,9 @@ defaultTab.value =  {
 
 <style>
 .appFullPage{
-  --app-bg: radial-gradient(72% 72% at 2% -5%, #f3d4b9 0%, #efebe8 51%, #f3f0f0 100%);
-  --app-accent-color: #512e0e;
-  --menu-color: #512e0e;
+  --app-bg: radial-gradient(72% 72% at 2% -5%, #a6dfbb 0%, #c4e1cc 51%, #cce7de 100%);
+  --app-accent-color: #0e5133;
+  --menu-color: #0e5133;
 }
 </style>
 
