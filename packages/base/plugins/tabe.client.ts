fbc805dd540c5a1ab76c005a88a4958dec6d2af9
Sean Tsang <9394wing@gmail.com>
2025-10-09 11:49:52 +0800

    reduce build size

--- packages/base/plugins/tabe.client.ts
+++ packages/base/plugins/tabe.client.ts
@@ -1,18 +1,57 @@
 
-import VxeUIAll from 'vxe-pc-ui';
-import 'vxe-pc-ui/lib/style.css';
-import VxeUITable from 'vxe-table';
-import 'vxe-table/lib/style.css';
-// import VxeUIPluginRenderElement from '@vxe-ui/plugin-render-element';
-import '@vxe-ui/plugin-render-element/dist/style.css';
+// import VxeUIAll from 'vxe-pc-ui';
+// import 'vxe-pc-ui/lib/style.css';
+// import VxeUITable from 'vxe-table';
+// import { VxeUI } from 'vxe-table'
+// import 'vxe-table/lib/style.css';
+// // import VxeUIPluginRenderElement from '@vxe-ui/plugin-render-element';
+// import '@vxe-ui/plugin-render-element/dist/style.css';
+
+import {
+  VxeUI,
+
+  VxeButton,
+  
+  VxeInput,
+  
+} from 'vxe-pc-ui'
+
+import {
+  VxeTable,
+  VxeColumn,
+  VxeColgroup,
+  VxeGrid,
+  VxeToolbar
+} from 'vxe-table'
+
+// 导入主题变量，也可以重写主题变量
+import 'vxe-pc-ui/lib/style.css'
+import 'vxe-table/lib/style.css'
+
+
+function lazyVxeUI (app:any) {
+  
+  app.use(VxeButton)
+  
+  app.use(VxeInput)
+  
+}
+
+function lazyVxeTable (app:any) {
+  app.use(VxeTable)
+  app.use(VxeColumn)
+  app.use(VxeColgroup)
+  app.use(VxeGrid)
+  app.use(VxeToolbar)
+}
 
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(VxeUIAll).use(VxeUITable);
+  nuxtApp.vueApp.use(lazyVxeUI).use(lazyVxeTable);
   const i18n = nuxtApp.$i18n as any;
   // VxeUI.use(VxeUIPluginRenderElement);
 
-  VxeUIAll.setConfig({
+  VxeUI.setConfig({
     // 对组件内置的提示语进行国际化翻译
     i18n: (key, args) => i18n.t(key, args),
     translate: (key, args) => i18n.t(key, args)
