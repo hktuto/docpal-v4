1876df114fc037203f01ee18790b5cc4b165aa68
oy <OY.Ou@weltronics.com>
2025-10-11 16:37:10 +0800

    [DPL-7017-4] Change to : Upload

--- pages/client-ai-upload/utils/aiUpoloadHelper.ts
+++ pages/client-ai-upload/utils/aiUpoloadHelper.ts
@@ -1,30 +1,28 @@
 type AiUploadDetail = {
-    id: string,
-    status: string
+  id: string
+  status: string
 }
-export const createAiUploadDetail = (params:AiUploadDetail) => {
-    return {
-        id: 'ai-upload-detial-' + params.id,
-        name: 'ai-upload-detial-'+params.id,
-        label: "AIUpload",
-        component: "LazyAiUploadDetail",
-        props: {
-            id: params.id,
-            status: params.status
-        }
+export const createAiUploadDetail = (params: AiUploadDetail) => {
+  return {
+    id: 'ai-upload-detial-' + params.id,
+    name: 'ai-upload-detial-' + params.id,
+    label: 'clientAIUpload',
+    component: 'LazyAiUploadDetail',
+    props: {
+      id: params.id,
+      status: params.status
     }
+  }
 }
 
 export const goAiUploadDetail = () => {
-    return {
-        id: 'client-ai-upload',
-            name: 'client-ai-upload',
-            icon: 'lucide:cloud-upload',
-            hoverIcon: 'lucide:cloud-upload',
-            label: "AIUpload",
-            component: "LazyAiUpload",
-            props:{
-                
-            }
-        }
+  return {
+    id: 'client-ai-upload',
+    name: 'client-ai-upload',
+    icon: 'lucide:cloud-upload',
+    hoverIcon: 'lucide:cloud-upload',
+    label: 'clientAIUpload',
+    component: 'LazyAiUpload',
+    props: {}
+  }
 }
