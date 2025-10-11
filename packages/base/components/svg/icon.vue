43b80bffa79da2bbfc1de861a83cad6592a30fcf
oy <OY.Ou@weltronics.com>
2025-10-11 09:52:26 +0800

    [DPL-7017-63]Change icon

--- packages/base/components/svg/icon.vue
+++ packages/base/components/svg/icon.vue
@@ -1,111 +1,119 @@
 <template>
-<div class="flex-x-center svgIcon">
-    <template v-if="src">
-        <template v-if="content">
-            <el-tooltip :content="content">
-                <template v-if="round">
-                    <span :class="svgRoundClass">
-                        <component :is="InlineSvg" :class="svgClass" :src="src"></component>
-                    </span>
-                </template>
-                <template v-else>
-                    <component :is="InlineSvg" :class="svgClass" :src="src"></component>
-                </template>
-            </el-tooltip>
-        </template>
-        <template v-else-if="round">
+  <div class="flex-x-center dp-svg-icon">
+    <template v-if="src || icon">
+      <template v-if="content">
+        <el-tooltip :content="content">
+          <template v-if="round">
             <span :class="svgRoundClass">
-                <component :is="InlineSvg" :class="svgClass" :src="src"></component>
+              <component :is="InlineSvg" :class="svgClass" :src="src"></component>
             </span>
-        </template>
-        <template v-else>
-            <component :is="InlineSvg" :class="svgClass" :src="src"></component>
-        </template>
+          </template>
+          <template v-else>
+            <component v-if="src" :is="InlineSvg" :class="svgClass" :src="src"></component>
+            <Icon v-else :name="icon" :class="'normal '+ svgClass"></Icon>
+          </template>
+        </el-tooltip>
+      </template>
+      <template v-else-if="round">
+        <span :class="svgRoundClass">
+          <component :is="InlineSvg" :class="svgClass" :src="src"></component>
+        </span>
+      </template>
+      <template v-else>
+        <component :is="InlineSvg" :class="svgClass" :src="src"></component>
+      </template>
     </template>
+
     <div class="anime-opacity">
-        <slot></slot>
+      <slot></slot>
     </div>
-</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import InlineSvg from 'vue-inline-svg';
+import InlineSvg from 'vue-inline-svg'
 const props = defineProps<{
-    src: string,
-    round?: boolean,
-    content?: string,
-    disabled?: boolean
+  src: string
+  icon: string
+  round?: boolean
+  content?: string
+  disabled?: boolean
 }>()
 const svgClass = computed(() => {
-    if(props.disabled) return 'svg-icon svg-icon-disabled'
-    return 'svg-icon'
+  if (props.disabled) return 'svg-icon svg-icon-disabled'
+  return 'svg-icon'
 })
 const svgRoundClass = computed(() => {
-    if(props.disabled) return 'svg-icon-round svg-icon-round-disabled'
-    return 'svg-icon-round'
+  if (props.disabled) return 'svg-icon-round svg-icon-round-disabled'
+  return 'svg-icon-round'
 })
 </script>
 
 <style lang="scss" scoped>
 .svg-icon {
-    cursor: pointer;
-    width: var(--icon-size, );
-    height: var(--icon-size, 24px);
+  cursor: pointer;
+  width: var(--icon-size, var(--app-font-size-m));
+  height: var(--icon-size, var(--app-font-size-m));
+  fill: currentColor;
+  color: var(--icon-color, --app-grey-900);
+  &:hover {
+    opacity: 0.7;
+  }
+  &:focus {
+    outline: unset;
+  }
+  path {
     fill: currentColor;
-    color: var(--icon-color, --app-grey-9500);
-    &:hover {
-        opacity: 0.7;
-    }
-    &:focus {
-        outline: unset;
-    }
-    path {
-        fill: currentColor;
-    }
+  }
 }
 
 .svg-icon-round {
-    cursor: pointer;
-    font-size: var(--icon-size, 18px);
-    width: var(--icon-bg-size, 32px);
-    height: var(--icon-bg-size, 32px);
-    color: var(--icon-color, --app-grey-900)!important;
-    background-color: var(--icon-bg-color, var(--app-grey-900));
-    border-radius: 50%;
-    display: grid;
-    place-items: center;
+  cursor: pointer;
+  font-size: var(--icon-size, var(--app-font-size-m));
+  width: var(--icon-bg-size, 32px);
+  height: var(--icon-bg-size, 32px);
+  color: var(--icon-color, --app-grey-900) !important;
+  background-color: var(--icon-bg-color, var(--app-grey-900));
+  border-radius: 50%;
+  display: grid;
+  place-items: center;
+  svg {
+    color: currentColor;
+    width: var(--icon-size, var(--app-font-size-m));
+    height: var(--icon-size, var(--app-font-size-m));
+    &:hover {
+      color: var(--icon-hover-color, --app-grey-9000);
+    }
+  }
+  &:hover {
+    opacity: 0.8;
+    color: var(--icon-hover-color, --app-grey-9000);
     svg {
-        color: currentColor;
-        width: var(--icon-size, 18px);
-        height: var(--icon-size, 18px);
-        &:hover{
-            color: var(--icon-hover-color, --app-grey-9000);
-        }
+      color: var(--icon-hover-color, --app-grey-9000) !important;
     }
-    &:hover{
-        opacity: 0.8;
-        color: var(--icon-hover-color, --app-grey-9000);
-        svg{
-            color: var(--icon-hover-color, --app-grey-9000)!important;
-        }
+  }
+  &-disabled {
+    cursor: not-allowed;
+    background-color: var(--icon-bg-color, var(--app-grey-600));
+    color: var(--app-grey-900) !important;
+    svg:hover {
+      opacity: unset;
+      cursor: unset;
     }
-    &-disabled  {
-        cursor: not-allowed;
-        background-color: var(--icon-bg-color, var(--app-grey-600));
-        color: var(--app-grey-900)!important;
-        svg:hover { 
-            opacity: unset;
-            cursor: unset;
-        }
-        &:hover { opacity: unset }
+    &:hover {
+      opacity: unset;
     }
+  }
 }
 .flex-x-center {
 }
 .anime-opacity {
-    display: none;
-    opacity: 0;
-    top: 0;
-    left: 0;
+  display: none;
+  opacity: 0;
+  top: 0;
+  left: 0;
+}
+.dp-svg-icon +.dp-svg-icon {
+  margin-left: var(--app-space-xxs);
 }
 </style>
--- pages/admin-user/components/group/dialog.vue
+++ pages/admin-user/components/group/dialog.vue
@@ -41,7 +41,7 @@ async function handleSubmit() {
     data.groupName = data.groupName.trim()
     state.loading = true
     await groupProvider?.CreateGroupApi(data)
-    ElMessage.success(t('tip_createdMsg', { modelName: null, name: t('user_NewUserGroup') }))
+    ElMessage.success(t('tip_createdMsg', {name:  t('user_NewUserGroup') }))
     state.visible = false
     FormRendererRef.value.vFormRenderRef.resetForm()
     emits('refresh')
