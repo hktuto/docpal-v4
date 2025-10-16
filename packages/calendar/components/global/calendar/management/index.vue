71bfa6102c494c1bf9fb9a4ddb6f4b5232d9814b
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-10-16 11:59:13 +0800

    [UPDATE] fix calendar management tabs

--- packages/calendar/components/global/calendar/management/index.vue
+++ packages/calendar/components/global/calendar/management/index.vue
@@ -5,16 +5,17 @@ const props = defineProps<{
   processInstanceId: string,
   activeTab: string
 }>()
+const activeTab = ref<string>(props.activeTab)
 </script>
 
 <template>
   <div class="pageContainer--padding calendar-management-page">
-    <el-tabs v-model="props.activeTab">
+    <el-tabs v-model="activeTab">
       <el-tab-pane :label="$t('Calendar View')" name="calendar">
-        <CalendarManagementDetail v-if="props.activeTab === 'calendar'" v-bind="$props" />
+        <CalendarManagementDetail v-if="activeTab === 'calendar'" v-bind="$props" />
       </el-tab-pane>
       <el-tab-pane :label="$t('Table View')" name="table">
-        <CalendarManagementTable v-if="props.activeTab === 'table'"/>
+        <CalendarManagementTable v-if="activeTab === 'table'"/>
       </el-tab-pane>
     </el-tabs>
   </div>
