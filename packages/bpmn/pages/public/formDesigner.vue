5761f4dbae9c651dc9e919871c90414a190ac70d
OY <oy.ou@wclsolution.com>
2025-10-17 15:18:33 +0800

    [public-form-designer]

---
+++ packages/bpmn/pages/public/formDesigner.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const workflowFieldList: any = ref({
+  labelKey: 'name',
+  nameKey: 'id',
+  data: []
+})
+
+const FormDesignRef = ref()
+
+defineExpose({})
+</script>
+
+<template>
+  <div class="pageContainer">
+    <FormDesigner ref="FormDesignRef" :fieldListApi="workflowFieldList"> </FormDesigner>
+  </div>
+</template>
+<style lang="scss" scoped>
+.pageContainer{
+  height: 100vh;
+  width: 100%;
+  padding: var(--app-space-s);
+  overflow: hidden;
+  position: relative;
+}
+</style>
