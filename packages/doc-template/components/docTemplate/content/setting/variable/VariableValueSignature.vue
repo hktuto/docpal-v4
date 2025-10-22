28dab49535429895527a8b464c9197eab28342fd
Seantsang <9394wing@gmail.com>
2025-10-22 09:08:10 +0800

    basic signature setup

--- packages/doc-template/components/docTemplate/content/setting/variable/VariableValueSignature.vue
+++ packages/doc-template/components/docTemplate/content/setting/variable/VariableValueSignature.vue
@@ -1,59 +1,71 @@
 <template>
-  <div class="link-editor">
-    <el-form-item :label="t('Type')">
-      <el-select v-model="link.type" placeholder="Type" @input="emitValue">
-        <el-option v-for="item in linkTypeList" :key="item" :label="item" :value="item" />
-      </el-select>
-    </el-form-item>
-
-    <el-form-item :label="t('Label')">
-      <el-input v-model="link.label" placeholder="Label" style="margin-bottom: 0.5rem;" @input="emitValue" />
-    </el-form-item>
-
-    <el-form-item :label="handleLabel(link.type)">
-      <el-input v-model="link.url" :placeholder="handleLabel(link.type)" @input="emitValue" />
-    </el-form-item>
+  <div class="variable_editor">
+    <template v-for="signature in modelValue">
+      <div class="signatureSettingContainer">
+        {{  signature }}
+        <div class="removeBtnContainer">
+          <Icon name="lucide:trash-2" @click="removeSignature(signature)" />
+        </div>
+      </div>
+    </template>
+    <template v-if="modelValue.length <= SignatureMaxLength">
+      <ElButton @click="addSignature">Add</ElButton>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
 import { ref, watch, defineProps, defineEmits } from 'vue'
 
 const { t } = useI18n()
-const props = defineProps<{ modelValue: { label: string, url: string } }>()
-const emit = defineEmits<{ (e: 'update:modelValue', value: { label: string, url: string }): void }>()
-const link = ref({
-  type: props.modelValue?.type || 'String',
-  label: props.modelValue?.label || '',
-  url: props.modelValue?.url || ''
-})
+
+const modelValue = defineModel<SignatureSetting[]>('modelValue',{default:[]})
+
 const linkTypeList = ['String', 'Document', 'Workflow', 'Case']
 
-watch(() => props.modelValue, v => {
-  link.value = { type: v?.type || 'String', label: v?.label || '', url: v?.url || '' }
-})
+type SignatureSetting = {
+  id: string // timestamp as a uuid
+  type: "personal" | "company", // what type of signature 
+  prefix: string, // signature prefix with template string replace
+  suffix: string, // signature suffix with template string replace
+  value: any, // acture value of the signature to replace with
+  signBy?: string // userId of sign user
+  signatureId?: string  // the signature to replace with
+}
+const defaultValue:SignatureSetting = {
+  id:"",
+  type: 'personal',
+  prefix:"",
+  suffix:"",
+  value: "",
+  signBy: "",
+  signatureId: ""
+} 
+const SignatureMaxLength = 1 // max 2 signature in 1 field
+function addSignature(){
+  if(!Array.isArray(modelValue.value)){
+    modelValue.value = []
+  }
+  nextTick(() => {
+    const newSign = {...defaultValue}
+    newSign.id = Date.now().toString()
+    modelValue.value.push(newSign)
+  })
+}
 
-function handleLabel(type: string) {
-  switch (type) {
-    case 'String':
-      return t('Url')
-    case 'Document':
-      return t('Document Id')
-    case 'Workflow':
-      return t('Workflow Id')
-    case 'Case':
-      return t('Case Id')
-    default:
-      return ''
+function removeSignature(signature:SignatureSetting) {
+  const index = modelValue.value.findIndex( item => item.id === signature.id)
+  console.log("removeSignature", index)
+  if(index !== -1) {
+    modelValue.value.splice(index, 1)
   }
 }
 
-function emitValue() {
-  emit('update:modelValue', { ...link.value })
-}
+
 </script>
 <style lang="scss" scoped>
-.link-editor {
+.variable_editor {
   display: flex;
   flex-direction: column;
+  width:100%;
 }
 </style> 
