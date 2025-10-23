619bbbe299bf971a483896112838070099788481
Seantsang <9394wing@gmail.com>
2025-10-23 09:47:21 +0800

    update form

--- packages/doc-template/components/docTemplate/content/setting/variable/signature/templateEditor.vue
+++ packages/doc-template/components/docTemplate/content/setting/variable/signature/templateEditor.vue
@@ -24,12 +24,20 @@ function changeType(newType:"personal" | "company"){
   emits('update',newData)
 }
 
+function companyChange(newCompany:string) {
+  const newData = {
+    ...props.setting,
+    company:newCompany
+  }
+  emits('update',newData)
+}
+
 async function getCompanyChopList(){
 
 }
 
 function remove(){
-
+  emits('delete')
 }
 
 function addVariable(variable: typeof templateVariableOption[number]){
@@ -40,18 +48,44 @@ function addVariable(variable: typeof templateVariableOption[number]){
 
 <template>
 <div class="templateEditorContainer">
-  <ElSelect :model-value="setting?.type" @change="changeType">
-    <ElOption label="Personal" value="personal" />
-    <ElOption label="Company" value="company" />
-  </ElSelect>
-  <div class="removeBtnContainer">
+  <div class="header">
+    <div class="label">Signature Setting</div>
+    <div class="removeBtnContainer">
           <Icon name="lucide:trash-2" @click="remove" />
         </div>
+  </div>
+  <ElForm>
+    <ElFormItem>
+
+      <ElSelect :model-value="setting?.type" @change="changeType">
+        <ElOption label="Personal" value="personal" />
+        <ElOption label="Company" value="company" />
+      </ElSelect>
+    </ElFormItem>
+  <template v-if="setting?.type === 'company'">
+    <ElSelect :model-value="setting?.company" @change="companyChange">
+      <ElOption v-for="company in companyListOptions" :label="company.name" :value="company.id" />
+    </ElSelect>
+  </template>
+  </ElForm>
 </div>
 </template>
 
 <style lang="css" scoped>
 .templateEditorContainer{
-
+  width: 100%;
+  padding: var(--app-space-s);
+  position: relative;
+  border-radius: var(--app-border-radius-s);
+  border: 1px solid var(--app-grey-900);
+}
+.header{
+  width:100%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+}
+.removeBtnContainer{
+  cursor: pointer;
 }
 </style>
