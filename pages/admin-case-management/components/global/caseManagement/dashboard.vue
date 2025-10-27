f4108a65d2d0586d6d29ff1283598692e50edd26
oy <OY.Ou@weltronics.com>
2025-10-27 15:03:06 +0800

    DPL-7309 [Case-dashboaard] master-table card

--- pages/admin-case-management/components/global/caseManagement/dashboard.vue
+++ pages/admin-case-management/components/global/caseManagement/dashboard.vue
@@ -14,6 +14,9 @@
             </el-dropdown-menu>
           </template>
         </el-dropdown>
+        <!-- <el-button type="danger" @click="handleClear">
+          {{ $t('common_clear') }}
+        </el-button> -->
         <el-button
           id="CaseManagement__Detail__CaseDashboardView__ViewLayout__Save"
           class="el-icon--right"
@@ -77,7 +80,10 @@ function handleDelete(i) {
   const index = state.layout.findIndex((item) => item.i === i)
   state.layout.splice(index, 1)
 }
-
+function handleClear() {
+  state.layout = []
+  handleSave()
+}
 async function handleSave() {
   try {
     state.saveLoading = true
