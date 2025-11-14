eb098fff1fe00d3bb7cae1df21cd97336aac0f23
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-11-14 13:36:26 +0800

    [update] update workflow document Sign

--- packages/workflow/utils/workflowHelper.ts
+++ packages/workflow/utils/workflowHelper.ts
@@ -1,153 +1,157 @@
-import { clientApi } from "api"
-const generateDocumentComponent = "LazyBpmnButtonGenerateDocument"
+import { clientApi } from 'api'
+import { bpmnStringToJson } from '#imports'
+
+const generateDocumentComponent = 'LazyBpmnButtonGenerateDocument'
 const booleanButtonComponent = 'LazyBpmnButtonBoolean'
-import { generateData, replaceVariables } from "docpal-document-editor/src/utils"
+import { generateData, replaceVariables } from 'docpal-document-editor/src/utils'
 
-export async function getBpmnAddtionalElement(xml:any,taskDefinitionKey:string, taskDetail: any, formData:any) {
-    const xmlJson = bpmnStringToJson(xml)
-    const currentTask = xmlJson.flatObj[taskDefinitionKey]
-    let signatureSetting:any = null
-    let buttonSetting:any
-    // check generate document button 
-    let buttons:any[] = []
-    let components:any[] = []
-    if(!currentTask || !currentTask.extensionElements) return {buttons,components}
-    const generateDocumentComponent = "LazyBpmnButtonGenerateDocument"
-    if(currentTask.extensionElements && currentTask.extensionElements['docpal:previewDocumentButton']){
-        if(Array.isArray(currentTask.extensionElements['docpal:previewDocumentButton'])){
-            currentTask.extensionElements['docpal:previewDocumentButton'].forEach((item:any) => {
-                buttons.push({
-                    props: {
-                        ...item,
-                        xml,
-                        formData,
-                        taskDetail
-                    },
-                    component: generateDocumentComponent
-                })
-            })
-        }else{
-            buttons.push({
-                props: {
-                   ...currentTask.extensionElements['docpal:previewDocumentButton'],
-                   xml,
-                   formData,
-                   taskDetail
-                },
-                component: generateDocumentComponent
-            })
-        }
+export async function getBpmnAddtionalElement(xml: any, taskDefinitionKey: string, taskDetail: any, formData: any) {
+  const xmlJson = bpmnStringToJson(xml)
+  const currentTask = xmlJson.flatObj[taskDefinitionKey]
+  let signatureSetting: any = null
+  let buttonSetting: any
+  // check generate document button
+  let buttons: any[] = []
+  let components: any[] = []
+  if (!currentTask || !currentTask.extensionElements) return { buttons, components }
+  const generateDocumentComponent = 'LazyBpmnButtonGenerateDocument'
+  if (currentTask.extensionElements && currentTask.extensionElements['docpal:previewDocumentButton']) {
+    if (Array.isArray(currentTask.extensionElements['docpal:previewDocumentButton'])) {
+      currentTask.extensionElements['docpal:previewDocumentButton'].forEach((item: any) => {
+        buttons.push({
+          props: {
+            ...item,
+            xml,
+            formData,
+            taskDetail
+          },
+          component: generateDocumentComponent
+        })
+      })
+    } else {
+      buttons.push({
+        props: {
+          ...currentTask.extensionElements['docpal:previewDocumentButton'],
+          xml,
+          formData,
+          taskDetail
+        },
+        component: generateDocumentComponent
+      })
     }
-    if(currentTask.extensionElements && currentTask.extensionElements['docpal:booleanButton']){
-        if(Array.isArray(currentTask.extensionElements['docpal:booleanButton'])){
-            currentTask.extensionElements['docpal:booleanButton'].forEach((item:any) => {
-                buttons.push({
-                    props: {
-                        ...item,
-                        xml,
-                        formData,
-                        taskDetail
-                    },
-                    component: booleanButtonComponent
-                })
-            })
-        }else{
-            buttons.push({
-                props: {
-                   ...currentTask.extensionElements['docpal:booleanButton'],
-                   xml,
-                   formData,
-                   taskDetail
-                },
-                component: booleanButtonComponent
-            })
-        }
+  }
+  if (currentTask.extensionElements && currentTask.extensionElements['docpal:booleanButton']) {
+    if (Array.isArray(currentTask.extensionElements['docpal:booleanButton'])) {
+      currentTask.extensionElements['docpal:booleanButton'].forEach((item: any) => {
+        buttons.push({
+          props: {
+            ...item,
+            xml,
+            formData,
+            taskDetail
+          },
+          component: booleanButtonComponent
+        })
+      })
+    } else {
+      buttons.push({
+        props: {
+          ...currentTask.extensionElements['docpal:booleanButton'],
+          xml,
+          formData,
+          taskDetail
+        },
+        component: booleanButtonComponent
+      })
     }
-    // TODO : get buttonSetting
-    if(currentTask.extensionElements && currentTask.extensionElements['docpal:buttonSetting']){
-      const buttonSettingFromTask = currentTask.extensionElements['docpal:buttonSetting']
-      buttonSetting = buttonSettingFromTask
-    }
-    if(currentTask.extensionElements && currentTask.extensionElements['docpal:signatureSetting']){
-      // if docpal:signatureSetting' is in current Task , that mean it is a signature task
-      // step 1 , get signature setting from task
-      const signatureSettingFromTask =  currentTask.extensionElements['docpal:signatureSetting']
-      // step 2, get target template setting from workflow xml
-      const signatureTask = xmlJson.flatObj[signatureSettingFromTask.attr_documentStepId]
-      // step 3, we only store template step id into docpal:signatureSetting, so need to get the template id from template task
-      const templateId = signatureTask.extensionElements['flowable:field'].find((item: any) => item.attr_name === 'templateId')?.['flowable:expression'].__cdata
-      // step 4, get all variable from template task and convert to workflow to template mapping
-      const variablesString = signatureTask.extensionElements['flowable:field'].find((item: any) => item.attr_name === 'variables')?.['flowable:expression'].__cdata
-      const variables = JSON.parse(variablesString)
-      const workflowToTemplateMapping = Object.keys(variables).reduce((prev: any, key: string) => {
-        prev[key] = variables[key].replace('${variables:get(', '').replace(')}', '')
-        return prev
-      }, {})
+  }
+  // TODO : get buttonSetting
+  if (currentTask.extensionElements && currentTask.extensionElements['docpal:buttonSetting']) {
+    const buttonSettingFromTask = currentTask.extensionElements['docpal:buttonSetting']
+    buttonSetting = buttonSettingFromTask
+  }
+  if (currentTask.extensionElements && currentTask.extensionElements['docpal:signatureSetting']) {
+    // if docpal:signatureSetting' is in current Task , that mean it is a signature task
+    // step 1 , get signature setting from task
+    const signatureSettingFromTask = currentTask.extensionElements['docpal:signatureSetting']
+    // step 2, get target template setting from workflow xml
+    const signatureTask = xmlJson.flatObj[signatureSettingFromTask.attr_documentStepId]
+    // step 3, we only store template step id into docpal:signatureSetting, so need to get the template id from template task
+    const templateId = signatureTask.extensionElements['flowable:field'].find((item: any) => item.attr_name === 'templateId')?.['flowable:expression'].__cdata
+    // step 4, get all variable from template task and convert to workflow to template mapping
+    const defaultField: any = ['parentPath', 'storeValue', 'documentName', 'documentType', 'templateId']
+    const workflowToTemplateMapping: any = (signatureTask.extensionElements['flowable:field'] || []).filter((item: any) => !defaultField.includes(item.attr_name))
+      .map((item: any) => ({
+        [item.attr_name]: item['flowable:expression'].__cdata.replace('${variables:get(', '').replace(')}', '')
+      }))
 
-      // step 5, get which workflow information to store signature
-      const currentStepSignatureKey = variables[signatureSettingFromTask.attr_signature]
-      const workflowKeyToStoreSignature = currentStepSignatureKey.replace('${variables:get(', '').replace(')}', '')
-      
-      // step 6, get template detail and setting json
-      const {data:detail} = await clientApi.api.getNuxeoTemplateTemplateid(templateId)
-      let json = await clientApi.api.postNuxeoDocumentPreview({idOrPath:detail.documentId},{
-        format: 'blob'
-      }).then(async(res) => {const t = await res.text(); return JSON.parse(t)})
-      
-      // replace signature variable
-      // convert workflow variable to template variable
-      const templateVariables = convertWorkflowVariableToTemplateVariable(formData, workflowToTemplateMapping)
-      console.log('templateVariables', templateVariables, formData)
-      // get current user detail 
-      const currenUserDetail = useUserState()
-      const userSignatureInfo = {
-        ...currenUserDetail.value,
-        role: currenUserDetail.value?.aclUserDetail?.roleName,
-        signature: "",
-        signDate: Date.now()
-      }
-      const signatureVariableSetting = json.variables.find((item: any) => item.id === signatureSettingFromTask.attr_signature)
-      templateVariables[signatureSettingFromTask.attr_signature] = userSignatureInfo
-      const newVariables = generateData(templateVariables, JSON.parse(JSON.stringify(json)))
-      let templateDetail = JSON.parse(JSON.stringify(json))
-      const content = templateDetail.json.content.content
-      templateDetail.json.content.content = replaceVariables(content, newVariables.variables)
-      // const json.json.content = replaceVariables(json.json.content, newVariables)
-      // finally, store signature setting
-      signatureSetting = {
-        templateDetail,
-        workflowKeyToStoreSignature,
-        signatureVariableSetting,
-        workflowToTemplateMapping,
-        templateId,
-        templateVariables
-      }
-      console.log('signatureSetting', signatureSetting)
-      // get 
+    // step 5, get which workflow information to store signature
+    const found = workflowToTemplateMapping.find((item: any) => item.hasOwnProperty(signatureSettingFromTask.attr_signature))
+    const workflowKeyToStoreSignature = found ? found[signatureSettingFromTask.attr_signature] : ''
+
+    // step 6, get template detail and setting json
+    const { data: detail } = await clientApi.api.getNuxeoTemplateTemplateid(templateId)
+    let json = await clientApi.api.postNuxeoDocumentPreview({ idOrPath: detail.documentId }, {
+      format: 'blob'
+    }).then(async (res) => {
+      const t = await res.text()
+      return JSON.parse(t)
+    })
+
+    // replace signature variable
+    // convert workflow variable to template variable
+    const templateVariables = convertWorkflowVariableToTemplateVariable(formData, workflowToTemplateMapping)
+    console.log('templateVariables', templateVariables, formData)
+    // get current user detail
+    const currenUserDetail = useUserState()
+    const userSignatureInfo = {
+      ...currenUserDetail.value,
+      role: currenUserDetail.value?.aclUserDetail?.roleName,
+      signature: '',
+      signDate: Date.now()
     }
-    return{
-       buttonSetting,
-        buttons,
-        components,
-        signatureSetting
+    const signatureVariableSetting = json.variables.find((item: any) => item.id === signatureSettingFromTask.attr_signature)
+    templateVariables[signatureSettingFromTask.attr_signature] = userSignatureInfo
+    const newVariables = generateData(templateVariables, JSON.parse(JSON.stringify(json)))
+    let templateDetail = JSON.parse(JSON.stringify(json))
+    const content = templateDetail.json.content.content
+    templateDetail.json.content.content = replaceVariables(content, newVariables.variables)
+    // const json.json.content = replaceVariables(json.json.content, newVariables)
+    // finally, store signature setting
+    signatureSetting = {
+      templateDetail,
+      workflowKeyToStoreSignature,
+      signatureVariableSetting,
+      workflowToTemplateMapping,
+      templateId,
+      templateVariables
     }
+    console.log('signatureSetting', signatureSetting)
+    // get
+  }
+  return {
+    buttonSetting,
+    buttons,
+    components,
+    signatureSetting
+  }
 }
 
 export function convertWorkflowVariableToTemplateVariable(variables: any, mapping: any) {
   return Object.keys(mapping).reduce((prev: any, key: string) => {
     const valueKey = mapping[key]
-    if(valueKey && variables[valueKey]) {
+    if (valueKey && variables[valueKey]) {
       // variables[valueKey] may be can convert yto json, so we need to convert it to json
       try {
         const json = JSON.parse(variables[valueKey])
         // REMARK : number will not throw error in JSON.parse, so we need to check it manually
-        if(typeof json === 'number') {
+        if (typeof json === 'number') {
           prev[key] = json.toString()
         } else {
           prev[key] = json
         }
       } catch (e) {
-        if(typeof variables[valueKey] === 'number') {
+        if (typeof variables[valueKey] === 'number') {
           prev[key] = variables[valueKey].toString()
         } else {
           prev[key] = variables[valueKey]
