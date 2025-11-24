49e7839980a9a3577617f1487a357e24ee532975
Seantsang <9394wing@gmail.com>
2025-11-24 12:19:04 +0800

    try hide max-height on form render

--- packages/workflow/components/workflow/Detail/formRender.vue
+++ packages/workflow/components/workflow/Detail/formRender.vue
@@ -294,7 +294,7 @@ provide('workflowFormRender', {
 
 <style lang="scss" scoped>
 .workflowForm {
-  max-height: calc(100vh - 220px);
+  // max-height: calc(100vh - 220px);
   overflow: hidden;
   :deep(.el-form) {
     height: 100%;
