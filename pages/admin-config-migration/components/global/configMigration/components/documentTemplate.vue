68a43c678a3acab9e7d656896372a82c373fc53d
Joshua_Zheng <Joshua.Zheng@wclsolution.com>
2025-11-26 18:07:09 +0800

    [UPDATE] update import config

---
+++ pages/admin-config-migration/components/global/configMigration/components/documentTemplate.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  documentTemplateList: any[]
+}>()
+
+function handleEditDocumentTemplate(item: any) {
+  console.log('item', item)
+}
+</script>
+
+<template>
+  <el-row :gutter="10">
+    <template v-for="item in props.documentTemplateList" :key="item.key">
+      <el-col :span="4">
+        <el-card style="max-height: 100px;">
+          <div class="card-header" @dblclick="handleEditDocumentTemplate(item)">
+            <h4>{{ item.name }}</h4>
+          </div>
+        </el-card>
+      </el-col>
+    </template>
+  </el-row>
+</template>
+
+<style scoped lang="scss">
+.el-col{
+  padding-block: 2px;
+  padding-right: 5px;
+  padding-left: 5px;
+}
+</style>
---
+++ pages/admin-config-migration/components/global/configMigration/components/masterTable.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import { ElMessageBox } from 'element-plus'
+
+const props = defineProps<{
+  masterTableList: any[]
+}>()
+const emit = defineEmits(['update'])
+const showDialog = ref(false)
+const showEditFieldDialog = ref(false)
+const masterTableItem = ref({})
+
+function handleOpenEditDialog(item: any) {
+  console.log('item', item)
+  masterTableItem.value = item
+  tableConfig.data = item.fields
+
+
+  showDialog.value = true
+}
+
+const { tableConfig, tableEvent, tableRef, query, reload, cleanSelectedRows } = useVxeTable({
+  id: 'workflowEditorFieldMappingSetting',
+  zoom: false,
+  virtualScroll: true,
+  columns: [
+    { title: 'Name', field: 'columnName' },
+    { title: 'Type', field: 'dataType' }
+  ],
+  bodyActions: [
+    [
+      {
+        code: 'edit',
+        name: 'common_edit',
+        action: ({ row }) => {
+          handleEditField(row)
+        }
+      },
+      {
+        code: 'delete',
+        name: 'common_delete',
+        action: async ({ row }) => {
+          const action = await ElMessageBox.confirm('Delete Item').catch(action => action)
+          if (action !== 'confirm') return
+          reload()
+        }
+      }
+    ]
+  ],
+  optionalConfig: {},
+  saveColumnOrder: false
+})
+
+function handleEditField(fieldItem: any) {
+
+}
+
+function handleUpdate(item: any) {
+
+  // emit('update', item)
+  showDialog.value = false
+}
+</script>
+
+<template>
+  <el-row :gutter="10">
+    <template v-for="item in props.masterTableList" :key="item.key">
+      <el-col :span="5">
+        <el-card style="min-height: 200px; max-height: 200px;" @dblclick="handleOpenEditDialog(item)">
+          <template #header>
+            <div class="card-header">
+              <h4>{{ item.name }}</h4>
+            </div>
+          </template>
+          <div class="tag-container">
+            <el-tag v-for="fieldItem in item.fields" :key="fieldItem.label" size="small">
+              {{ fieldItem.columnName }}
+            </el-tag>
+          </div>
+        </el-card>
+      </el-col>
+    </template>
+  </el-row>
+
+  <el-dialog v-model="showDialog" title="Master Table Fields" class="big">
+    <div class="tableContainer">
+      <VxeGrid ref="tableRef" v-bind="tableConfig" v-on="tableEvent" />
+    </div>
+  </el-dialog>
+
+  <el-dialog v-model="showEditFieldDialog" title="Edit Master Table Fields" to-body>
+    <el-form>
+      <el-form-item>
+
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button type="primary" @click="handleUpdate">Update</el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<style scoped lang="scss">
+.tag-container {
+  display: flex;
+  flex-wrap: wrap;
+  max-height: 130px;
+  overflow-y: auto;
+  gap: 2px;
+}
+
+.el-col {
+  padding-block: 2px;
+  padding-right: 5px;
+  padding-left: 5px;
+}
+
+.tableContainer {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+}
+</style>
